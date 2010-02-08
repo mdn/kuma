af55e4c9795550893e0de35d94d7fbacde019465
James Socol <james@mozilla.com>
2010-02-08 11:45:49 -0800

    Making /media/ work in development environments

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,4 +7,12 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^search',include('search.urls')),
+
 )
+
+if settings.DEBUG:
+    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+    urlpatterns += patterns('',
+        (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
+          {'document_root': settings.MEDIA_ROOT}),
+    )
