7fff588efbc7b6d8e5b4280deb34182c60a232cd
James Socol <james@mozilla.com>
2010-02-09 11:01:37 -0800

    Adding multidb support

--- settings.py
+++ settings.py
@@ -27,6 +27,11 @@ DATABASES = {
     }
 }
 
+DATABASE_ROUTERS = ('multidb.MasterSlaveRouter',)
+
+# Put the aliases for your slave databases in this list
+SLAVE_DATABASES = []
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
