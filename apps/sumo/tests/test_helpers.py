b261085e171140372853d75d1e844a3b21b342ad
James Socol <james@mozilla.com>
2010-03-03 17:44:52 -0800

    More |suggestions tests

--- apps/sumo/tests/test_helpers.py
+++ apps/sumo/tests/test_helpers.py
@@ -1,7 +1,10 @@
 from nose.tools import eq_
 
+import test_utils
+
 import jingo
 
+jingo.load_helpers()
 
 def render(s, context={}):
     t = jingo.env.from_string(s)
@@ -17,7 +20,38 @@ def test_spellcheck():
 
 
 def test_suggestions():
+    """suggestions should generate the right HTML"""
+    request = test_utils.RequestFactory().get('/en/search?q=worng')
     w = 'worng'
     t = "{{ q|suggestions('en-US') }}"
     exp = '<a href="/en/search?q=wrong"><strong>wrong</strong></a>'
-    eq_(exp, render(t, {'q': w}))
+    eq_(exp, render(t, {'q': w, 'request': request}))
+
+
+def test_suggestions_page2():
+    """suggestions should reset page numbers to 1"""
+    request = test_utils.RequestFactory().get('/en/search?q=worng&page=2')
+    w = 'worng'
+    t = "{{ q|suggestions('en-US') }}"
+    exp = '<a href="/en/search?q=wrong&amp;page=1"><strong>wrong</strong></a>'
+    eq_(exp, render(t, {'q': w, 'request': request}))
+
+
+def test_suggestions_categories():
+    """suggestions should respect MultiValueDict bits"""
+    req = '/en/search?q=worng&category=1&category=2'
+    request = test_utils.RequestFactory().get(req)
+    w = 'worng'
+    t = "{{ q|suggestions('en-US') }}"
+    exp = '<a href="/en/search?q=wrong&amp;category=1&amp;category=2"><strong>wrong</strong></a>'
+    eq_(exp, render(t, {'q': w, 'request': request}))
+
+
+def test_suggestions_highlight():
+    """suggestions should not highlight correct words"""
+    req = '/en/search?q=right worng'
+    request = test_utils.RequestFactory().get(req)
+    q = 'right worng'
+    t = "{{ q|suggestions('en-US') }}"
+    exp = '<a href="/en/search?q=right+wrong">right <strong>wrong</strong></a>'
+    eq_(exp, render(t, {'q': q, 'request': request}))
