5ed781e28172916617669083978ca465d033c8d8
James Socol <james@mozilla.com>
2010-03-05 09:33:31 -0800

    Moving the spelling suggestion filters and tests into the search app for cleanliness.

--- apps/search/tests.py
+++
@@ -1,7 +0,0 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
