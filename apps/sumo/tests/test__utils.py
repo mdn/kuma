32be0335fd22876379e9716e0df88ffc953fc151
James Socol <james@mozilla.com>
2010-04-21 11:56:06 -0700

    Making sure not to switch to the wrong encoding at any point.

--- apps/sumo/tests/test__utils.py
+++ apps/sumo/tests/test__utils.py
@@ -6,12 +6,15 @@ from sumo.utils import urlencode
 
 
 def test_urlencode():
-    """Our urlencode is UTF-8-safe."""
-    items = [('q', u'Fran\xc3\xa7ais')]
+    """Our urlencode is Unicode-safe."""
+    items = [('q', u'Fran\xe7ais')]
     eq_('q=Fran%C3%A7ais', urlencode(items))
+
+    items = [('q', u'は「着')]
+    eq_('q=%E3%81%AF%E3%80%8C%E7%9D%80', urlencode(items))
 
 
 def test_urlencode_int():
-    """urlencode() should not try to encode integers."""
+    """urlencode() should not choke on integers."""
     items = [('q', 't'), ('a', 1)]
     eq_('q=t&a=1', urlencode(items))
