b2563c4dffcf2515d23f7f861600fc01cb0578da
Paul Craciunoiu <paul.craciunoiu@gmail.com>
2010-04-22 17:44:03 -0700

    Adding ast and gd to settings.py ... and reindenting.

--- settings.py
+++ settings.py
@@ -57,11 +57,11 @@ LANGUAGE_CODE = 'en-US'
 
 # Supported languages
 SUMO_LANGUAGES = (
-    'ar', 'as', 'bg', 'bn-BD', 'bn-IN', 'ca', 'cs', 'da', 'de', 'el', 'en-US',
-    'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fr', 'fur', 'fy-NL', 'ga-IE', 'gl',
-    'gu-IN', 'he', 'hi-IN', 'hr', 'hu', 'id', 'is', 'it', 'ja', 'kk', 'kn',
-    'ko', 'lt', 'mk', 'mn', 'mr', 'ms', 'nb-NO', 'nl', 'no', 'oc', 'pa-IN',
-    'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'rw', 'si', 'sk', 'sl',
+    'ar', 'as', 'ast', 'bg', 'bn-BD', 'bn-IN', 'ca', 'cs', 'da', 'de', 'el',
+    'en-US', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fr', 'fur', 'fy-NL', 'ga-IE',
+    'gd', 'gl', 'gu-IN', 'he', 'hi-IN', 'hr', 'hu', 'id', 'is', 'it', 'ja',
+    'kk', 'kn', 'ko', 'lt', 'mk', 'mn', 'mr', 'ms', 'nb-NO', 'nl', 'no', 'oc',
+    'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'rw', 'si', 'sk', 'sl',
     'sq', 'sr-CYRL', 'sr-LATN', 'sv-SE', 'ta-LK', 'te', 'th', 'tr',
     'uk', 'vi', 'zh-CN', 'zh-TW',
 )
