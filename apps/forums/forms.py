2320697b26d33a32ff7305550903c69d387bb9a7
Joshua Gross <josh@unwieldy.net>
2010-05-24 09:48:42 -0700

    Limit length of forum post bodies (bug 567475)

--- apps/forums/forms.py
+++ apps/forums/forms.py
@@ -5,6 +5,10 @@ from .models import Post
 
 class ReplyForm(forms.ModelForm):
     """Reply form for forum threads."""
+    content = forms.CharField(
+                min_length=5,
+                max_length=10000,
+                widget=forms.Textarea(attrs={'rows': 10, 'cols': 80}))
 
     class Meta:
         model = Post
@@ -16,4 +20,6 @@ class NewThreadForm(forms.Form):
     title = forms.CharField(min_length=5, max_length=255,
                             widget=forms.TextInput(attrs={'size': 80}))
     content = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 30, 'cols': 76}))
+                min_length=5,
+                max_length=10000,
+                widget=forms.Textarea(attrs={'rows': 30, 'cols': 76}))
