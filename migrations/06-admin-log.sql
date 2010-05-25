6400e256905ee577bef69a4d5de8e292ed03fe1f
James Socol <james@mozilla.com>
2010-05-25 14:25:48 -0700

    Adding the migration to create the admin log table (bug 568067)

---
+++ migrations/06-admin-log.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `django_admin_log` (
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `action_time` datetime NOT NULL,
+    `user_id` integer NOT NULL,
+    `content_type_id` integer,
+    `object_id` longtext,
+    `object_repr` varchar(200) NOT NULL,
+    `action_flag` smallint UNSIGNED NOT NULL,
+    `change_message` longtext NOT NULL
+)
+;
+ALTER TABLE `django_admin_log` ADD CONSTRAINT `content_type_id_refs_id_288599e6` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`);
+ALTER TABLE `django_admin_log` ADD CONSTRAINT `user_id_refs_id_c8665aa` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+CREATE INDEX `django_admin_log_user_id` ON `django_admin_log` (`user_id`);
+CREATE INDEX `django_admin_log_content_type_id` ON `django_admin_log` (`content_type_id`);
+COMMIT;
