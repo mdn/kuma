bbfa4f86ece76df22d5808732a921c1bb799e381
James Socol <james@mozilla.com>
2010-05-28 20:13:53 -0700

    Adding a docstring to EventWatch.

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -5,6 +5,11 @@ from sumo.models import ModelBase
 
 
 class EventWatch(ModelBase):
+    """
+    Allows anyone to watch a specific item for changes. Uses email instead of
+    user ID so anonymous visitors can also watch things eventually.
+    """
+
     content_type = models.ForeignKey(ContentType)
     watch_id = models.IntegerField(db_index=True)
     email = models.EmailField(db_index=True)
