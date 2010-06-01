4f26a61e0cdb9c5e507e6bba9f21a0b2a3bc2a7f
Paul Craciunoiu <paul.craciunoiu@gmail.com>
2010-06-01 15:42:53 -0700

    Adding Iloko [ilo] locale.

--- settings.py
+++ settings.py
@@ -61,11 +61,11 @@ LANGUAGE_CODE = 'en-US'
 SUMO_LANGUAGES = (
     'ar', 'as', 'ast', 'bg', 'bn-BD', 'bn-IN', 'bs', 'ca', 'cs', 'da', 'de',
     'el', 'en-US', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fr', 'fur', 'fy-NL',
-    'ga-IE', 'gd', 'gl', 'gu-IN', 'he', 'hi-IN', 'hr', 'hu', 'id', 'is', 'it',
-    'ja', 'kk', 'kn', 'ko', 'lt', 'mk', 'mn', 'mr', 'ms', 'nb-NO', 'nl', 'no',
-    'oc', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'rw', 'si', 'sk',
-    'sl', 'sq', 'sr-CYRL', 'sr-LATN', 'sv-SE', 'ta-LK', 'te', 'th', 'tr',
-    'uk', 'vi', 'zh-CN', 'zh-TW',
+    'ga-IE', 'gd', 'gl', 'gu-IN', 'he', 'hi-IN', 'hr', 'hu', 'id', 'ilo',
+    'is', 'it', 'ja', 'kk', 'kn', 'ko', 'lt', 'mk', 'mn', 'mr', 'ms', 'nb-NO',
+    'nl', 'no', 'oc', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'rw',
+    'si', 'sk', 'sl', 'sq', 'sr-CYRL', 'sr-LATN', 'sv-SE', 'ta-LK', 'te',
+    'th', 'tr', 'uk', 'vi', 'zh-CN', 'zh-TW',
 )
 
 LANGUAGES = dict([(i.lower(), LOCALES[i].native)
