d460b69f70f455efb755c349b38b3ec8480d1455
Ricky Rosario <rickyrosario@gmail.com>
2010-06-04 14:16:31 -0400

    Only render dir attribute if the variable is set (bug 567929)

--- apps/sumo/tests/test_templates.py
+++ apps/sumo/tests/test_templates.py
@@ -1,9 +1,11 @@
-import pyquery
-
 from django.test.client import Client
+from django.test import TestCase
+from django.http import HttpRequest
 
+from nose.tools import eq_, raises
+from pyquery import PyQuery as pq
 import jingo
-from nose.tools import eq_
+import test_utils
 
 from sumo.urlresolvers import reverse
 
@@ -18,6 +20,33 @@ def test_breadcrumb():
     c = Client()
     response = c.get(reverse('search'))
 
-    doc = pyquery.PyQuery(response.content)
+    doc = pq(response.content)
     href = doc('.breadcrumbs a')[0]
     eq_('/', href.attrib['href'][0])
+
+
+class TestBaseTemplate(TestCase):
+    """Tests for layout/base.html"""
+
+    def setUp(self):
+        super(TestBaseTemplate, self).setUp()
+
+        request = test_utils.RequestFactory()
+        request.locale = 'en-US'
+        self.request = request
+        self.template = 'layout/base.html'
+
+    @raises(KeyError)
+    def test_no_dir_attribute(self):
+        """Make sure dir attr isn't rendered when no dir is specified."""
+        html = jingo.render_to_string(self.request, self.template)
+        doc = pq(html)
+        doc('html')[0].attrib['dir']
+
+    def test_rtl_dir_attribute(self):
+        """Make sure dir attr is set to 'rtl' when specified as so."""
+        html = jingo.render_to_string(self.request, self.template,
+                                      {'dir': 'rtl'})
+        doc = pq(html)
+        dir_attr = doc('html').attr['dir']
+        eq_('rtl', dir_attr)
