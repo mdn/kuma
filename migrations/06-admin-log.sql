572ed80ae31291cd652f6565bdf304c4a5d6c3c9
James Socol <james@mozilla.com>
2010-06-07 17:51:21 -0700

    Fixing up our migrations to match reality and run with no issues.

--- migrations/06-admin-log.sql
+++ migrations/06-admin-log.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `django_admin_log` (
     `object_repr` varchar(200) NOT NULL,
     `action_flag` smallint UNSIGNED NOT NULL,
     `change_message` longtext NOT NULL
-)
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci
 ;
 ALTER TABLE `django_admin_log` ADD CONSTRAINT `content_type_id_refs_id_288599e6` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`);
 ALTER TABLE `django_admin_log` ADD CONSTRAINT `user_id_refs_id_c8665aa` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
