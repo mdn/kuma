7bbcc1de64542e7e8975e66b10bee416658038dc
Paul Craciunoiu <paul.craciunoiu@gmail.com>
2010-06-10 16:29:17 -0700

    Add better formatted error messages for the forums forms.

--- apps/forums/forms.py
+++ apps/forums/forms.py
@@ -5,8 +5,12 @@ from tower import ugettext_lazy as _lazy
 from .models import Thread, Post
 
 
-MSG_CONTENT = _lazy('Content must be longer than 5 characters.')
-MSG_TITLE = _lazy('Title must be longer than 5 characters.')
+MSG_TITLE_REQUIRED = _lazy(u'Please provide a title.')
+MSG_TITLE_SHORT = _lazy(u'Your title is too short (%(show_value)s characters). It must be at least %(limit_value)s characters.')
+MSG_TITLE_LONG = _lazy(u'Please keep the length of your title to %(show_value)s characters or less. It is currently %(limit_value)s characters.')
+MSG_CONTENT_REQUIRED = _lazy(u'Please provide a message.')
+MSG_CONTENT_SHORT = _lazy(u'Your message is too short (%(show_value)s characters). It must be at least %(limit_value)s characters.')
+MSG_CONTENT_LONG = _lazy(u'Please keep the length of your message to %(show_value)s characters or less. It is currently %(limit_value)s characters.')
 
 
 # TODO: remove this and use strip kwarg once ticket #6362 is done
@@ -25,8 +29,9 @@ class ReplyForm(forms.ModelForm):
                 min_length=5,
                 max_length=10000,
                 widget=forms.Textarea(attrs={'rows': 10, 'cols': 80}),
-                error_messages={'required': MSG_CONTENT,
-                                'min_length': MSG_CONTENT})
+                error_messages={'required': MSG_CONTENT_REQUIRED,
+                                'min_length': MSG_CONTENT_SHORT,
+                                'max_length': MSG_CONTENT_LONG})
 
     class Meta:
         model = Post
@@ -37,21 +42,25 @@ class NewThreadForm(forms.Form):
     """Form to start a new thread."""
     title = StrippedCharField(min_length=5, max_length=255,
                               widget=forms.TextInput(attrs={'size': 80}),
-                              error_messages={'required': MSG_TITLE,
-                                              'min_length': MSG_CONTENT})
+                              error_messages={'required': MSG_TITLE_REQUIRED,
+                                              'min_length': MSG_TITLE_SHORT,
+                                              'max_length': MSG_TITLE_LONG})
     content = StrippedCharField(
                 min_length=5,
                 max_length=10000,
                 widget=forms.Textarea(attrs={'rows': 30, 'cols': 76}),
-                error_messages={'required': MSG_CONTENT,
-                                'min_length': MSG_CONTENT})
+                error_messages={'required': MSG_CONTENT_REQUIRED,
+                                'min_length': MSG_CONTENT_SHORT,
+                                'max_length': MSG_CONTENT_LONG})
 
 
 class EditThreadForm(forms.ModelForm):
     """Form to start a new thread."""
     title = StrippedCharField(min_length=5, max_length=255,
                               widget=forms.TextInput(attrs={'size': 80}),
-                              error_messages={'required': MSG_TITLE})
+                              error_messages={'required': MSG_TITLE_REQUIRED,
+                                              'min_length': MSG_TITLE_SHORT,
+                                              'max_length': MSG_TITLE_LONG})
 
     class Meta:
         model = Thread
@@ -64,8 +73,9 @@ class EditPostForm(forms.Form):
             min_length=5,
             max_length=10000,
             widget=forms.Textarea(attrs={'rows': 30, 'cols': 76}),
-            error_messages={'required': MSG_CONTENT,
-                            'min_length': MSG_CONTENT})
+            error_messages={'required': MSG_CONTENT_REQUIRED,
+                            'min_length': MSG_CONTENT_SHORT,
+                            'max_length': MSG_CONTENT_LONG})
 
     class Meta:
         model = Post
