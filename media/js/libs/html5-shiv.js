4a7b51fa0e75e16fc850d25025d5c8a0b1904abe
Ricky Rosario <rickyrosario@gmail.com>
2010-07-12 11:22:13 -0400

    html5 shiv for IE (bug 576720)

---
+++ media/js/libs/html5-shiv.js
@@ -0,0 +1,2 @@
+// For discussion and comments, see: http://remysharp.com/2009/01/07/html5-enabling-script/ (credit to @jdalton for minif)
+/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/
