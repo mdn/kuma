917ad7d85679a4f2a3ebb29291f4fb797211dfa1
Ricky Rosario <rickyrosario@gmail.com>
2010-07-27 11:43:57 -0400

    Don't assume that CACHE_PREFIX is defined in settings

--- apps/flagit/tests/__init__.py
+++ apps/flagit/tests/__init__.py
@@ -22,8 +22,9 @@ class TestCaseBase(TestCase):
         self.client.get('/')
 
         # Change the CACHE_PREFIX to avoid conflicts
-        self.orig_cache_prefix = settings.CACHE_PREFIX
-        settings.CACHE_PREFIX += 'stage' + slugify(datetime.now())
+        self.orig_cache_prefix = getattr(settings, 'CACHE_PREFIX', None)
+        settings.CACHE_PREFIX = self.orig_cache_prefix or '' + 'test' + \
+                                slugify(datetime.now())
 
     def tearDown(self):
         settings.CACHE_PREFIX = self.orig_cache_prefix
