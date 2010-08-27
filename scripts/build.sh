ef0e846686578cbcca11f0c7dd845353829455ad
Ricky Rosario <rickyrosario@gmail.com>
2010-08-27 15:58:02 -0400

    [bug 591417] Add parameter to build.sh to avoid db name collisions

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,12 @@ VENV=$WORKSPACE/venv
 
 echo "Starting build..."
 
+if [ -z $1 ]; then
+    echo "Warning: You should provide a unique name for this job to prevent database collisions."
+    echo "Usage: ./build.sh <name>"
+    echo "Continuing, but don't say you weren't warned."
+fi
+
 # Clean up after last time.
 find . -name '*.pyc' -exec rm {} \;
 
@@ -24,6 +30,8 @@ cat > settings_local.py <<SETTINGS
 from settings import *
 ROOT_URLCONF = 'workspace.urls'
 LOG_LEVEL = logging.ERROR
+DATABASES['default']['NAME'] = 'kitsune_$1'
+DATABASES['default']['TEST_NAME'] = 'test_kitsune_$1'
 DATABASES['default']['TEST_CHARSET'] = 'utf8'
 DATABASES['default']['TEST_COLLATION'] = 'utf8_general_ci'
 SETTINGS
