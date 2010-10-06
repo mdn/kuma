404a97fae73165e06b4c7b5cd79dcc20e8f901b8
Erik Rose <erik@mozilla.com>
2010-10-05 17:05:14 -0700

    Add disclosers for the sections on the Edit Article page.
    Use HTML 5 <details> and <summary> tags with jQuery/CSS fallback.

--- media/js/wiki.js
+++ media/js/wiki.js
@@ -11,6 +11,8 @@
 
         initPrepopulatedSlugs();
         initActionModals();
+        initDetailsTags();
+
         if ($('body').is('.document')) { // Document page
             initForTags();
         }
@@ -22,6 +24,74 @@
         }
 
         Marky.createFullToolbar('.forum-editor-tools', '#id_content');
+    }
+
+    // Make <details> tags work even if the browser doesn't support them.
+    // From http://mathiasbynens.be/notes/html5-details-jquery
+    function initDetailsTags() {
+        // Note <details> tag support. Modernizr doesn't do this properly as of 1.5; it thinks Firefox 4 can do it, even though the tag has no "open" attr.
+        if (!('open' in document.createElement('details')))
+            document.documentElement.className += ' no-details';
+
+        // Execute the fallback only if there’s no native `details` support
+        if (!('open' in document.createElement('details'))) {
+            // Loop through all `details` elements
+            $('details').each(function() {
+                // Store a reference to the current `details` element in a variable
+                var $details = $(this),
+                    // Store a reference to the `summary` element of the current `details` element (if any) in a variable
+                    $detailsSummary = $('summary', $details),
+                    // Do the same for the info within the `details` element
+                    $detailsNotSummary = $details.children(':not(summary)'),
+                    // This will be used later to look for direct child text nodes
+                    $detailsNotSummaryContents = $details.contents(':not(summary)');
+
+                // If there is no `summary` in the current `details` element…
+                if (!$detailsSummary.length) {
+                    // …create one with default text
+                    $detailsSummary = $(document.createElement('summary')).text('Details').prependTo($details);
+                }
+
+                // Look for direct child text nodes
+                if ($detailsNotSummary.length !== $detailsNotSummaryContents.length) {
+                    // Wrap child text nodes in a `span` element
+                    $detailsNotSummaryContents.filter(function() {
+                        // Only keep the node in the collection if it’s a text node containing more than only whitespace
+                        return (this.nodeType === 3) && (/[^\t\n\r ]/.test(this.data));
+                    }).wrap('<span>');
+                    // There are now no direct child text nodes anymore — they’re wrapped in `span` elements
+                    $detailsNotSummary = $details.children(':not(summary)');
+                }
+
+                // Hide content unless there’s an `open` attribute
+                if (typeof $details.attr('open') !== 'undefined') {
+                    $details.addClass('open');
+                    $detailsNotSummary.show();
+                } else {
+                    $detailsNotSummary.hide();
+                }
+
+                // Set the `tabindex` attribute of the `summary` element to 0 to make it keyboard accessible
+                $detailsSummary.attr('tabindex', 0).click(function() {
+                    // Focus on the `summary` element
+                    $detailsSummary.focus();
+                    // Toggle the `open` attribute of the `details` element
+                    typeof $details.attr('open') !== 'undefined' ? $details.removeAttr('open') : $details.attr('open', 'open');
+                    // Toggle the additional information in the `details` element
+                    $detailsNotSummary.toggle(0);
+                    $details.toggleClass('open');
+                }).keyup(function(event) {
+                    if (13 === event.keyCode || 32 === event.keyCode) {
+                        // Enter or Space is pressed — trigger the `click` event on the `summary` element
+                        // Opera already seems to trigger the `click` event when Enter is pressed
+                        if (!($.browser.opera && 13 === event.keyCode)) {
+                            event.preventDefault();
+                            $detailsSummary.click();
+                        }
+                    }
+                });
+            });
+        }
     }
 
     // Return the OS that the cookie indicates or, failing that, that appears
