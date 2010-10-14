2860f1c71f598c68cb5b5a906cdc92220b53e64d
Ricky Rosario <rickyrosario@gmail.com>
2010-10-14 17:05:35 -0400

    [bug 604439] Stylized the revision history page. * Refactored the vertical tabs into a macro and added them to appropriate pages

--- apps/notifications/helpers.py
+++ apps/notifications/helpers.py
@@ -6,7 +6,9 @@ from . import check_watch
 
 @register.function
 @jinja2.contextfunction
-def is_watching(context, obj):
-    if not hasattr(context['request'].user, 'email'):
+def is_watching(context, obj, user=None):
+    if not user:
+        user = context['request'].user
+    if not hasattr(user, 'email'):
         return False
-    return check_watch(obj.__class__, obj.pk, context['request'].user.email)
+    return check_watch(obj.__class__, obj.pk, user.email)
