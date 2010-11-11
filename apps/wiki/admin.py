fabc3ed13393020c86075724646470334c25b7a7
James Socol <james@mozilla.com>
2010-11-11 18:36:01 -0500

    Alphabetize and add to wiki.admin.

--- apps/wiki/admin.py
+++ apps/wiki/admin.py
@@ -4,11 +4,12 @@ from wiki.models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
-    list_filter = ('is_template', 'is_localizable', 'category', 'locale')
     list_display = ('locale', 'title', 'category', 'is_localizable')
     list_display_links = ('title',)
+    list_filter = ('is_template', 'is_localizable', 'category', 'locale')
     raw_id_fields = ('parent',)
     readonly_fields = ('id', 'current_revision')
+    search_fields = ('title',)
 
 
 admin.site.register(Document, DocumentAdmin)
