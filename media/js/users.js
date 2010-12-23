936bace499524dae8ba10ea9eb81c893eeb3ca4f
Paul Craciunoiu <paul.craciunoiu@gmail.com>
2010-12-22 16:03:22 -0800

    [bug 617206] Revamped profile. One column layout, box for contact info.
    Also:
    * Add sidebar tabs to users app, for My profile/Edit my profile (for me) or display name (for other users)
    * Fix sidebar tab shadow when active tab is a link (hopefully with no regressions).

--- media/js/users.js
+++ media/js/users.js
@@ -13,7 +13,7 @@
         $emails.each(function () {
             var email_val = $(this).text();
             $a = $('<a/>').attr('href', 'mailto:' + email_val)
-                          .html($(this).html());
+                          .html(email_val);
             $(this).html($a);
         });
     }
