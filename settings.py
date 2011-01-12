72adb1f051abaf6d0cf40008ad4308313bd09bc7
Erik Rose <erik@mozilla.com>
2011-01-11 17:43:44 -0800

    Uncomment WEBTRENDS_* settings so tests pass even if they aren't defined in settings_local.py.

--- settings.py
+++ settings.py
@@ -570,10 +570,9 @@ NOTIFICATIONS_FROM_ADDRESS = 'notifications@support.mozilla.com'
 CHAT_SERVER = 'https://chat-support.mozilla.com:9091'
 CHAT_CACHE_KEY = 'sumo-chat-queue-status'
 
-#WEBTRENDS_WIKI_REPORT_URL = 'https://foo.com/v2_1/ReportService/
-#                             profiles/123456/reports/987654/?format=json'
-#WEBTRENDS_USER = r'someaccount\someperson'  # format: account\username
-#WEBTRENDS_PASSWORD = 'password'
+WEBTRENDS_WIKI_REPORT_URL = 'https://example.com/see_production.rst'
+WEBTRENDS_USER = r'someaccount\someusername'
+WEBTRENDS_PASSWORD = 'password'
 WEBTRENDS_EPOCH = date(2010, 8, 1)  # When WebTrends started gathering stats on
                                     # the KB
 WEBTRENDS_REALM = 'Webtrends Basic Authentication'
