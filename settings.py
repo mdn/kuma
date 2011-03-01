4a6f6344bce82b145b3630f82a272d73792c24f8
groovecoder <luke.crouch@gmail.com>
2011-03-01 14:29:43 -0600

    default database name

--- settings.py
+++ settings.py
@@ -31,7 +31,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'kitsune',  # Or path to database file if using sqlite3.
+        'NAME': 'kuma',  # Or path to database file if using sqlite3.
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
