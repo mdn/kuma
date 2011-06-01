0c6ded612759bb16b03633ea44c0aae8bd91df1c
Leslie Michael Orchard <l.m.orchard@pobox.com>
2011-06-01 14:15:49 -0400

    Forgot to change the home template to use pagination; tweak page size from 24 to 15 so it fits better on the page and triggers pagination earlier

--- apps/demos/views.py
+++ apps/demos/views.py
@@ -42,7 +42,7 @@ from threadedcomments.forms import ThreadedCommentForm
 from utils import JingoTemplateLoader
 template_loader = JingoTemplateLoader()
 
-DEMOS_PAGE_SIZE = getattr(settings, 'DEMOS_PAGE_SIZE', 24)
+DEMOS_PAGE_SIZE = getattr(settings, 'DEMOS_PAGE_SIZE', 15)
 DEMOS_LAST_NEW_COMMENT_ID = 'demos_last_new_comment_id'
 
 # bug 657779: migrated from plain tags to tech:* tags for these:
