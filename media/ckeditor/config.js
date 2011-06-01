70a6341d496220b6a4bc0a869dccf86c28b5499a
Craig Cook <craigcook@gmail.com>
2011-06-01 14:52:44 -0700

    Merge to mdn branch of "Kuma wiki templates, CKEditor skin, misc. CSS updates, Bebas Neue"

---
+++ media/ckeditor/config.js
@@ -0,0 +1,19 @@
+/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
+    config.autoGrow_minHeight = 300;
+    config.contentsCss = './css/wiki-edcontent.css'; 
+    config.toolbarCanCollapse = false;
+    config.resize_enabled = false;
+    config.dialog_backgroundCoverColor = 'black';
+    config.dialog_backgroundCoverOpacity = 0.3;
+    config.docType = '<!DOCTYPE html>';
+};
