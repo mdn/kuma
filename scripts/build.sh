b669d7ea7708be7304b996c45f3449b6467e1509
groovecoder <luke.crouch@gmail.com>
2011-06-01 11:57:19 -0500

    bug 656285 use project-specific db

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,55 +1,56 @@
 # This script should be called from within Hudson
+DB = $JOB_NAME
 cd $WORKSPACE
 VENV=$WORKSPACE/venv
 VENDOR=$WORKSPACE/vendor
 LOCALE=$WORKSPACE/locale
 
 echo "Starting build on executor $EXECUTOR_NUMBER..." `date`
-
 echo "Setup..." `date`
 
 # Make sure there's no old pyc files around.
 find . -name '*.pyc' | xargs rm
 
+# virtualenv - create if necessary
 if [ ! -d "$VENV/bin" ]; then
   echo "No virtualenv found.  Making one..."
   virtualenv $VENV
 fi
-
 echo "Activating $VENV"
 source $VENV/bin/activate
 
 pip install -q -r requirements/compiled.txt
 
-# Create paths we want for addons
+# locale - create if necessary
 if [ ! -d "$LOCALE" ]; then
     echo "No locale dir?  Cloning..."
     svn co http://svn.mozilla.org/projects/mdn/trunk/locale $LOCALE
-    #git clone --recursive git://github.com/fwenzel/mdn-locales.git $LOCALE
 fi
 
+# vendor - create if necessary
 if [ ! -d "$VENDOR" ]; then
     echo "No vendor lib?  Cloning..."
     git clone --recursive git://github.com/mozilla/kuma-lib.git $VENDOR
 fi
-
 # Update the vendor lib.
 echo "Updating vendor..."
 pushd $VENDOR && git pull origin master && git submodule sync && git submodule update --init;
 popd
 
+# product details - create if necessary
 if [ ! -d "$WORKSPACE/../product_details_json" ]; then
     mkdir $WORKSPACE/../product_details_json
 fi
 python manage.py update_product_details
 
+# create settings_local.py
 cat > settings_local.py <<SETTINGS
 from settings import *
 ROOT_URLCONF = '%s.urls' % ROOT_PACKAGE
 LOG_LEVEL = logging.ERROR
 DATABASES['default']['HOST'] = 'sm-hudson01'
 DATABASES['default']['USER'] = 'hudson'
-DATABASES['default']['TEST_NAME'] = 'test_kuma'
+DATABASES['default']['TEST_NAME'] = 'test_$DB'
 DATABASES['default']['TEST_CHARSET'] = 'utf8'
 DATABASES['default']['TEST_COLLATION'] = 'utf8_general_ci'
 CACHE_BACKEND = 'caching.backends.locmem://'
