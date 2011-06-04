73321fa127f1723fb54cdbc4022170b7b52991a8
groovecoder <luke.crouch@gmail.com>
2011-06-03 20:56:58 -0500

    bug 658274 start view tests with submit view GET

---
+++ apps/demos/test_views.py
@@ -0,0 +1,27 @@
+from django import http, test
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+from mock import patch
+from nose.tools import eq_
+from pyquery import PyQuery as pq
+import test_utils
+
+class DemoViewsTest(test_utils.TestCase):
+    fixtures = ['apps/demos/fixtures/test_users.json']
+
+    def test_submit_loggedout(self):
+        r = self.client.get(reverse('demos_submit'), follow=True)
+        choices = pq(r.content)('p.choices')
+        eq_(choices.find('a.button').length, 2)
+
+    # TODO: extract into a DevmoTestCase class and/or a wrapper?
+    @patch('dekicompat.backends.DekiUserBackend.authenticate')
+    @patch('dekicompat.backends.DekiUserBackend.get_user')
+    def test_submit_loggedin(self, authenticate, get_user):
+        authenticate.return_value = User.objects.get(username='testuser')
+        get_user.return_value = User.objects.get(username='testuser')
+        self.client.login(authtoken='1')
+        r = self.client.get(reverse('demos_submit'), follow=True)
+        assert pq(r.content)('form#demo-submit')
