e82c6c591494ca174fdffeedb539677111c045c3
groovecoder <luke.crouch@gmail.com>
2011-06-06 14:16:39 -0500

    bug 658274 mockdekiauth decorator and submit tests

--- apps/demos/test_views.py
+++ apps/demos/test_views.py
@@ -8,6 +8,18 @@ from nose.tools import eq_
 from pyquery import PyQuery as pq
 import test_utils
 
+
+def mockdekiauth(test):
+    @patch('dekicompat.backends.DekiUserBackend.authenticate')
+    @patch('dekicompat.backends.DekiUserBackend.get_user')
+    def test_new(self, authenticate, get_user):
+        authenticate.return_value = self.testuser
+        get_user.return_value = self.testuser
+        self.client.login(authtoken='1')
+        test(self)
+    return test_new
+
+
 class DemoViewsTest(test_utils.TestCase):
     fixtures = ['test_users.json']
 
@@ -19,12 +31,20 @@ class DemoViewsTest(test_utils.TestCase):
         choices = pq(r.content)('p.choices')
         eq_(choices.find('a.button').length, 2)
 
-    # TODO: extract into a DevmoTestCase class and/or a wrapper?
-    @patch('dekicompat.backends.DekiUserBackend.authenticate')
-    @patch('dekicompat.backends.DekiUserBackend.get_user')
-    def test_submit_loggedin(self, authenticate, get_user):
-        authenticate.return_value = self.testuser
-        get_user.return_value = self.testuser
-        self.client.login(authtoken='1')
+    @mockdekiauth
+    def test_submit_loggedin(self):
         r = self.client.get(reverse('demos_submit'), follow=True)
         assert pq(r.content)('form#demo-submit')
+
+    @mockdekiauth
+    def test_submit_post_invalid(self):
+        r = self.client.post('/en-US/demos/submit', data={}, follow=True)
+        d = pq(r.content)
+        assert d('form#demo-submit')
+        assert d('li#field_title ul.errorlist')
+        assert d('li#field_summary ul.errorlist')
+        assert d('li#field_screenshot_1 ul.errorlist')
+        assert d('li#field_demo_package ul.errorlist')
+        assert d('li#field_license_name ul.errorlist')
+        assert d('li#field_captcha ul.errorlist')
+        assert d('li#field_accept_terms ul.errorlist')
