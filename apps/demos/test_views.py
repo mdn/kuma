4dc8e6f685b6ebc3fdee32821cce7702a0ec637f
groovecoder <luke.crouch@gmail.com>
2011-06-08 14:33:15 -0500

    bug 658274 more demos views tests; debug build script?

--- apps/demos/test_views.py
+++ apps/demos/test_views.py
@@ -1,7 +1,9 @@
 from django import http, test
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+
+from sumo.urlresolvers import reverse
+from sumo.tests import LocalizingClient
 
 from mock import patch
 from nose.tools import eq_
@@ -27,20 +29,21 @@ class DemoViewsTest(test_utils.TestCase):
 
     def setUp(self):
         self.testuser = User.objects.get(username='testuser')
+        self.client = LocalizingClient()
 
     def test_submit_loggedout(self):
-        r = self.client.get(reverse('demos_submit'), follow=True)
+        r = self.client.get(reverse('demos_submit'))
         choices = pq(r.content)('p.choices')
         eq_(choices.find('a.button').length, 2)
 
     @mockdekiauth
     def test_submit_loggedin(self):
-        r = self.client.get(reverse('demos_submit'), follow=True)
+        r = self.client.get(reverse('demos_submit'))
         assert pq(r.content)('form#demo-submit')
 
     @mockdekiauth
     def test_submit_post_invalid(self):
-        r = self.client.post('/en-US/demos/submit', data={}, follow=True)
+        r = self.client.post('/en-US/demos/submit', data={})
         d = pq(r.content)
         assert d('form#demo-submit')
         assert d('li#field_title ul.errorlist')
@@ -55,8 +58,42 @@ class DemoViewsTest(test_utils.TestCase):
         s = save_valid_submission('hello world')
 
         url = reverse('demos_detail', args=[s.slug])
-        r = self.client.get(url, follow=True)
+        r = self.client.get(url)
         d = pq(r.content)
         a = d('a[href="%s"]' % url)
         eq_(s.title, a.text())
         eq_(s.title, d('h1.page-title').text())
+        edit_link = d('ul.manage a.edit')
+        assert not edit_link
+
+    @mockdekiauth
+    def test_creator_can_edit(self):
+        s = save_valid_submission('hello world')
+
+        url = reverse('demos_detail', args=[s.slug])
+        r = self.client.get(url)
+        d = pq(r.content)
+        edit_link = d('ul.manage a.edit')
+        assert edit_link
+        edit_url = reverse('demos_edit', args=[s.slug], locale='en-US')
+        eq_(edit_url, edit_link.attr("href"))
+
+        r = self.client.get(edit_url)
+        assert pq(r.content)('form#demo-submit')
+        eq_('Save changes', pq(r.content)('p.fm-submit button[type="submit"]').text())
+
+    @mockdekiauth
+    def test_hidden_field(self):
+        s = save_valid_submission('hello world')
+
+        edit_url = reverse('demos_edit', args=[s.slug], locale='en-US')
+        r = self.client.get(edit_url)
+        assert pq(r.content)('input[name="hidden"][type="checkbox"]')
+
+    @mockdekiauth
+    def test_derby_field(self):
+        s = save_valid_submission('hello world')
+
+        edit_url = reverse('demos_edit', args=[s.slug], locale='en-US')
+        r = self.client.get(edit_url)
+        assert pq(r.content)('fieldset#devderby-submit')
