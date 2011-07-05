29b228f0c0754e82555bee88bd81d449581a56c0
groovecoder <luke.crouch@gmail.com>
2011-07-05 12:42:52 -0500

    bug 664330 use getattr for both DEV and STAGE

--- settings.py
+++ settings.py
@@ -115,10 +115,7 @@ PROD_LANGUAGES = MDN_LANGUAGES
 
 def lazy_lang_url_map():
     from django.conf import settings
-    try:
-        langs = DEV_LANGUAGES if (settings.DEV or settings.STAGE) else PROD_LANGUAGES
-    except AttributeError:
-        langs = PROD_LANGUAGES
+    langs = DEV_LANGUAGES if (getattr(settings, 'DEV', False) or getattr(settings, 'STAGE', False)) else PROD_LANGUAGES
     lang_url_map = dict([(i.lower(), i) for i in langs])
     lang_url_map['pt'] = 'pt-PT'
     return lang_url_map
@@ -129,10 +126,7 @@ LANGUAGE_URL_MAP = lazy(lazy_lang_url_map, dict)()
 def lazy_langs():
     from django.conf import settings
     from product_details import product_details
-    try:
-        langs = DEV_LANGUAGES if (settings.DEV or settings.STAGE) else PROD_LANGUAGES
-    except AttributeError:
-        langs = PROD_LANGUAGES
+    langs = DEV_LANGUAGES if (getattr(settings, 'DEV', False) or getattr(settings, 'STAGE', False)) else PROD_LANGUAGES
     return dict([(lang.lower(), product_details.languages[lang]['native'])
                 for lang in langs])
 
@@ -142,10 +136,7 @@ LANGUAGE_CHOICES = tuple([(i, LOCALES[i].native) for i in MDN_LANGUAGES])
 # DEKI uses different locale keys
 def lazy_language_deki_map():
     from django.conf import settings
-    try:
-        langs = DEV_LANGUAGES if (settings.DEV or settings.STAGE) else PROD_LANGUAGES
-    except AttributeError:
-        langs = PROD_LANGUAGES
+    langs = DEV_LANGUAGES if (getattr(settings, 'DEV', False) or getattr(settings, 'STAGE', False)) else PROD_LANGUAGES
     lang_deki_map = dict([(i, i) for i in langs])
     lang_deki_map['en-US'] = 'en'
     lang_deki_map['zh-CN'] = 'cn'
