37c83ebe66defb6c08326b04be5fc2599c022b36
groovecoder <luke.crouch@gmail.com>
2011-07-12 17:56:11 -0700

    bug 665128 cleanup & doc

--- media/ckeditor/plugins/mdn-buttons/plugin.js
+++ media/ckeditor/plugins/mdn-buttons/plugin.js
@@ -1,31 +1,10 @@
-// http://blog.lystor.org.ua/2010/11/ckeditor-plugin-and-toolbar-button-for.html
-/*
-(function(){
-    var a= {
-        exec:function(editor){
-            var format = {
-                element : "pre"
-            };
-            var style = new CKEDITOR.style(format);
-            style.apply(editor.document);
-        }
-    };
+// See: http://blog.lystor.org.ua/2010/11/ckeditor-plugin-and-toolbar-button-for.html
 
-    CKEDITOR.plugins.add("mdn-buttons",{
-        init:function(editor){
-            var pluginName = "mdn-buttons";
-            editor.addCommand(pluginName, a);
-            editor.ui.addButton(pluginName, {
-                label:"Button PRE",
-                icon: this.path + "button-pre.png",
-                command:b
-            });
-        }
-    });
-})();
-*/
+// the plugin automatically creates button commands for these tags
 CKEDITOR.config.mdnButtons_tags = ['code', 'pre', 'dl', 'dt', 'dd'];
+
 (function(){
+    // make a generic command object that wraps text in <tag>
     var tagCommand = function(tag) {
         var command = {
             exec: function(editor, data){
@@ -42,28 +21,18 @@ CKEDITOR.config.mdnButtons_tags = ['code', 'pre', 'dl', 'dt', 'dd'];
         init: function(editor) {
             var tags = CKEDITOR.config.mdnButtons_tags;
             var pluginName = "mdn-buttons";
+            // addCommand and addButton for each tag in the list
             for (var i = 0, j = tags.length; i < j; i++) {
                 var tag = tags[i];
                 var commandName = pluginName + '-' + tag;
                 var command = tagCommand(tag);
-//                alert('tag: ' + tag + ', commandName: ' + commandName);
-//                alert(command);
                 editor.addCommand(commandName, command);
                 editor.ui.addButton(tag+'Button', {
                     label: tag,
                     command: commandName,
                     icon: this.path + 'button-' + tag + '.png'
                 });
-//                alert('addButton('+tag+'Button)');
             }
-/*
-            editor.addCommand(pluginName, command);
-            editor.ui.addButton('MDNButton', {
-                label: 'pre',
-                command: pluginName,
-                icon: this.path + 'button-pre.png',
-            });
-*/
         }
     });
 })();
