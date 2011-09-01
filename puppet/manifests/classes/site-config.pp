e32469013cd5a123b83f14637d01f871c8b77303
Les Orchard <l.m.orchard@pobox.com>
2011-09-01 11:29:42 -0400

    vagrant: Need to make ~vagrant world-readable to make Deki work

--- puppet/manifests/classes/site-config.pp
+++ puppet/manifests/classes/site-config.pp
@@ -72,6 +72,9 @@ class mysql_config {
 }
 
 class kuma_config {
+    file { "/home/vagrant":
+        owner => "vagrant", group => "vagrant", mode => 0755;
+    }
     file { 
         [ "/home/vagrant/logs",
             "/home/vagrant/uploads",
