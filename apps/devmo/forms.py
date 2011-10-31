ed6dce63e6c6372974f86839f2b8d284f4818074
groovecoder <luke.crouch@gmail.com>
2011-10-31 12:26:40 -0500

    Bug 692239 locale and timezone on django user profile

--- apps/devmo/forms.py
+++ apps/devmo/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.conf import settings
-from django.contrib.auth import authenticate, forms as auth_forms
-from django.contrib.auth.models import User
 
 from tower import ugettext_lazy as _
-from taggit.utils import parse_tags, edit_string_for_tags
+from taggit.utils import parse_tags
 
 from devmo.models import UserProfile
 
@@ -35,8 +32,8 @@ class UserProfileEditForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
-        fields = ('fullname', 'title', 'organization', 'location', 'bio',
-                  'irc_nickname', 'interests')
+        fields = ('fullname', 'title', 'organization', 'location',
+                  'locale', 'timezone', 'bio', 'irc_nickname', 'interests')
 
     # Email is on the form, but is handled in the view separately
     email = forms.EmailField(label=_('Email'), required=True)
