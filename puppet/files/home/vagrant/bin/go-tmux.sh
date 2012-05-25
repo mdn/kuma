b31526d60cc6a7700ff670182452670364ec10dc
Les Orchard <me@lmorchard.com>
2012-05-25 12:50:41 -0400

    Add go-tmux.sh command to vagrant to launch django, node, and a spare shell

---
+++ puppet/files/home/vagrant/bin/go-tmux.sh
@@ -0,0 +1,18 @@
+SESSION=$USER
+
+tmux has-session -t $SESSION
+if [ $? -eq 0 ]; then
+    echo "Session $SESSION already exists. Attaching."
+    sleep 1
+    tmux attach -t $SESSION
+    exit 0;
+fi
+
+tmux new-session -d -s $SESSION
+
+tmux split-window -t $SESSION:0 -v -p 50 './manage.py runserver; /bin/bash'
+tmux swap-pane    -t $SESSION:0 -U
+tmux split-window -t $SESSION:0 -v -p 50 'node kumascript/run.js; /bin/bash'
+tmux select-pane  -t $SESSION:0.2
+ 
+tmux attach -t $SESSION
