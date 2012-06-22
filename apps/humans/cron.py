b96636705dd638e95e5e8b201cc45bd80c9e79d8
groovecoder <luke.crouch@gmail.com>
2012-06-21 22:20:31 -0500

    fix humans to work with new github api

--- apps/humans/cron.py
+++ apps/humans/cron.py
@@ -2,7 +2,8 @@ import cronjobs
 
 from humans.models import HumansTXT
 
+
 @cronjobs.register
 def humans_txt():
-    humans = HumansTXT() 
+    humans = HumansTXT()
     humans.generate_file()
--- apps/humans/models.py
+++ apps/humans/models.py
@@ -2,23 +2,25 @@ import json
 import urllib
 from django.conf import settings
 
-GITHUB_REPOS = "https://github.com/api/v2/json/repos/show/mozilla/kuma/contributors"
+GITHUB_REPOS = "https://api.github.com/repos/mozilla/kuma/contributors"
+
 
 class Human:
     def __init__(self):
         self.name = None
         self.website = None
 
+
 class HumansTXT:
     def generate_file(self):
         githubbers = self.get_github(json.load(urllib.urlopen(GITHUB_REPOS)))
-        
+
         target = open("%s/humans.txt" % settings.HUMANSTXT_ROOT, 'w')
 
         self.write_to_file(githubbers, target, "Contributors on Github")
 
         target.close()
-    
+
     def write_to_file(self, humans, target, message):
         target.write("%s \n" % message)
         for h in humans:
@@ -32,13 +34,13 @@ class HumansTXT:
             raw_data = json.load(urllib.urlopen(GITHUB_REPOS))
         else:
             raw_data = data
-            
+
         humans = []
-        for contributor in raw_data['contributors']:
+        for contributor in raw_data:
             human = Human()
             try:
                 human.name = contributor['name']
-            except: # Github doesn't have a name if profile isn't filled out
+            except:  # Github doesn't have a name if profile isn't filled out
                 human.name = contributor['login']
 
             try:
@@ -49,5 +51,3 @@ class HumansTXT:
             humans.append(human)
 
         return humans
-        
-      
