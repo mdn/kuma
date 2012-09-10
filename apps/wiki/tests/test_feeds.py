84b5b25bdb356f27a60b78efc80a0b4e547907b9
Les Orchard <me@lmorchard.com>
2012-09-10 14:49:29 -0400

    fix bug 785272: post-merge test bugfix

--- apps/wiki/tests/test_feeds.py
+++ apps/wiki/tests/test_feeds.py
@@ -108,7 +108,7 @@ class FeedTests(TestCaseBase):
             if "Edited" in desc_text:
                 ok_('<h3>Tag changes:</h3>' in desc_text)
                 ok_('<span class="diff_add" style="background-color: #afa; '
-                    'text-decoration: none;">"more",&nbsp;</span>'
+                    'text-decoration: none;">"more"<br />&nbsp;</span>'
                     in desc_text)
                 ok_('<h3>Review changes:</h3>' in desc_text)
                 ok_('<span class="diff_add" style="background-color: #afa; '
