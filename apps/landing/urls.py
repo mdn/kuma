f77321900bf6cb2ab888d71802f1decccfa298c6
groovecoder <luke.crouch@gmail.com>
2013-03-06 11:53:23 -0600

    waffle search view to ES

--- apps/landing/urls.py
+++ apps/landing/urls.py
@@ -5,8 +5,6 @@ urlpatterns = patterns('landing.views',
     url(r'^addons/?$', 'addons', name='addons'),
     url(r'^mozilla/?$', 'mozilla', name='mozilla'),
     url(r'^mobile/?$', 'mobile', name='mobile'),
-    # TODO: Enabled until switchover to self-hosted kuma wiki search. Waffle me?
-    url(r'^search/?$', 'search', name='search'),
     url(r'^web/?$', 'web', name='web'),
     url(r'^newsletter/?$', 'apps_newsletter', name='apps_newsletter'),
     url(r'^learn/?$', 'learn', name='learn'),
