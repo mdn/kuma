59adae2761782e64e53f728c7a29e585dab7b67f
tx2z <tx2z@caosmental.com>
2013-03-12 01:12:12 +0100

    fix bug 778464 - organice and clean code in wiki_ckeditor

--- media/js/wiki_ckeditor_translate.js
+++ media/js/wiki_ckeditor_translate.js
@@ -1,80 +1,94 @@
-(function ($) {
-
-  var $body = $("body"),
-      $window = $(window);
+(function($) {
 
   // Callback functions after CKE is ready
-  var setup_ckeditor = function () {
-  
-    var $appBoxes  = $(".approved .boxed"),
-        $head      = $("#article-head"),
-        $transdesc = $("#trans-description"),
-        $transcont = $("#content-fields"),
-        $tools     = $("div.cke_toolbox"),
-        $wikiArt   = $("#cke_wikiArticle"),
-        contentTop = $("#content").offset(),
-        transTop   = $transcont.offset(),
-        headHeight = $head.height(),
-        toolHeight = $tools.height(),
-        fixed = false;
-        
-    $("#content-fields .approved .boxed").css({ paddingTop: toolHeight+54});
-  
+  var setup_ckeditor = function() {
+
+    var $appBoxes = $(".approved .boxed"),
+      $head = $("#article-head"),
+      $transdesc = $("#trans-description"),
+      $transcont = $("#content-fields"),
+      $tools = $("div.cke_toolbox"),
+      $wikiArt = $("#cke_wikiArticle"),
+      contentTop = $("#content").offset(),
+      transTop = $transcont.offset(),
+      headHeight = $head.height(),
+      toolHeight = $tools.height(),
+      contentBottom = $(".page-meta").first().offset().top - 300,
+      fixed = false;
+
+    $("#content-fields .approved .boxed").css({
+      paddingTop: toolHeight + 50
+    });
+
     // Switch header and toolbar styles on scroll to keep them on screen
-    $window.scroll(function() {
-
-      var scrollTop = $window.scrollTop(),
-          transTop  = $transcont.offset();
-          
-    	if( scrollTop >= contentTop.top ) {
-        if( !fixed ) {
+    $(document).scroll(function() {
+      if ($(this).scrollTop() >= contentTop.top && $(this).scrollTop() < contentBottom) { // If top of the window is betwen top of #content and top of metadata (first .page-meta) blocks, the header is fixed
+        if (!fixed) {
           fixed = true;
-          $head.css({position:"fixed", top:19, width:"95%"});
-          $transdesc.css({ marginTop: headHeight });
+          $head.css({
+            position: 'fixed',
+            top: 19,
+            width: "95%"
+          });
+          $tools.css({
+            position: 'fixed',
+            top: headHeight + 28,
+            width: $("#cke_id_content").width() - 11
+          });
+          $("#cke_id_content").css({
+            marginTop: headHeight
+          });
+          $(".approved header").hide();
+          $(".localized header").hide();
+          $("#content-fields .approved .boxed").css({
+            paddingTop: toolHeight - 40
+          });
         }
-      } 
-      else {
-        if( fixed ) {
+      } else { // If not, header is relative
+        if (fixed) {
           fixed = false;
-          $head.css({position:"relative", top:"auto", width:"auto"});
-          $transdesc.css({ marginTop: 0 });
+          $head.css({
+            position: 'relative',
+            top: "auto",
+            width: "auto"
+          });
+          $tools.css({
+            position: 'relative',
+            top: "auto",
+            width: "auto"
+          });
+          $("#cke_id_content").css({
+            marginTop: 0
+          });
+          $(".approved header").show();
+          $(".localized header").show();
+          $("#content-fields .approved .boxed").css({
+            paddingTop: toolHeight + 50
+          });
         }
       }
-
-      if( scrollTop >= ( transTop.top - headHeight ) ) {
-        $tools.css({position:"fixed", top:headHeight+45, width:$("#cke_id_content").width()-10});
-        $transcont.addClass("fixed");
-        $("#content-fields header").css({ position: "fixed", top:headHeight });
-        $("td.cke_top").css({ height: toolHeight+38 });
-      }
-      else {
-        $tools.css({position:"relative", top:"auto", width:"auto"});
-        $transcont.removeClass("fixed");
-        $("#content-fields header").css({ position: "static", top:"auto" });
-        $("td.cke_top").css({ height: "auto" });
-      }
-
-      $("#cke_contents_id_content").css({ height: $appBoxes.height() });
 
       // remove the id_content required attribute
       $('#id_content').removeAttr("required");
 
     });
-    
-    $window.resize(function() { // Recalculate box width on resize
+
+    $(window).resize(function() { // Recalculate box width on resize
       if (fixed) {
-        $tools.css({ width: $wikiArt.width()-10 }); // Readjust toolbox to fit
+        $tools.css({
+          width: $wikiArt.width() - 10
+        }); // Readjust toolbox to fit
       }
     });
 
   };
 
-  $("#id_content").each(function () {
-      if (!$body.is(".edit.is-template")) {
-          $(this).ckeditor(setup_ckeditor, {
-              customConfig: "/docs/ckeditor_config.js"
-          });
-      }
+  $("#id_content").each(function() {
+    if (!$("body").is(".edit.is-template")) {
+      $(this).ckeditor(setup_ckeditor, {
+        customConfig: "/docs/ckeditor_config.js"
+      });
+    }
   });
 
 })(jQuery);
