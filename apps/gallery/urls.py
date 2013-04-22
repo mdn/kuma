67bae434e9f9d423830303d03ec621b3a53ff772
Berker Peksag <berker.peksag@gmail.com>
2013-04-22 11:42:59 +0300

    [Bug 856118] Part 1/2: Switch to RedirectView.

--- apps/gallery/urls.py
+++ apps/gallery/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import redirect_to
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('gallery.views',
-    url(r'^/$', redirect_to, {'url': 'images'}, name='gallery.home'),
+    url(r'^/$', RedirectView.as_view(url='images'), name='gallery.home'),
     url(r'^/async$', 'gallery_async', name='gallery.async'),
     url(r'^/(?P<media_type>\w+)s$', 'gallery', name='gallery.gallery'),
     url(r'^/(?P<media_type>\w+)s/search$', 'search', name='gallery.search'),
