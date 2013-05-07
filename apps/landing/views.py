c1767baf55af49ef57f477bac5f0832d2e4d4505
David Walsh <dwalsh@mozilla.com>
2013-05-07 14:23:47 -0500

    Fix bug 868104 - Update homepage content

--- apps/landing/views.py
+++ apps/landing/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render
 
 import constance.config
 from waffle.decorators import waffle_switch
+import waffle
 
 from devmo import (SECTION_USAGE, SECTION_ADDONS, SECTION_APPS, SECTION_MOBILE,
                    SECTION_WEB, SECTION_MOZILLA)
@@ -20,9 +21,14 @@ from landing.forms import SubscriptionForm
 def home(request):
     """Home page."""
 
+    template = 'landing/home.html'
     demos = (Submission.objects.all_sorted('upandcoming')
             .exclude(hidden=True))[:5]
-
+    if waffle.flag_is_active(request, 'new_homepage'):
+        template = 'landing/home_new.html'
+        demos = Submission.objects.filter(featured=True)\
+                    .exclude(hidden=True)\
+                    .order_by('-modified').all()[:1]
     tweets = []
     for section in SECTION_USAGE:
         tweets += Bundle.objects.recent_entries(section.twitter)[:2]
@@ -32,8 +38,10 @@ def home(request):
     for s in SECTION_USAGE:
         updates += Bundle.objects.recent_entries(s.updates)[:1]
 
-    return render(request, 'landing/home.html',
-                  {'demos': demos, 'updates': updates, 'tweets': tweets})
+    return render(request, template,
+                  {'demos': demos, 'updates': updates, 'tweets': tweets,
+                    'current_challenge_tag_name': 
+                    str(constance.config.DEMOS_DEVDERBY_CURRENT_CHALLENGE_TAG).strip()})
 
 
 def addons(request):
