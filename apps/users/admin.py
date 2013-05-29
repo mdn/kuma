c5e370e2c226897c3a9863749f259a3735d2989a
James Bennett <james@b-list.org>
2013-05-29 10:37:06 -0500

    Add some more use of raw_id_fields for user bans, and more filter options.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -6,8 +6,8 @@ from users.models import UserBan
 class UserBanAdmin(admin.ModelAdmin):
     fields = ('user', 'by', 'reason', 'is_active')
     list_display = ('user', 'by', 'reason')
-    list_filter = ('is_active',)
-    raw_id_fields = ('user',)
+    list_filter = ('is_active', 'by')
+    raw_id_fields = ('user', 'by')
     search_fields = ('user__username', 'reason')
 
 
