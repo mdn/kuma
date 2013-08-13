65377674e2ea39ed7eb606b3b721a822ccceb655
Les Orchard <me@lmorchard.com>
2013-08-13 12:32:29 -0400

    Vagrant bug fixes & tweaks
    * Added logoutput => true to Exec default, so the output of all commands
      is included in puppet output. Much noisier, but offers much better
      feedback about setup failures.
    
    * Workaround for permission issues in using npm install for kumascript
    
    * Update vendor to catch South 0.8.3 upgrade, to fix a migration failure

--- puppet/manifests/classes/nodejs.pp
+++ puppet/manifests/classes/nodejs.pp
@@ -9,13 +9,29 @@ class nodejs {
         ensure => link,
         require => Package['nodejs-dev']
     }
+
+    # HACK: npm seems to have a bug in this environment where it believes the
+    # active user is still root, even when it's  vagrant. So, it tries to
+    # access /root/.npm, which throws a file permission error. This dirty dirty
+    # hack applies a bandaid in exchange for exposing /root slightly
+    file { "/root":
+        ensure => directory,
+        owner => "root", group => "root", mode => 0666;
+    }
+    file { "/root/.npm":
+        ensure => directory,
+        owner => "root", group => "root", mode => 0777,
+        require => File["/root"];
+    }
+    
     exec { 'npm-install':
         cwd => "/vagrant/kumascript",
-        command => "/usr/bin/npm install",
+        user => 'vagrant',
+        command => "/usr/bin/npm install fibers",
         creates => "/vagrant/kumascript/node_modules/fibers",
         require => [
             Package["nodejs"], Package["nodejs-dev"], Package["npm"],
-            File["/usr/include/node"],
+            File["/usr/include/node"], File["/root/.npm"]
         ]
     }
 }
