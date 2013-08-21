2792d2075eb9e21f29f47d558fed9f055fb82659
David Walsh <dwalsh@mozilla.com>
2013-08-21 04:04:48 -0500

    Add menu component, provide functional settings wiki menu

--- media/redesign/js/wiki.js
+++ media/redesign/js/wiki.js
@@ -1,9 +1,13 @@
 (function($) {
 
+	// Settings menu
+	$('#settings-menu').mozMenu();
+
 	// New tag placeholder
 	// Has to be placed in ready call because the plugin is initialized in one
 	$.ready(function() {
 		$('.tagit-new input').attr('placeholder', gettext('New tag...'));
 	});
+
 
 })(jQuery);
