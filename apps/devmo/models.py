999cc85be2bbbe7fae130a2c00cecfb438cb2432
Sudheesh Singanamalla <SudheeshSinganamalla@Sudheeshs-MacBook-Pro.local>
2013-10-06 21:59:43 -0700

    Fixes Bug 719651 - Shifted http to https

--- apps/devmo/models.py
+++ apps/devmo/models.py
@@ -67,22 +67,22 @@ class UserProfile(ModelBase):
         )),
         ('twitter', dict(
             label=_(u'Twitter'),
-            prefix='http://twitter.com/',
+            prefix='https://twitter.com/',
             regex='^https?://twitter.com/',
         )),
         ('github', dict(
             label=_(u'GitHub'),
-            prefix='http://github.com/',
+            prefix='https://github.com/',
             regex='^https?://github.com/',
         )),
         ('stackoverflow', dict(
             label=_(u'StackOverflow'),
-            prefix='http://stackoverflow.com/users/',
+            prefix='https://stackoverflow.com/users/',
             regex='^https?://stackoverflow.com/users/',
         )),
         ('linkedin', dict(
             label=_(u'LinkedIn'),
-            prefix='http://www.linkedin.com/in/',
+            prefix='https://www.linkedin.com/in/',
             regex='^https?://www.linkedin.com/in/',
         )),
     ]
