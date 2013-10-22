568ad3e735a0c0c9badb12b2ab99084dacb81b30
David Walsh <davidwalsh83@gmail.com>
2013-10-22 15:18:03 -0500

    Cache bust the WYSIWYG styles

--- apps/wiki/templates/wiki/ckeditor_config.js
+++ apps/wiki/templates/wiki/ckeditor_config.js
@@ -1,106 +1,106 @@
 CKEDITOR.on('instanceReady', function(ev) {
-    var writer = ev.editor.dataProcessor.writer;
+  var writer = ev.editor.dataProcessor.writer;
 
-    // Tighten up the indentation a bit from the default of wide tabs.
-    writer.indentationChars = '  ';
-    
-    // Configure this set of tags to open and close all on the same line, if
-    // possible.
-    var oneliner_tags = [
-        'hgroup', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 
-        'p', 'th', 'td', 'li'
-    ];
-    for(var i = 0, tag; tag = oneliner_tags[i]; i++) {
-        writer.setRules(tag, {
-            indent: true,
-            breakBeforeOpen: true,
-            breakAfterOpen: false,
-            breakBeforeClose: false,
-            breakAfterClose: true
-        });
-    }
-    
-    // Retrieve nodes important to moving the path bar to the top
-    var tbody = ev.editor._.cke_contents.$.parentNode.parentNode,
-        pathP = tbody.lastChild.childNodes[0].childNodes[1],
-        toolbox = tbody.childNodes[0].childNodes[0].childNodes[0];
+  // Tighten up the indentation a bit from the default of wide tabs.
+  writer.indentationChars = ' ';
+  
+  // Configure this set of tags to open and close all on the same line, if
+  // possible.
+  var oneliner_tags = [
+    'hgroup', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 
+    'p', 'th', 'td', 'li'
+  ];
+  for(var i = 0, tag; tag = oneliner_tags[i]; i++) {
+    writer.setRules(tag, {
+      indent: true,
+      breakBeforeOpen: true,
+      breakAfterOpen: false,
+      breakBeforeClose: false,
+      breakAfterClose: true
+    });
+  }
+  
+  // Retrieve nodes important to moving the path bar to the top
+  var tbody = ev.editor._.cke_contents.$.parentNode.parentNode,
+    pathP = tbody.lastChild.childNodes[0].childNodes[1],
+    toolbox = tbody.childNodes[0].childNodes[0].childNodes[0];
 
-    if(toolbox && pathP) {
-        toolbox.appendChild(pathP);
-    }
+  if(toolbox && pathP) {
+    toolbox.appendChild(pathP);
+  }
 
-    // Callback for inline, if necessary
-    var callback = CKEDITOR.inlineCallback;
-    callback && callback(ev);
+  // Callback for inline, if necessary
+  var callback = CKEDITOR.inlineCallback;
+  callback && callback(ev);
 });
 
 // Provide redirect pattern for corresponding plugin
 mdn.ckeditor.redirectPattern = '{{ redirect_pattern|safe }}';
 
 (function() {
-    // Brick dialog "changed" prompts
-    var originalOn = CKEDITOR.dialog.prototype.on;
-    CKEDITOR.dialog.prototype.on = function(event, callback) {
-        // If it's the cancel event that pops up the confirmation, just get out
-        if(event == 'cancel' && callback.toString().indexOf('confirmCancel') != -1) {
-            return true;
-        }
-        originalOn.apply(this, arguments);
+  // Brick dialog "changed" prompts
+  var originalOn = CKEDITOR.dialog.prototype.on;
+  CKEDITOR.dialog.prototype.on = function(event, callback) {
+    // If it's the cancel event that pops up the confirmation, just get out
+    if(event == 'cancel' && callback.toString().indexOf('confirmCancel') != -1) {
+      return true;
+    }
+    originalOn.apply(this, arguments);
+  };
+
+  // <time> elements should be inline
+  CKEDITOR.dtd.$inline['time'] = 1;
+  delete CKEDITOR.dtd.$block['time'];
+
+  // Tell CKEditor that <i> elements are block so empty <i>'s aren't removed
+  // This is essentially for Font-Awesome
+  CKEDITOR.dtd.$block['i'] = 1;
+  delete CKEDITOR.dtd.$removeEmpty['i'];
+
+  // Manage key presses
+  var keys = mdn.ckeditor.keys = {
+      control2: CKEDITOR.CTRL + 50,
+      control3: CKEDITOR.CTRL + 51,
+      control4: CKEDITOR.CTRL + 52,
+      control5: CKEDITOR.CTRL + 53,
+      control6: CKEDITOR.CTRL + 54,
+      controlK: CKEDITOR.CTRL + 75,
+      controlL: CKEDITOR.CTRL + 76,
+      controlShiftL: CKEDITOR.CTRL + CKEDITOR.SHIFT + 76,
+      controlS: CKEDITOR.CTRL + 83,
+      controlO: CKEDITOR.CTRL + 79,
+      controlP: CKEDITOR.CTRL + 80,
+      controlShiftO: CKEDITOR.CTRL + CKEDITOR.SHIFT + 79,
+      controlShiftS: CKEDITOR.CTRL + CKEDITOR.SHIFT + 83,
+      shiftSpace: CKEDITOR.SHIFT + 32,
+      tab: 9,
+      shiftTab: CKEDITOR.SHIFT + 9,
+      enter: 13,
+      back: 1114149,
+      forward: 1114151
+    },
+    block = function(k) {
+      return CKEDITOR.config.blockedKeystrokes.push(keys[k]);
     };
 
-    // <time> elements should be inline
-    CKEDITOR.dtd.$inline['time'] = 1;
-    delete CKEDITOR.dtd.$block['time'];
-
-    // Tell CKEditor that <i> elements are block so empty <i>'s aren't removed
-    // This is essentially for Font-Awesome
-    CKEDITOR.dtd.$block['i'] = 1;
-    delete CKEDITOR.dtd.$removeEmpty['i'];
-
-    // Manage key presses
-    var keys = mdn.ckeditor.keys = {
-            control2: CKEDITOR.CTRL + 50,
-            control3: CKEDITOR.CTRL + 51,
-            control4: CKEDITOR.CTRL + 52,
-            control5: CKEDITOR.CTRL + 53,
-            control6: CKEDITOR.CTRL + 54,
-            controlK: CKEDITOR.CTRL + 75,
-            controlL: CKEDITOR.CTRL + 76,
-            controlShiftL: CKEDITOR.CTRL + CKEDITOR.SHIFT + 76,
-            controlS: CKEDITOR.CTRL + 83,
-            controlO: CKEDITOR.CTRL + 79,
-            controlP: CKEDITOR.CTRL + 80,
-            controlShiftO: CKEDITOR.CTRL + CKEDITOR.SHIFT + 79,
-            controlShiftS: CKEDITOR.CTRL + CKEDITOR.SHIFT + 83,
-            shiftSpace: CKEDITOR.SHIFT + 32,
-            tab: 9,
-            shiftTab: CKEDITOR.SHIFT + 9,
-            enter: 13,
-            back: 1114149,
-            forward: 1114151
-        },
-        block = function(k) {
-            return CKEDITOR.config.blockedKeystrokes.push(keys[k]);
-        };
-
-    // Prevent key handling
-    block('tab');
-    block('shiftTab');
-    block('control2');
-    block('control3');
-    block('control4');
-    block('control5');
-    block('control6');
-    block('controlO');
-    block('controlS');
-    block('controlShiftL');
-    block('controlShiftO');
+  // Prevent key handling
+  block('tab');
+  block('shiftTab');
+  block('control2');
+  block('control3');
+  block('control4');
+  block('control5');
+  block('control6');
+  block('controlO');
+  block('controlS');
+  block('controlShiftL');
+  block('controlShiftO');
 })();
 
 CKEDITOR.timestamp = '{{ BUILD_ID_JS }}';
 CKEDITOR.editorConfig = function(config) {
 
-    config.extraPlugins = 'definitionlist,mdn-buttons,mdn-link,mdn-syntaxhighlighter,mdn-keystrokes,mdn-attachments,mdn-image,mdn-enterkey,mdn-wrapstyle,mdn-table,tablesort,mdn-sampler,mdn-sample-finder,mdn-maximize,mdn-redirect,youtube,autogrow';
+   config.extraPlugins = 'definitionlist,mdn-buttons,mdn-link,mdn-syntaxhighlighter,mdn-keystrokes,mdn-attachments,mdn-image,mdn-enterkey,mdn-wrapstyle,mdn-table,tablesort,mdn-sampler,mdn-sample-finder,mdn-maximize,mdn-redirect,youtube,autogrow';
     config.removePlugins = 'link,image,tab,enterkey,table,maximize';
     config.entities = false;
   
@@ -122,9 +122,9 @@ CKEDITOR.editorConfig = function(config) {
     config.toolbar = 'MDN';
     config.tabSpaces = 2;
     config.contentsCss = [
-      mdn.waffles.redesign ? mdn.mediaPath + 'redesign/css/main.css' : mdn.mediaPath + 'css/wiki-screen.css',
-      mdn.mediaPath + 'css/wiki-edcontent.css', 
-      mdn.mediaPath + 'css/libs/font-awesome/css/font-awesome.min.css',
+      mdn.waffles.redesign ? mdn.mediaPath + 'redesign/css/main.css?{{ BUILD_ID_JS }}' : mdn.mediaPath + 'css/wiki-screen.css?{{ BUILD_ID_JS }}',
+      mdn.mediaPath + 'css/wiki-edcontent.css?{{ BUILD_ID_JS }}',
+      mdn.mediaPath + 'css/libs/font-awesome/css/font-awesome.min.css?{{ BUILD_ID_JS }}',
       '/en-US/docs/Template:CustomCSS?raw=1'
     ];
     
