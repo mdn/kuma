c654d9949226a91127b35a4900ad2aa734c070ad
groovecoder <luke.crouch@gmail.com>
2013-10-28 13:29:57 -0500

    fix bug 929379 - style "view all" as a button

--- media/redesign/js/search.js
+++ media/redesign/js/search.js
@@ -1,19 +1,14 @@
 (function($) {
-  
   var parentSelector = '.search-pane';
-    $(parentSelector + ' .close').on('click', function(e) {
-      e.preventDefault();
-      $('#search-results-close-container').addClass('closed');
-    });
 
-    var $more = $('.search-results-more');
-    $more.find('.view-all').on('click', function(e) {
-      e.preventDefault();
-      var hiddenClass = 'closed';
-      var $parent = $(this).closest(parentSelector);
-      $parent.find('.' + hiddenClass).removeClass(hiddenClass);
-      $parent.find('.pager').removeClass('hidden');
-      $more.removeClass('with-view-all');
-    });
+  var $more = $('.search-results-more');
+  $more.find('.view-all').on('click', function(e) {
+    e.preventDefault();
+    var hiddenClass = 'closed';
+    var $parent = $(this).closest(parentSelector);
+    $parent.find('.' + hiddenClass).removeClass(hiddenClass);
+    $parent.find('.pager').removeClass('hidden');
+    $more.removeClass('with-view-all');
+  });
 
 })(jQuery);
