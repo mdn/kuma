70435b49e6fbc1279a613367ce9eb81acf4c432b
Jannis Leidel <jannis@leidel.info>
2014-02-12 13:50:46 +0100

    Added forgotten setting.

--- settings_travis.py
+++ settings_travis.py
@@ -2,6 +2,7 @@ from settings import *
 
 ASYNC_SIGNALS = False
 ES_URLS = ['http://localhost:9200']
+ES_INDEX_PREFIX = 'mdn'
 ES_INDEXES = {'default': 'main_index'}
 ES_INDEXING_TIMEOUT = 30
 ES_LIVE_INDEX = True
