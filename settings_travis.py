d5446a3a542c8b4aee01bf7b2336ed9cd7230b68
Jannis Leidel <jannis@leidel.info>
2014-02-12 13:39:46 +0100

    Use an own settings module for Travis.

---
+++ settings_travis.py
@@ -0,0 +1,8 @@
+from settings import *
+
+ASYNC_SIGNALS = False
+ES_URLS = ['http://localhost:9200']
+ES_INDEXES = {'default': 'main_index'}
+ES_INDEXING_TIMEOUT = 30
+ES_LIVE_INDEX = True
+ES_DISABLED = False
