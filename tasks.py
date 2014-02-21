8f1e2cdb9d3ff3949336d2de4297d8bba98f6fb4
Jannis Leidel <jannis@leidel.info>
2014-02-21 19:17:04 +0100

    Added invoke task to build wheelball of the compiled requirements for travis.

--- tasks.py
+++ tasks.py
@@ -1,26 +1,55 @@
-from invoke import run, task
+import os
+from invoke import run, task, Collection
 
-default_name = 'wheels'
+target = 'wheelhouse'
+ns = Collection()
+
+
+def go(cmd, **kwargs):
+    return run(cmd.format(**kwargs))
 
 
 @task
-def clean(name=default_name):
-    run('rm -rf {0} {0}.tar.xz'.format(name))
+def clean(name):
+    go('rm -rf {name}', name=target)
+
+
+def build_wheelball(name, requirements,
+                    cache='/tmp'):
+    requirements = ['-r requirements/{0}.txt'.format(req)
+                    for req in requirements]
+
+    go('mkdir -p {name}', name=target)
+    label = os.path.join(target, name)
+    go('sudo pip2.6 wheel --no-use-wheel --wheel-dir={label} {reqs} '
+        '--download-cache={cache}',
+        label=label, reqs=' '.join(requirements), cache=cache)
+    go('cd {target} && tar cfJ {name}.tar.xz {name}',
+       target=target, name=name)
+    go('cd .. && rm -rf {label}', label=label)
+    print('Built ' + label)
 
 
 @task('clean')
-def wheels(name=default_name):
-    run('sudo pip2.6 wheel --no-use-wheel --wheel-dir={0} '
-        '--download-cache=/home/vagrant/src/puppet/cache/pip '
-        '-r requirements/prod.txt '
-        '-r requirements/dev.txt '
-        '-r requirements/compiled.txt'.format(name))
-    run('tar cfJ {0}.tar.xz {0} && rm -rf {0}'.format(name))
-    print("\n  If you have access to S3 you can run now: 'inv upload'")
+def base_wheels(name='wheels'):
+    build_wheelball(name, ['prod', 'dev', 'compiled'])
+
+
+@task('clean')
+def travis_wheels(name='travis_wheels'):
+    build_wheelball(name, ['compiled'])
 
 
 @task
-def upload(name=default_name):
-    run('aws --profile mozilla s3 cp --acl public-read '
-        '{0}.tar.xz s3://pkgs.mozilla.net/python/mdn/{0}.tar.xz'.format(name))
-    print("\n  Running 'inv clean' is recommended")
+def upload(name):
+    go('aws --profile mozilla s3 cp --acl public-read '
+       '{name}.tar.xz s3://pkgs.mozilla.net/python/mdn/{name}.tar.xz',
+       name=name)
+
+
+wheel = Collection('wheel')
+wheel.add_task(base_wheels, 'base')
+wheel.add_task(travis_wheels, 'travis')
+ns.add_collection(wheel)
+ns.add_task(upload)
+ns.add_task(clean)
