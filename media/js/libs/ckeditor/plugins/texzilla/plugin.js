34c39e2ddcfd8b88a224c262d2c8b9009f443fe3
Raniere Silva <raniere@ime.unicamp.br>
2014-02-25 12:50:01 -0300

    Update based on version 1.0

--- media/js/libs/ckeditor/plugins/texzilla/plugin.js
+++ media/js/libs/ckeditor/plugins/texzilla/plugin.js
@@ -1,31 +1,26 @@
-﻿// CKEditor TeXZilla Plugin
+// CKEditor TeXZilla Plugin
 // Copyright (C) 2014  Raniere Silva
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-CKEDITOR.plugins.add( 'texzilla', {
-    icons: 'texzilla',
+/* global CKEDITOR: false */
 
-    init: function( editor ) {
-        CKEDITOR.dialog.add('texzillaDialog', this.path + 'dialogs/texzilla.js');
+CKEDITOR.plugins.add("texzilla", {
+    icons: "texzilla",
 
-        editor.addCommand('texzillaDialog', new CKEDITOR.dialogCommand('texzillaDialog'));
-        editor.ui.addButton('texzilla', {
-            label: 'Insert MathML based on (La)TeX',
-            command: 'texzillaDialog',
-            icon: this.path + 'icons/texzilla.png',
-            toolbar: 'insert'
+    init: function(editor) {
+        CKEDITOR.dialog.add("texzillaDialog",
+            this.path + "dialogs/texzilla.js");
+
+        editor.addCommand("texzillaDialog",
+            new CKEDITOR.dialogCommand("texzillaDialog"));
+        editor.ui.addButton("texzilla", {
+            label: "Insert MathML based on (La)TeX",
+            command: "texzillaDialog",
+            icon: this.path + "icons/texzilla.png",
+            toolbar: "insert"
         });
     }
 });
