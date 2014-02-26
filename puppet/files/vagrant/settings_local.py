0d8a751ad47e519ed10766eb01f5237c975aa4da
groovecoder <luke.crouch@gmail.com>
2014-02-26 17:19:56 -0600

    fix #2058 - restore django-debug-toolbar

--- puppet/files/vagrant/settings_local.py
+++ puppet/files/vagrant/settings_local.py
@@ -34,31 +34,30 @@ CELERY_ALWAYS_EAGER = False
 
 INSTALLED_APPS = INSTALLED_APPS + (
     "django_extensions",
-    # TODO: re-enable after django 1.4
-    # "debug_toolbar",
+    "debug_toolbar",
     "devserver",
 )
 
-MIDDLEWARE_CLASSES = (
-    # TODO: re-enable after django 1.4
-    # "debug_toolbar.middleware.DebugToolbarMiddleware",
-) + MIDDLEWARE_CLASSES
+JINGO_EXCLUDE_APPS = JINGO_EXCLUDE_APPS + (
+    'debug_toolbar',
+)
 
 DEBUG_TOOLBAR_CONFIG = {
     "INTERCEPT_REDIRECTS": False,
 }
 
 DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    #'cache_panel.CachePanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
 )
 
 DEVSERVER_MODULES = (
