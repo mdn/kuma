940c32ea0b776799e2027a26c03fb4b49f8ac6b9
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-03-01 14:38:40 +0100

    bug 975868 - Started CKEditor v3 to v4 updated.
    * Moved CKEditor v3 files to ckeditor-old directory.
    * Added CKEditor v4, SCAYT, WSC submodules.
    * Added CKEditor build script and build config.

---
+++ media/js/libs/ckeditor/source/build-config.js
@@ -0,0 +1,56 @@
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono',
+	ignore: [
+		'dev',
+		'README.md',
+		'.gitignore',
+		'.gitattributes',
+		'.idea',
+		'.mailmap',
+		'.DS_Store'
+	],
+	plugins: {
+		a11yhelp: 1,
+		about: 1,
+		basicstyles: 1,
+		bidi: 1,
+		blockquote: 1,
+		clipboard: 1,
+		contextmenu: 1,
+		dialogadvtab: 1,
+		elementspath: 1,
+		enterkey: 1,
+		entities: 1,
+		find: 1,
+		htmlwriter: 1,
+		image: 1,
+		indentlist: 1,
+		language: 1,
+		link: 1,
+		list: 1,
+		liststyle: 1,
+		magicline: 1,
+		maximize: 1,
+		pastefromword: 1,
+		pastetext: 1,
+		preview: 1,
+		removeformat: 1,
+		resize: 1,
+		scayt: 1,
+		showblocks: 1,
+		showborders: 1,
+		sourcearea: 1,
+		stylescombo: 1,
+		table: 1,
+		tabletools: 1,
+		toolbar: 1,
+		undo: 1,
+		wsc: 1,
+		wysiwygarea: 1
+	}
+};
