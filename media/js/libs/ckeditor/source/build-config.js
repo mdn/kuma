2c40f9f654b318a811fa6339dd35aca0055a55d8
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-03-16 11:56:43 +0100

    Copied tablesort plugin from CKEditor 3 to 4.

--- media/js/libs/ckeditor/source/build-config.js
+++ media/js/libs/ckeditor/source/build-config.js
@@ -59,6 +59,7 @@ var CKBUILDER_CONFIG = {
 		'mdn-sampler': 1,
 		'mdn-syntaxhighlighter': 1,
 		'mdn-system-integration': 1,
-		'mdn-toggle-block': 1
+		'mdn-toggle-block': 1,
+		tablesort: 1
 	}
 };
