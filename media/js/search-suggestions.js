6ba348baf329f29e8ada6a89412b60ddb61b0df3
tx2z <tx2z@caosmental.com>
2014-03-18 20:16:50 +0100

    search filter css & js improvements

--- media/js/search-suggestions.js
+++ media/js/search-suggestions.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function listener(){
+$(document).ready(function(){
     "use strict";
     var filtersData = window.mdn.searchFilters;
 
-    var search = $('.search-wrapper');
-    var searchFilters = search.find('.filters');
-    var searchInput = search.find('input');
-    var searchForm = search.find('form');
-    var suggestions = $('form.search .suggestions');
+    var $search = $('.search-wrapper');
+    var $searchFilters = $search.find('.filters');
+    var $searchInput = $search.find('input');
+    var $searchForm = $search.find('form');
+    var $suggestions = $('form.search .suggestions');
     var BASE_SEARCH_URL = $('form.search').attr('action');
 
-    var previousValue = searchInput.val();
+    var previousValue = $searchInput.val();
 
     var fnSuggestions = {
       'parse':function(s){
@@ -17,17 +17,17 @@ document.addEventListener('DOMContentLoaded', function listener(){
           return matches ? matches[1] || matches[2] : matches;
       },
       'open': function(){
-        suggestions.attr('data-hidden', 'false');
-        suggestions.slideDown();
+        $suggestions.attr('data-hidden', 'false');
+        $suggestions.slideDown();
       },
       'close': function(){
-        suggestions.attr('data-hidden', 'true');
-        suggestions.slideUp();
+        $suggestions.attr('data-hidden', 'true');
+        $suggestions.slideUp();
       },
       'populate': function(){
-        if(suggestions.attr('data-hidden') == 'false')
+        if($suggestions.attr('data-hidden') == 'false')
         {
-            var fs = searchInput.val().match(/#(\S+)/) || [];
+            var fs = $searchInput.val().match(/#(\S+)/) || [];
             this.recoverTopics(fs[1] || '');
             $('.search').mozKeyboardNav();
         }
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function listener(){
             .addClass('topic button')
             .attr('data-topic', f.slice('#'.length))
             .text(f)
-            .appendTo(searchFilters);
+            .appendTo($searchFilters);
 
         $('<button></button>')
             .addClass('close')
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function listener(){
             .appendTo(filter);
       },
       'parseAndAddFilters': function(){
-          var toParse = searchInput.val();
+          var toParse = $searchInput.val();
           var filter = true;
           var filters = [];
           var _this = this;
@@ -64,12 +64,12 @@ document.addEventListener('DOMContentLoaded', function listener(){
           if(filters.length >= 1){
 
               filters.forEach(_this.addFilter());
-              suggestions.attr('data-hidden', 'true');
-              suggestions.hide();
+              $suggestions.attr('data-hidden', 'true');
+              $suggestions.hide();
           }
 
-          searchInput.val(toParse);
-          searchInput.attr('size', searchInput.val().length == 0 ? 15 : searchInput.val().length);
+          $searchInput.val(toParse);
+          $searchInput.attr('size', $searchInput.val().length == 0 ? 15 : $searchInput.val().length);
       },
       'removeAccents': function(srt) {
           var map={'À':'A','Á':'A','Â':'A','Ã':'A','Ä':'A','Å':'A','Æ':'AE','Ç':'C','È':'E','É':'E','Ê':'E','Ë':'E','Ì':'I','Í':'I','Î':'I','Ï':'I','Ð':'D','Ñ':'N','Ò':'O','Ó':'O','Ô':'O','Õ':'O','Ö':'O','Ø':'O','Ù':'U','Ú':'U','Û':'U','Ü':'U','Ý':'Y','ß':'s','à':'a','á':'a','â':'a','ã':'a','ä':'a','å':'a','æ':'ae','ç':'c','è':'e','é':'e','ê':'e','ë':'e','ì':'i','í':'i','î':'i','ï':'i','ñ':'n','ò':'o','ó':'o','ô':'o','õ':'o','ö':'o','ø':'o','ù':'u','ú':'u','û':'u','ü':'u','ý':'y','ÿ':'y','Ā':'A','ā':'a','Ă':'A','ă':'a','Ą':'A','ą':'a','Ć':'C','ć':'c','Ĉ':'C','ĉ':'c','Ċ':'C','ċ':'c','Č':'C','č':'c','Ď':'D','ď':'d','Đ':'D','đ':'d','Ē':'E','ē':'e','Ĕ':'E','ĕ':'e','Ė':'E','ė':'e','Ę':'E','ę':'e','Ě':'E','ě':'e','Ĝ':'G','ĝ':'g','Ğ':'G','ğ':'g','Ġ':'G','ġ':'g','Ģ':'G','ģ':'g','Ĥ':'H','ĥ':'h','Ħ':'H','ħ':'h','Ĩ':'I','ĩ':'i','Ī':'I','ī':'i','Ĭ':'I','ĭ':'i','Į':'I','į':'i','İ':'I','ı':'i','Ĳ':'IJ','ĳ':'ij','Ĵ':'J','ĵ':'j','Ķ':'K','ķ':'k','Ĺ':'L','ĺ':'l','Ļ':'L','ļ':'l','Ľ':'L','ľ':'l','Ŀ':'L','ŀ':'l','Ł':'L','ł':'l','Ń':'N','ń':'n','Ņ':'N','ņ':'n','Ň':'N','ň':'n','ŉ':'n','Ō':'O','ō':'o','Ŏ':'O','ŏ':'o','Ő':'O','ő':'o','Œ':'OE','œ':'oe','Ŕ':'R','ŕ':'r','Ŗ':'R','ŗ':'r','Ř':'R','ř':'r','Ś':'S','ś':'s','Ŝ':'S','ŝ':'s','Ş':'S','ş':'s','Š':'S','š':'s','Ţ':'T','ţ':'t','Ť':'T','ť':'t','Ŧ':'T','ŧ':'t','Ũ':'U','ũ':'u','Ū':'U','ū':'u','Ŭ':'U','ŭ':'u','Ů':'U','ů':'u','Ű':'U','ű':'u','Ų':'U','ų':'u','Ŵ':'W','ŵ':'w','Ŷ':'Y','ŷ':'y','Ÿ':'Y','Ź':'Z','ź':'z','Ż':'Z','ż':'z','Ž':'Z','ž':'z','ſ':'s','ƒ':'f','Ơ':'O','ơ':'o','Ư':'U','ư':'u','Ǎ':'A','ǎ':'a','Ǐ':'I','ǐ':'i','Ǒ':'O','ǒ':'o','Ǔ':'U','ǔ':'u','Ǖ':'U','ǖ':'u','Ǘ':'U','ǘ':'u','Ǚ':'U','ǚ':'u','Ǜ':'U','ǜ':'u','Ǻ':'A','ǻ':'a','Ǽ':'AE','ǽ':'ae','Ǿ':'O','ǿ':'o'};
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function listener(){
       },
       'recoverTopics': function(f){
           // clean suggestion div
-          suggestions.empty();
+          $suggestions.empty();
           var _this = this;
           $.each(filtersData, function(index, group){
               var title = $('<strong>').text(group.name);
@@ -103,45 +103,45 @@ document.addEventListener('DOMContentLoaded', function listener(){
                                   .on('click', function(e){
                                       e.preventDefault();
                                       _this.addFilter('#'+filter.slug);
-                                      searchInput.val(searchInput.val().replace('#'+f,""));
-                                      previousValue = searchInput.val();
-                                      searchInput.attr('size', searchInput.val().length == 0 ? 15 : searchInput.val().length);
-                                      suggestions.attr('data-hidden', 'true');
-                                      suggestions.hide();
-                                      searchInput.focus();
+                                      $searchInput.val($searchInput.val().replace('#'+f,""));
+                                      previousValue = $searchInput.val();
+                                      $searchInput.attr('size', $searchInput.val().length == 0 ? 15 : $searchInput.val().length);
+                                      $suggestions.attr('data-hidden', 'true');
+                                      $suggestions.hide();
+                                      $searchInput.focus();
                                   });
                       $(ul).append($li);
                       show = true;
                   }
               });
               if(show){
-                  suggestions.append(title);
-                  suggestions.append(ul);
+                  $suggestions.append(title);
+                  $suggestions.append(ul);
               }
           });
       }
     };
 
-    search.on('click', function(){
-        searchInput.focus();
+    $search.on('click', function(){
+        $searchInput.focus();
     });
 
     // Open Q : other events than input?
-    searchInput.on('input', function(e){
+    $searchInput.on('input', function(e){
 
-        searchInput.attr('size', searchInput.val().length);
+        $searchInput.attr('size', $searchInput.val().length);
         fnSuggestions.parseAndAddFilters();
 
         // find out if there is a difference of exactly one # between input.value and previousValue
         // Current algorithm is very simple. Must be improved in the future
         // Currently consider only the last character
-        if(searchInput.val().length - previousValue.length === 1 &&
-           searchInput.val()[searchInput.val().length -1] === '#')
+        if($searchInput.val().length - previousValue.length === 1 &&
+           $searchInput.val()[$searchInput.val().length -1] === '#')
         {
             fnSuggestions.open();
         }
 
-        if(previousValue.length - searchInput.val().length === 1 &&
+        if(previousValue.length - $searchInput.val().length === 1 &&
            previousValue[previousValue.length -1] === '#')
         {
             fnSuggestions.close();
@@ -149,12 +149,12 @@ document.addEventListener('DOMContentLoaded', function listener(){
 
         fnSuggestions.populate();
 
-        previousValue = searchInput.val();
+        previousValue = $searchInput.val();
     });
 
     $('.show-topics').on('click', function(e){
         e.preventDefault();
-        if(suggestions.attr('data-hidden') == 'false') {
+        if($suggestions.attr('data-hidden') == 'false') {
           fnSuggestions.close();
         }else{
           fnSuggestions.open();
@@ -172,7 +172,7 @@ document.addEventListener('DOMContentLoaded', function listener(){
             .map(function(t){ return 'topic='+t; })
             .join('&');
 
-        var q = encodeURIComponent(searchInput.val());
+        var q = encodeURIComponent($searchInput.val());
 
         var searchURL = BASE_SEARCH_URL + '?' +
             'q=' + q +
@@ -181,6 +181,4 @@ document.addEventListener('DOMContentLoaded', function listener(){
         console.log('search URL', searchURL);
         location.href = searchURL;
     });
-
-    document.removeEventListener('DOMContentLoaded', listener);
 });
