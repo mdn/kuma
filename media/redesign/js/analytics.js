0f32b3b336a328c03ff9671b3270bdd1503cb966
David Walsh <davidwalsh83@gmail.com>
2014-03-20 09:53:02 -0500

    Updates to tighten up our analytics JS

--- media/redesign/js/analytics.js
+++ media/redesign/js/analytics.js
@@ -1,84 +1,91 @@
-// create namespace
-if (typeof mdn === 'undefined') {
-  var mdn = {};
-}
+(function() {
+    'use strict';
 
-mdn.analytics = {
-  trackEvent: function(eventArray, callback) {
-    // submit eventArray to GA and call callback only after tracking has
-    // been sent, or if sending fails.
-    //
-    // callback is optional.
-    //
-    // Example usage:
-    //
-    // $(function() {
-    //      var handler = function(e) {
-    //           var self = this;
-    //           e.preventDefault();
-    //           $(self).off('submit', handler);
-    //           gaTrack(
-    //              ['_trackEvent', 'Newsletter Registration', 'submit', newsletter],
-    //              function() {$(self).submit();}
-    //           );
-    //      };
-    //      $(thing).on('submit', handler);
-    // });
+    // Adding to globally available mdn object
+    var analytics = mdn.analytics = {
+        /*
+            Tracks generic events passed to the method
+        */
+        trackEvent: function(eventArray, callback) {
+            // Submit eventArray to GA and call callback only after tracking has
+            // been sent, or if sending fails.
+            //
+            // callback is optional.
+            //
+            // Example usage:
+            //
+            // $(function() {
+            //            var handler = function(e) {
+            //                     var self = this;
+            //                     e.preventDefault();
+            //                     $(self).off('submit', handler);
+            //                     gaTrack(
+            //                            ['_trackEvent', 'Newsletter Registration', 'submit'],
+            //                            function() { $(self).submit(); }
+            //                     );
+            //            };
+            //            $(thing).on('submit', handler);
+            // });
 
-    var timer = null;
-    var hasCallback = typeof(callback) === 'function';
-    var gaCallback;
+            var _gaq = window._gaq;
+            var timeout;
+            var timedCallback;
 
-    // Only build new function if callback exists.
-    if (hasCallback) {
-      gaCallback = function() {
-        clearTimeout(timer);
-        callback();
-      };
-    }
-    if (typeof(window._gaq) === 'object') {
-      // send event to GA
-      window._gaq.push(eventArray);
-      // Only set up timer and hitCallback if a callback exists.
-      if (hasCallback) {
-        // Failsafe - be sure we do the callback in a half-second
-        // even if GA isn't able to send in our trackEvent.
-        timer = setTimeout(gaCallback, 500);
+            // Create the timed callback if a callback function has been provided
+            if (typeof(callback) == 'function') {
+                timedCallback = function() {
+                    clearTimeout(timeout);
+                    callback();
+                };
+            }
 
-        // But ordinarily, we get GA to call us back immediately after
-        // it finishes sending our things.
-        // https://developers.google.com/analytics/devguides/collection/gajs/#PushingFunctions
-        // This is called after GA has sent the current pending data:
-        window._gaq.push(gaCallback);
-      }
-    } else {
-      // GA disabled or blocked or something, make sure we still
-      // call the caller's callback:
-      if (hasCallback) {
-          callback();
-      }
-    }
-  },
-  /*
-  Track all outgoing links
-  */
-  initOutboundLinks: function($target) {
-    $target.on('click', 'a', function (e) {
-      if (this.hostname && this.hostname !== window.location.hostname) {
-        var newTab = (this.target === '_blank' || e.metaKey || e.ctrlKey);
-        var href = this.href;
-        var callback = function() {
-          window.location = href;
-        };
+            // If Analytics has loaded, go ahead wiht tracking
+            if (_gaq && _gaq.push) {
+                // Send event to GA
+                _gaq.push(eventArray);
+                // Only set up timeout and hitCallback if a callback exists.
+                if (timedCallback) {
+                    // Failsafe - be sure we do the callback in a half-second
+                    // even if GA isn't able to send in our trackEvent.
+                    timeout = setTimeout(timedCallback, 500);
 
-        var data = ['_trackEvent', 'Outbound Links', href];
-        if (newTab) {
-          mdn.analytics.trackEvent(data);
-        } else {
-          e.preventDefault();
-          mdn.analytics.trackEvent(data, callback);
+                    // But ordinarily, we get GA to call us back immediately after
+                    // it finishes sending our things.
+                    // https://developers.google.com/analytics/devguides/collection/gajs/#PushingFunctions
+                    // This is called after GA has sent the current pending data:
+                    _gaq.push(timedCallback);
+                }
+            }
+            else if(callback) {
+                // GA disabled or blocked or something, make sure we still
+                // call the caller's callback:
+                callback();
+            }
+        },
+
+        /*
+            Track all outgoing links
+        */
+        trackOutboundLinks: function(target) {
+            $(target).on('click', 'a', function (e) {
+                var host = this.hostname;
+
+                if(host && host != location.hostname) {
+                    var newTab = (this.target == '_blank' || e.metaKey || e.ctrlKey);
+                    var href = this.href;
+                    var callback = function() {
+                        location = href;
+                    };
+                    var data = ['_trackEvent', 'Outbound Links', href];
+
+                    if (newTab) {
+                        analytics.trackEvent(data);
+                    } else {
+                        e.preventDefault();
+                        analytics.trackEvent(data, callback);
+                    }
+                }
+            });
         }
-      }
-    });
-  }
-};
+    };
+})();
