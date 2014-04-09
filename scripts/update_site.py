cd307d437bdec5a2db3b1e34a7c1618c22451d95
groovecoder <luke.crouch@gmail.com>
2014-04-09 09:04:04 -0500

    clean out non-dev-server code

--- scripts/update_site.py
+++ scripts/update_site.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 """
 Usage: update_site.py [options]
-Updates a server's sources, vendor libraries, packages CSS/JS
-assets, migrates the database, and other nifty deployment tasks.
+
+Updates the developer-dev.allizom.org server
 
 Options:
   -h, --help            show this help message and exit
-  -e ENVIRONMENT,
-  --environment=ENVIRONMENT
-                        Type of environment. One of
-                        (dev|stage|prod|mdn_dev|mdn_stage|mdn_prod)
-                        Example:
-                        update_site.py -e stage
   -v, --verbose         Echo actions before taking them.
 """
 
@@ -20,23 +14,8 @@ import sys
 from textwrap import dedent
 from optparse import  OptionParser
 
-# Constants
-PROJECT = 0
-
-ENV_BRANCH = {
-    # 'env': 'git branch'
-    # kuma-stage is set to 'stage'
-    'dev':          'master',
-    'stage':        'master',
-    'mdn_dev':      'master',
-    # developer-stage9 is set to 'mdn_stage'
-    'mdn_stage':    'next',
-    'mdn_prod':     'next',
-}
-
-PIP_INSTALL_COMPILED = "pip install -q -r requirements/compiled.txt"
 RM_SETTINGS_PYC = "rm -f settings*.pyc"
-GIT_PULL = "git pull -q origin %(branch)s"
+GIT_PULL = "git pull -q origin master"
 GIT_RESET_HARD = "git reset --hard HEAD"
 GIT_SUBMODULE_SYNC = "git submodule sync"
 GIT_SUBMODULE_UPDATE = "git submodule update --init -q"
@@ -49,17 +28,16 @@ EXEC = 'exec'
 CHDIR = 'chdir'
 
 
-def update_site(env, debug):
+def update_site(debug):
     """Run through commands to update this site."""
     error_updating = False
     here = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    project_branch = {'branch': ENV_BRANCH[env]}
 
     commands = [
         (CHDIR, here),
         (EXEC, RM_SETTINGS_PYC),
         (EXEC, GIT_RESET_HARD),
-        (EXEC, GIT_PULL % project_branch),
+        (EXEC, GIT_PULL),
         (EXEC, GIT_SUBMODULE_SYNC),
         (EXEC, GIT_SUBMODULE_UPDATE),
     ]
@@ -126,9 +104,6 @@ def main():
         """.rstrip())
 
     options = OptionParser(usage=usage)
-    e_help = "Type of environment. One of (%s) Example: update_site.py \
-        -e stage" % '|'.join(ENV_BRANCH.keys())
-    options.add_option("-e", "--environment", help=e_help)
     options.add_option("-v", "--verbose",
                        help="Echo actions before taking them.",
                        action="store_true", dest="verbose")
@@ -136,12 +111,7 @@ def main():
 
     if opts.verbose:
         debug = True
-    if opts.environment in ENV_BRANCH.keys():
-        update_site(opts.environment, debug)
-    else:
-        sys.stderr.write("Invalid environment!\n")
-        options.print_help(sys.stderr)
-        sys.exit(1)
+    update_site(debug)
 
 
 if __name__ == '__main__':
