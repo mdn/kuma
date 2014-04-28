e9410fc5ea0c2d6a0b4b9bb653465dbc69fb2d7c
Raniere Silva <raniere@ime.unicamp.br>
2014-04-28 20:56:10 -0300

    Remove function call
    as suggest by @darkwing.

--- media/redesign/js/wiki.js
+++ media/redesign/js/wiki.js
@@ -13,14 +13,15 @@
         var $div = $('<div class="offscreen"><math xmlns="http://www.w3.org/1998/Math/MathML"><mspace height="23px" width="77px"/></math></div>').appendTo(document.body);
         var box = $div.get(0).firstChild.firstChild.getBoundingClientRect();
         $div.remove();
-        return Math.abs(box.height - 23) > 1 || Math.abs(box.width - 77) > 1;
-    })() && (function() {
 
-        // Add CSS fallback
-        $('<link href="/media/css/libs/mathml.css" rel="stylesheet" type="text/css" />').appendTo(document.head);
+        var supportsMathML = Math.abs(box.height - 23) <= 1 && Math.abs(box.width - 77) <= 1;
+        if (!supportsMathML) {
+            // Add CSS fallback
+            $('<link href="/media/css/libs/mathml.css" rel="stylesheet" type="text/css" />').appendTo(document.head);
 
-        // Add notification
-        $('#wikiArticle').prepend('<div class="notice"><p>Your browser does not seem to support MathML. Some CSS fallback will be used instead, but the mathematics on this page might not render correctly.</p></div>');
+            // Add notification
+            $('#wikiArticle').prepend('<div class="notice"><p>Your browser does not seem to support MathML. Some CSS fallback will be used instead, but the mathematics on this page might not render correctly.</p></div>');
+        }
     })();
 
     /*
