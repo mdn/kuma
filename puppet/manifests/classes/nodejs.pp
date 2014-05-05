6d249d61ceb29d6beba36053f466d8e95f80d791
Les Orchard <me@lmorchard.com>
2014-05-05 18:13:02 -0400

    Revised nodejs v0.10.26 installation
    * Install node.js v0.10.26 from a package stored in our Amazon S3 space
    
    * Ensure the old version of node.js is removed
    
    * Ensure any old installations of the fibers module are removed
    
    * Add the rlwrap package
    
    * Ensure the chris-lea node.js PPA is removed, because it no longer
      offers the exact node.js version we want
    
    * Bonus: Only add the deadsnakes PPA when necessary

--- puppet/manifests/classes/nodejs.pp
+++ puppet/manifests/classes/nodejs.pp
@@ -1,31 +1,50 @@
 # Get node.js and npm installed Ubuntu 12.04 LTS
 class nodejs {
-    # See also: https://launchpad.net/~chris-lea/+archive/node.js/
-    exec { 'install-chris-lea-node-repo':
-        command => '/usr/bin/apt-add-repository -y ppa:chris-lea/node.js && /usr/bin/apt-get update',
-        creates => '/etc/apt/sources.list.d/chris-lea-node_js-precise.list',
-        require => Package['python-software-properties']
+    exec { "nodejs_delete_old_ppa":
+        command => "/bin/rm /etc/apt/sources.list.d/chris-lea-node_js-precise.list*",
+        onlyif => "/bin/ls -1 /etc/apt/sources.list.d | /bin/grep -c 'chris-lea-node_js-precise.list'"
     }
-    package { 'nodejs':
-        ensure => '0.10.26-1chl1~precise1',
-        require => Exec['install-chris-lea-node-repo']
+    exec { "nodejs_uninstall_old_node":
+        command => "/usr/bin/apt-get remove -y nodejs",
+        onlyif => "/usr/bin/node --version | /bin/grep -c 'v0.6.12'"
+    }
+    exec { "nodejs_download":
+        cwd => "/home/vagrant/src/puppet/cache",
+        timeout => 300,
+        command => "/usr/bin/wget https://s3-us-west-2.amazonaws.com/pkgs.mozilla.net/vagrant/mdn/nodejs_0.10.26-1chl1~precise1_i386.deb",
+        creates => "/home/vagrant/src/puppet/cache/nodejs_0.10.26-1chl1~precise1_i386.deb",
+    }
+    package { "nodejs":
+        provider => "dpkg",
+        source => "/home/vagrant/src/puppet/cache/nodejs_0.10.26-1chl1~precise1_i386.deb",
+        require => [
+            Package["rlwrap"],
+            Exec['nodejs_download'],
+            Exec['nodejs_delete_old_ppa'],
+            Exec['nodejs_uninstall_old_node']
+        ]
     }
     file { "/usr/include/node":
         target => "/usr/include/nodejs",
         ensure => link,
         require => Package['nodejs']
     }
+    file { "/usr/local/lib/node_modules/fibers":
+        ensure => absent,
+        force => true
+    }
+    file { "/home/vagrant/src/kumascript/node_modules/fibers":
+        ensure => absent,
+        force => true
+    }
     exec { 'npm-fibers-install':
         command => "/usr/bin/npm install -g fibers@1.0.1",
         creates => "/usr/lib/node_modules/fibers",
         require => [
             Package["nodejs"],
-            File["/usr/include/node"]
+            File["/usr/include/node"],
+            File["/usr/local/lib/node_modules/fibers"],
+            File["/home/vagrant/src/kumascript/node_modules/fibers"]
         ]
-    }
-    # An old version of fibers lived here, ensure it's gone.
-    file { "/usr/local/lib/node_modules/fibers":
-        ensure => absent,
-        force => true
     }
 }
