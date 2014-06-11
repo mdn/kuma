e44fe29a63500b8172221e02180ddfb68b8dbe7d
Jannis Leidel <jannis@leidel.info>
2014-06-11 18:06:16 +0200

    Grant permissions to all tables to be able to create test database.

--- puppet/files/tmp/init.sql
+++ puppet/files/tmp/init.sql
@@ -4,6 +4,7 @@ grant all on *.* to 'root'@'192.168.10.1' identified by '';
 
 drop database if exists kuma;
 create database kuma;
-grant all privileges on kuma.* to kuma@'%' identified by 'kuma';
-grant all privileges on kuma.* to kuma@localhost identified by 'kuma';
-grant all privileges on kuma.* to kuma@10.0.2.15 identified by 'kuma';
+grant all privileges on *.* to kuma@'%' identified by 'kuma';
+grant all privileges on *.* to kuma@localhost identified by 'kuma';
+grant all privileges on *.* to kuma@10.0.2.15 identified by 'kuma';
+flush privileges;
