aeaf2e9ec432553b750fe670be0ce3d843e9b874
David Walsh <davidwalsh83@gmail.com>
2014-06-26 18:31:00 -0500

    Updating puppet script versions to version on dev/stage/prod

--- puppet/manifests/classes/cleancss.pp
+++ puppet/manifests/classes/cleancss.pp
@@ -1,7 +1,7 @@
 # Get clean-css
 class cleancss {
     exec { 'cleancss-install':
-        command => '/usr/bin/npm install -g clean-css@2.1.8',
+        command => '/usr/bin/npm install -g clean-css@2.2.3',
         creates => '/usr/local/bin/cleancss',
         require => [
             Package['nodejs'],
