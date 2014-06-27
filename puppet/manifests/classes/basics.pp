8f2a8edadd2a55c52dadf34026a0b1c05ad8c0f6
Jannis Leidel <jannis@leidel.info>
2014-06-27 12:40:22 +0200

    Actually install a newer version of Puppet.

--- puppet/manifests/classes/basics.pp
+++ puppet/manifests/classes/basics.pp
@@ -10,14 +10,10 @@ class basics {
           "libxml2-dev", "libxslt1.1", "libxslt1-dev",
           "libjpeg62", "libjpeg62-dev",
           "libfreetype6", "libfreetype6-dev",
-          "libpng12-0", "libpng12-dev", "libaugeas-dev",
+          "libpng12-0", "libpng12-dev",
           "libtidy-0.99-0", "libtidy-dev", "rlwrap", "make"]:
             ensure => installed,
             require => Exec['apt-get-update'];
-    }
-    package { "ruby-augeas":
-        ensure   => installed,
-        provider => gem,
     }
     exec {
         "deadsnakes-ppa":
