cd963a990daf3559faabad9f0fd85ffe58c1fd78
Jannis Leidel <jannis@leidel.info>
2014-06-27 15:12:26 +0200

    Removed enough of the old pure SQL migrations to have a passing run again.

--- migrations/10-actioncounters-convert-to-south.sql
+++ migrations/10-actioncounters-convert-to-south.sql
@@ -1,4 +1,3 @@
 --
 -- actioncounters app converted to use South, simulate --fake to skip first migration
 --
-INSERT INTO `south_migrationhistory` VALUES (null,'actioncounters','0001_initial','2011-08-31 10:00:00');
