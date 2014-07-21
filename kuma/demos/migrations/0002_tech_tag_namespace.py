7d6c45becf523cd58afc8ab9dc028a8e8e57916a
groovecoder <luke.crouch@gmail.com>
2014-07-21 12:39:37 -0500

    retro-fit demos migrations to new module location

--- kuma/demos/migrations/0002_tech_tag_namespace.py
+++ kuma/demos/migrations/0002_tech_tag_namespace.py
@@ -10,11 +10,11 @@ from taggit.managers import _TaggableManager
 
 class Migration(DataMigration):
 
-    KNOWN_TECH_TAGS = ( 
+    KNOWN_TECH_TAGS = (
         "audio", "canvas", "css3", "device", "files", "fonts", "forms",
         "geolocation", "javascript", "html5", "indexeddb", "dragndrop",
         "mobile", "offlinesupport", "svg", "video", "webgl", "websockets",
-        "webworkers", "xhr", "multitouch", 
+        "webworkers", "xhr", "multitouch",
     )
 
     def forwards(self, orm):
@@ -89,7 +89,7 @@ class Migration(DataMigration):
             'comments_total': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
             'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True'}),
-            'demo_package': ('demos.models.ReplacingZipFileField', [], {'max_upload_size': '62914560', 'max_length': '255'}),
+            'demo_package': ('kuma.demos.models.ReplacingZipFileField', [], {'max_upload_size': '62914560', 'max_length': '255'}),
             'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'featured': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'hidden': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
@@ -101,17 +101,17 @@ class Migration(DataMigration):
             'likes_total': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_index': 'True', 'blank': 'True'}),
             'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'navbar_optout': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'screenshot_1': ('demos.models.ReplacingImageWithThumbField', [], {'max_length': '255'}),
-            'screenshot_2': ('demos.models.ReplacingImageWithThumbField', [], {'max_length': '255', 'blank': 'True'}),
-            'screenshot_3': ('demos.models.ReplacingImageWithThumbField', [], {'max_length': '255', 'blank': 'True'}),
-            'screenshot_4': ('demos.models.ReplacingImageWithThumbField', [], {'max_length': '255', 'blank': 'True'}),
-            'screenshot_5': ('demos.models.ReplacingImageWithThumbField', [], {'max_length': '255', 'blank': 'True'}),
+            'screenshot_1': ('kuma.demos.models.ReplacingImageWithThumbField', [], {'max_length': '255'}),
+            'screenshot_2': ('kuma.demos.models.ReplacingImageWithThumbField', [], {'max_length': '255', 'blank': 'True'}),
+            'screenshot_3': ('kuma.demos.models.ReplacingImageWithThumbField', [], {'max_length': '255', 'blank': 'True'}),
+            'screenshot_4': ('kuma.demos.models.ReplacingImageWithThumbField', [], {'max_length': '255', 'blank': 'True'}),
+            'screenshot_5': ('kuma.demos.models.ReplacingImageWithThumbField', [], {'max_length': '255', 'blank': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
             'source_code_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
             'summary': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             # HACK: This custom field has gone away entirely, so using its
             # superclass as a stand-in
-            #'tags': ('demos.models.ConstrainedTagField', [], {}),
+            #'tags': ('kuma.demos.models.ConstrainedTagField', [], {}),
             'tags': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
             'video_url': ('embedutils.VideoEmbedURLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
