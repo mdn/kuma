a1f49737aab8cdd1cd2bbfa33720075e9f57dfb5
Jannis Leidel <jannis@leidel.info>
2014-07-29 16:38:13 +0200

    Shield accounts helper from anonymous users.

--- kuma/users/helpers.py
+++ kuma/users/helpers.py
@@ -113,6 +113,8 @@ def social_accounts(user):
         {% if accounts %} -- if there is at least one social account
     """
     accounts = {}
+    if not user.is_authenticated():
+        return accounts
     for account in user.socialaccount_set.all().iterator():
         providers = accounts.setdefault(account.provider, [])
         providers.append(account)
