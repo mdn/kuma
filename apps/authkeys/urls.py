232a0fddf586b41c292dc14e7b446ab0a8b1e43b
Jannis Leidel <jannis@leidel.info>
2014-08-04 16:54:38 +0200

    Updated some account related URLs.
    - Renamed "social" URL pattern into "account"
    - Moved URL patterns from allauth to kuma.
    - Moved authkeys and other URLs into "users/account" namespace
    
    Conflicts:
    	kuma/users/providers/github/urls.py

--- apps/authkeys/urls.py
+++ apps/authkeys/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('authkeys.views',
     url(r'^$', 'list', name='authkeys.list'),
-    url(r'^new$', 'new', name='authkeys.new'),
-    url(r'^(?P<pk>\d+)/history$', 'history', name='authkeys.history'),
-    url(r'^(?P<pk>\d+)/delete$', 'delete', name='authkeys.delete'),
+    url(r'^/new$', 'new', name='authkeys.new'),
+    url(r'^/(?P<pk>\d+)/history$', 'history', name='authkeys.history'),
+    url(r'^/(?P<pk>\d+)/delete$', 'delete', name='authkeys.delete'),
 )
