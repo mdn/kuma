2f7f7068cf0e6cbe72a590db62e80460d8a1d8c2
Stephanie Hobson <sjhobson@gmail.com>
2014-10-28 09:36:10 -0700

    Bug 1080877 - 10th anniversary skin
    Backgrounds, blue buttons, blue links.

--- settings.py
+++ settings.py
@@ -626,6 +626,9 @@ MINIFY_BUNDLES = {
         'demostudio': (
             'redesign/css/demos.css',
         ),
+        'demostudio_10th': (
+            'redesign/css/demos_10th.css',
+        ),
         'devderby': (
             'css/devderby.css',
         ),
