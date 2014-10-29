828928e250c03e8ce952fcae45b71b670993ac83
Francisco Ambrozio <francisco@franciscoambrozio.com>
2014-10-28 22:05:05 -0200

    bug 916229 - Tests

--- kuma/wiki/tests/test_tasks.py
+++ kuma/wiki/tests/test_tasks.py
@@ -5,14 +5,21 @@ from kuma.wiki.tasks import update_community_stats
 from django.core.cache import get_cache
 
 class UpdateCommunityStatsTests(TestCase):
-    def populate_data(self):
+    _contributors = 10
+
+    def setUp(self):
         c = connection.cursor()
 
         try:
-            for i in range(1, 6):
+            for i in range(0, self._contributors):
                 username = 'test_' + str(i)
                 title = 'Test ' + str(i)
                 slug = 'Test ' + str(i)
+
+                if i % 2 == 0:
+                    locale = 'en-US'
+                else:
+                    locale = 'pt-BR'
 
                 insert_user = "INSERT INTO auth_user ( \
                                     username, \
@@ -31,11 +38,20 @@ class UpdateCommunityStatsTests(TestCase):
                                     'John', \
                                     'Doe', \
                                     '!', \
-                                    1, \
+                                    0, \
                                     1, \
                                     0, \
                                     CURRENT_TIMESTAMP, \
                                     CURRENT_TIMESTAMP)"
+
+                c.execute(insert_user)
+                c.execute("SELECT LAST_INSERT_ID()")
+                last_id = c.fetchone()
+
+                try:
+                    user_id = last_id[0]
+                except IndexError:
+                    return False
 
                 insert_document = "INSERT INTO wiki_document ( \
                                          title, \
@@ -53,16 +69,53 @@ class UpdateCommunityStatsTests(TestCase):
                                          '" + slug + "', \
                                          0, \
                                          1, \
-                                         'en-US', \
+                                         '" + locale + "', \
                                          '<html></html>', \
                                          10, \
                                          0, \
                                          0, \
                                          0)"
 
+                c.execute(insert_document)
+                c.execute("SELECT LAST_INSERT_ID()")
+                last_id = c.fetchone()
+
+                try:
+                    document_id = last_id[0]
+                except IndexError:
+                    return False
+
+                insert_revision = "INSERT INTO wiki_revision ( \
+                                           document_id, \
+                                           summary, \
+                                           content, \
+                                           keywords, \
+                                           created, \
+                                           comment, \
+                                           creator_id, \
+                                           is_approved, \
+                                           is_mindtouch_migration, \
+                                           tags, \
+                                           toc_depth \
+                                       ) VALUES ( \
+                                           " + str(document_id) + ", \
+                                           '', \
+                                           '', \
+                                           '', \
+                                           CURRENT_TIMESTAMP, \
+                                           '', \
+                                           " + str(user_id) + ", \
+                                           1, \
+                                           0, \
+                                           '', \
+                                           0 \
+                                       )"
+                c.execute(insert_revision)
+
         finally:
             c.close()
 
+        return True
 
     def test_update_community_stats(self):
         update_community_stats.__call__()
@@ -73,4 +126,6 @@ class UpdateCommunityStatsTests(TestCase):
         self.assertTrue(stats.has_key('locales'))
         self.assertTrue(type(stats['contributors']) is long)
         self.assertTrue(type(stats['locales']) is long)
+        self.assertEqual(stats['contributors'], self._contributors)
+        self.assertEqual(stats['locales'], 2)
 
