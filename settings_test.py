745a17384e3c9ab73068b0727331813eff7cea43
Jannis Leidel <jannis@leidel.info>
2015-01-20 15:52:32 +0100

    Bug 756533 - Move actioncounters test model into own package.
    This prevents the creation of the table in non test environments.

--- settings_test.py
+++ settings_test.py
@@ -3,4 +3,7 @@ from settings import INSTALLED_APPS
 CELERY_ALWAYS_EAGER = True
 ES_LIVE_INDEX = False
 
-INSTALLED_APPS += ('kuma.core.tests.taggit_extras',)
+INSTALLED_APPS += (
+    'kuma.core.tests.taggit_extras',
+    'kuma.actioncounters.tests',
+)
