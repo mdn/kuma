mdn/kuma

commit 7d23be4f67f43f7fb5e30bd0f7474aa9b39a4148

bug 1116335 - update constance import statements

kuma/wiki/managers.py
@@ -4,7 +4,7 @@ from django.core import serializers
 from django.db import models
 
 import bleach
-import constance.config
+import config
 
 from kuma.core.cache import memcache
 
@@ -23,15 +23,15 @@ class TransformManager(models.Manager):
 class BaseDocumentManager(models.Manager):
     """Manager for Documents, assists for queries"""
     def clean_content(self, content_in, use_constance_bleach_whitelists=False):
-        allowed_hosts = constance.config.KUMA_WIKI_IFRAME_ALLOWED_HOSTS
+        allowed_hosts = config.KUMA_WIKI_IFRAME_ALLOWED_HOSTS
         out = (parse_content(content_in)
                .filterIframeHosts(allowed_hosts)
                .serialize())
 
         if use_constance_bleach_whitelists:
-            tags = constance.config.BLEACH_ALLOWED_TAGS
-            attributes = constance.config.BLEACH_ALLOWED_ATTRIBUTES
-            styles = constance.config.BLEACH_ALLOWED_STYLES
+            tags = config.BLEACH_ALLOWED_TAGS
+            attributes = config.BLEACH_ALLOWED_ATTRIBUTES
+            styles = config.BLEACH_ALLOWED_STYLES
         else:
             tags = ALLOWED_TAGS
             attributes = ALLOWED_ATTRIBUTES

kuma/attachments/forms.py
@@ -3,7 +3,7 @@ from tower import ugettext_lazy as _lazy
 
 from django import forms
 
-import constance.config
+from constance import config
 
 from .models import AttachmentRevision
 
@@ -29,7 +29,7 @@ class AttachmentRevisionForm(forms.ModelForm):
         uploaded_file.seek(0)
 
         if mime_type not in \
-                constance.config.WIKI_ATTACHMENT_ALLOWED_TYPES.split():
+                config.WIKI_ATTACHMENT_ALLOWED_TYPES.split():
             raise forms.ValidationError(MIME_TYPE_INVALID)
         return self.cleaned_data['file']
 

kuma/attachments/tests/test_templates.py
@@ -2,7 +2,7 @@ from nose.plugins.attrib import attr
 from nose.tools import eq_, ok_
 from pyquery import PyQuery as pq
 
-import constance.config
+from constance import config
 from django.conf import settings
 
 from kuma.users.tests import UserTestCase
@@ -17,12 +17,12 @@ class AttachmentTests(UserTestCase, WikiTestCase):
 
     def setUp(self):
         super(AttachmentTests, self).setUp()
-        self.old_allowed_types = constance.config.WIKI_ATTACHMENT_ALLOWED_TYPES
-        constance.config.WIKI_ATTACHMENT_ALLOWED_TYPES = 'text/plain'
+        self.old_allowed_types = config.WIKI_ATTACHMENT_ALLOWED_TYPES
+        config.WIKI_ATTACHMENT_ALLOWED_TYPES = 'text/plain'
 
     def tearDown(self):
         super(AttachmentTests, self).tearDown()
-        constance.config.WIKI_ATTACHMENT_ALLOWED_TYPES = self.old_allowed_types
+        config.WIKI_ATTACHMENT_ALLOWED_TYPES = self.old_allowed_types
 
     @attr('security')
     def test_xss_file_attachment_title(self):

kuma/attachments/tests/test_views.py
@@ -8,7 +8,7 @@ from django.core.files.base import ContentFile
 from django.core.files import temp as tempfile
 from django.utils.http import parse_http_date_safe
 
-import constance.config
+from constance import config
 
 from kuma.users.tests import UserTestCase
 from kuma.wiki.models import Document
@@ -23,14 +23,14 @@ from ..utils import make_test_file
 class AttachmentTests(UserTestCase, WikiTestCase):
 
     def setUp(self):
-        self.old_allowed_types = constance.config.WIKI_ATTACHMENT_ALLOWED_TYPES
-        constance.config.WIKI_ATTACHMENT_ALLOWED_TYPES = 'text/plain'
+        self.old_allowed_types = config.WIKI_ATTACHMENT_ALLOWED_TYPES
+        config.WIKI_ATTACHMENT_ALLOWED_TYPES = 'text/plain'
         super(AttachmentTests, self).setUp()
         self.client.login(username='admin', password='testpass')
 
     def tearDown(self):
         super(AttachmentTests, self).tearDown()
-        constance.config.WIKI_ATTACHMENT_ALLOWED_TYPES = self.old_allowed_types
+        config.WIKI_ATTACHMENT_ALLOWED_TYPES = self.old_allowed_types
 
     def _post_new_attachment(self):
         file_for_upload = make_test_file(

kuma/attachments/views.py
@@ -14,7 +14,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.clickjacking import xframe_options_sameorigin
 from django.views.decorators.http import require_GET, require_POST
 
-import constance.config
+from constance import config
 
 from kuma.core.decorators import login_required
 from kuma.core.utils import paginate
@@ -139,7 +139,7 @@ def new_attachment(request):
             return HttpResponseRedirect(attachment.get_absolute_url())
     else:
         if request.POST.get('is_ajax', ''):
-            allowed_list = constance.config.WIKI_ATTACHMENT_ALLOWED_TYPES.split()
+            allowed_list = config.WIKI_ATTACHMENT_ALLOWED_TYPES.split()
             allowed_types = ', '.join(map(guess_extension, allowed_list))
             error_obj = {
                 'title': request.POST.get('is_ajax', ''),

kuma/core/tasks.py
@@ -5,7 +5,7 @@ from django.db import connection
 from django.contrib.sessions.models import Session
 from django.utils import timezone
 
-import constance.config
+from constance import config
 
 from .cache import memcache
 from .models import IPBan
@@ -27,7 +27,7 @@ def clean_sessions():
     """
     now = timezone.now()
     logger = clean_sessions.get_logger()
-    chunk_size = constance.config.SESSION_CLEANUP_CHUNK_SIZE
+    chunk_size = config.SESSION_CLEANUP_CHUNK_SIZE
 
     if memcache.add(LOCK_ID, now.strftime('%c'), LOCK_EXPIRE):
         total_count = get_expired_sessions(now).count()

kuma/core/tests/__init__.py
@@ -9,7 +9,7 @@ from django.utils.functional import wraps
 from django.utils.importlib import import_module
 from django.utils.translation import trans_real
 
-import constance.config
+from constance import config
 from constance.backends import database as constance_database
 from djcelery.app import app
 from nose import SkipTest
@@ -79,14 +79,14 @@ class override_constance_settings(overrider):
     def enable(self):
         self.old_cache = constance_database.db_cache
         constance_database.db_cache = None
-        self.old_settings = dict((k, getattr(constance.config, k))
-                                 for k in dir(constance.config))
+        self.old_settings = dict((k, getattr(config, k))
+                                 for k in dir(config))
         for k, v in self.options.items():
-            constance.config._backend.set(k, v)
+            config._backend.set(k, v)
 
     def disable(self):
         for k, v in self.old_settings.items():
-            constance.config._backend.set(k, v)
+            config._backend.set(k, v)
         constance_database.db_cache = self.old_cache
 
 

kuma/demos/forms.py
@@ -16,7 +16,7 @@ from django.forms.widgets import CheckboxSelectMultiple, RadioSelect
 from django.utils.translation import ugettext_lazy as _
 
 from captcha.fields import ReCaptchaField
-import constance.config
+from constance import config
 
 from kuma.core.utils import parse_tags
 from . import TAG_DESCRIPTIONS
@@ -96,7 +96,7 @@ class SubmissionEditForm(MyModelForm):
             (TAG_DESCRIPTIONS[x]['tag_name'], TAG_DESCRIPTIONS[x]['title'])
             for x in parse_tags(
                 'challenge:none %s' %
-                constance.config.DEMOS_DEVDERBY_CHALLENGE_CHOICE_TAGS,
+                config.DEMOS_DEVDERBY_CHALLENGE_CHOICE_TAGS,
                 sorted=False)
             if x in TAG_DESCRIPTIONS
         )

commit b0a0889b78bc5b631b3024f464b33fa9d4d38175

Bug 1116335 - Fixed import error introduced in 7d23be4f67f43f7fb5e30bd0f7474aa9b39a4148.

kuma/wiki/managers.py
@@ -4,9 +4,7 @@ from django.core import serializers
 from django.db import models
 
 import bleach
-import config
-
-from kuma.core.cache import memcache
+from constance import config
 
 from .constants import (ALLOWED_TAGS, ALLOWED_ATTRIBUTES, ALLOWED_STYLES,
                         TEMPLATE_TITLE_PREFIX)