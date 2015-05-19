9b97a2651b12599fe1aa90302e0ef57530f73a69
harish <harry.softer@gmail.com>
2015-05-19 14:54:25 +0530

    bug 747805 - Changing the initial notification show with guidelines and privacy policy.

--- media/js/scrollback.js
+++ media/js/scrollback.js
@@ -1,64 +1,59 @@
-/* jshint browser:true, multistr:true */
-/* jshint strict: false */
+/* global document, window, setTimeout*/
 /* exported addScrollback */
 
 var scrollbackAdded = false;
 var scrollbackWarningShowed = false;
-var host = 'scrollback.io';
+var host = "scrollback.io";
 function showNotice() {
+    "use strict";
 	/* displays a notice that the widget is not a substitute for tech support */
 	if(!scrollbackWarningShowed) {
 		scrollbackWarningShowed = true;
 
-		/* compute position of the scrollback widget relative to the window and place the notice over it */
-		var scrollbackEl = document.getElementsByClassName('scrollback-stream')[0];
-		var scrollbackStyle = window.getComputedStyle(scrollbackEl);
+        var noticeOverlay = document.createElement("div");
+        var noticeDiv = document.createElement("div");
+        var okayButton = document.createElement("button");
+        var pTag = document.createElement("p");
+        var guidelinesLink = document.createElement("a");
+        var privacyLink = document.createElement("a");
 
-		var noticeText = "Welcome to the Mozilla IRC network. Please follow the "+
-		"<a href=>"+
-		"general rules and etiquette</a> of this community.";
+        guidelinesLink.href = "https://developer.mozilla.org/en-US/docs/Mozilla/QA/Getting_Started_with_IRC";
+        guidelinesLink.setAttribute("target", "_blank");
+		guidelinesLink.appendChild(document.createTextNode("guidelines"));
+        privacyLink.href = "http://web.scrollback.io/privacy/";
+        privacyLink.setAttribute("target", "_blank");
+		privacyLink.appendChild(document.createTextNode("Privacy Policy"));
+		guidelinesLink.style.color = privacyLink.style.color = "#000000";
+		guidelinesLink.style.textDecoration = privacyLink.style.textDecoration = "underline";
 
-		var noticeDiv = document.createElement('div');
-		var link = document.createElement('a');
 
-		link.href = 'https://developer.mozilla.org/en-US/docs/Mozilla/QA/Getting_Started_with_IRC';
-		link.appendChild(document.createTextNode("general rules and etiquette"));
-		link.style.color = '#000000';
-		link.style.textDecoration = 'underline';
+		pTag.appendChild(document.createTextNode("This is a community chat room, please follow the "));
+		pTag.appendChild(guidelinesLink);
+		pTag.appendChild(document.createTextNode("."));
+        pTag.appendChild(document.createTextNode(" This public chat is provided by Scrollback and is subject to its "));
+		pTag.appendChild(privacyLink);
+		pTag.appendChild(document.createTextNode("."));
 
-		noticeDiv.appendChild(document.createTextNode("Welcome to Mozilla's IRC network. Please follow the "));
-		noticeDiv.appendChild(link);
-		noticeDiv.appendChild(document.createTextNode(" of this community."));
 
-		['position', 'bottom', 'left', 'right'].forEach(function (prop) {
-			noticeDiv.style[prop] = scrollbackStyle[prop];
-		});
+        noticeOverlay.className += " scrollback-stream scrollback-toast";
+        okayButton.innerText = okayButton.textContent = "Okay";
+		var okayButtonStyle = "-webkit-appearance: none;  -moz-appearance: none;  appearance: none;  -webkit-transition: 0.3s ease;  transition: 0.3s ease;  background-color: rgb(51, 204, 170);  background-image: none;  border-radius: 2px;  color: rgb(255, 255, 255);  cursor: pointer;  display: inline-block;  font-size: 13px;  font-weight: bold;  padding: .75em 1em;  text-transform: uppercase;width:100%;border-bottom:0;";
 
-		noticeDiv.style.boxSizing = 'border-box';
-		noticeDiv.style.zIndex = 1000;
-		noticeDiv.style.padding = '8px';
-		noticeDiv.style.backgroundColor = '#33ccaa';
-		noticeDiv.style.color = '#333333';
-		noticeDiv.style.boxShadow = '0 4px 16px 0 rgba(0,0,0,0.5)'
-		noticeDiv.style.width = parseInt(scrollbackStyle.width) + 2*parseInt(scrollbackStyle.borderWidth) + 'px';
-
-		var closeButton = document.createElement('button');
-		closeButton.innerHTML = "Okay";
-		var closeButtonStyle = 'display: block; margin: 0 auto; cursor: pointer; color: white; padding: 0;' +
-			'line-height: 40px; text-align: center; border: none; background: #226655; margin-top: 8px; width: 100%';
-
-		closeButton.setAttribute('style', closeButtonStyle);
-		closeButton.onclick = function() {
-			noticeDiv.remove();
-		}
-
-		noticeDiv.appendChild(closeButton);
-
-		document.body.appendChild(noticeDiv);
+		okayButton.setAttribute("style", okayButtonStyle);
+		okayButton.onclick = function() {
+			noticeOverlay.parentNode.removeChild(noticeOverlay);
+		};
+        noticeDiv.appendChild(pTag);
+        noticeDiv.appendChild(okayButton);
+        noticeDiv.setAttribute("style", "line-height: 1.625;padding: 1.5em; text-align:center;width:100%;position:absolute; bottom:0;left:0;box-sizing: border-box;background-color:white;");
+        noticeOverlay.setAttribute("style", "  background-color: rgba(0,0,0,.6); box-sizing: border-box;z-index:1000");
+        noticeOverlay.appendChild(noticeDiv);
+		document.body.appendChild(noticeOverlay);
 	}
 }
 
 function addScrollback(roomName, suggestedNick) {
+    "use strict";
 	if (!roomName) {
 		return;
 	}
@@ -66,8 +61,8 @@ function addScrollback(roomName, suggestedNick) {
 		window.scrollback = {
 			"room": roomName,
 			"form": "toast",
-			"minimize": true,
-			"titlebarColor": "#00539f"
+			"titlebarColor": "#00539f",
+            minimize: false
 		};
 		if(suggestedNick) {
 			window.scrollback.nick = suggestedNick;
@@ -76,16 +71,17 @@ function addScrollback(roomName, suggestedNick) {
 		}
 		var el = document.createElement("script");
 		el.async = 1;
-		el.src = 'https://' + host + '/client.min.js';
+		el.src = "https://" + host + "/client.min.js";
 		document.getElementsByTagName("script")[0].parentNode.appendChild(el);
 		scrollbackAdded = true;
-		setTimeout(showNotice, 2000);
+        el.onload = function(){
+            setTimeout(showNotice, 100);
+        };
 	} else {
 		var iframe = document.getElementsByClassName("scrollback-stream")[0];
 		var url = iframe.src;
-		url = url.replace('https://' + host, '');
-		url = 'https://' + host + url.replace(/\/[^/]*/, '/' + roomName);
-        console.log(url);
+		url = url.replace("https://" + host, "");
+		url = "https://" + host + url.replace(/\/[^/]*/, "/" + roomName);
 		iframe.src = url;
 	}
 }
