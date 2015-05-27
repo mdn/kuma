397b78815150ab642f60d1262fb649a7805991d0
David Walsh <davidwalsh83@gmail.com>
2015-05-27 08:34:55 -0500

    bug 1150118 - JavaScript code quality updates

--- media/js/fonts.js
+++ media/js/fonts.js
@@ -3,145 +3,144 @@
         Font Loading
     */
 
-    if(win.waffle && win.waffle.flag_is_active('web_font_loader')) {
+    if(!(win.waffle && win.waffle.flag_is_active('web_font_loader'))) return;
 
-        // what fonts are we loading?
-        var fonts = [
-            {
-                "name" : "Open Sans Light",
-                "class" : "ffo-opensanslight",
-                "varient" : [
-                    {"weight" : "normal"},
-                    {"weight" : "bold"},
-                    {"style" : "italic"}
-                ]
-            },
-            {
-                "name" : "Open Sans",
-                "class" : "ffo-opensans",
-                "varient" : [
-                    {"weight" : "normal"},
-                    {"weight" : "bold"},
-                    {"style" : "italic"}
-                ]
-            }
-        ];
+    // what fonts are we loading?
+    var fonts = [
+        {
+            "name" : "Open Sans Light",
+            "class" : "ffo-opensanslight",
+            "varient" : [
+                {"weight" : "normal"},
+                {"weight" : "bold"},
+                {"style" : "italic"}
+            ]
+        },
+        {
+            "name" : "Open Sans",
+            "class" : "ffo-opensans",
+            "varient" : [
+                {"weight" : "normal"},
+                {"weight" : "bold"},
+                {"style" : "italic"}
+            ]
+        }
+    ];
 
-        // to collect the results of the observers as they return
-        var fontsLoaded = {};
+    // to collect the results of the observers as they return
+    var fontsLoaded = {};
 
-        // timout for all observers
-        var ffoTimeout = 2000;
+    // timout for all observers
+    var ffoTimeout = 2000;
 
-        // starts observers for all fonts in fonts array
-        function ffoLoad() {
-            // for each font
-            $.each(fonts, function() {
-                // add the class to the fontsLoaded object
-                var fontClass = this.class;
-                fontsLoaded[fontClass] = {};
+    // load the observer plug in
+    $.getScript('/media/js/fontfaceobserver-min.js')
+    .done(ffoLoad)
+    .fail(ffoFinished);
 
-                // get name for later use
-                var fontName = this.name;
+    // starts observers for all fonts in fonts array
+    function ffoLoad() {
+        // for each font
+        $.each(fonts, function() {
+            // add the class to the fontsLoaded object
+            var fontClass = this.class;
 
-                // for each varient of this font
-                $.each(this.varient, function(index) {
-                    var fontVarient = this;
-                    var fontVarientNumber = index;
-                    // add an object to the fontsLoaded object
-                    fontsLoaded[fontClass][fontVarientNumber] = false;
+            // get name for later use
+            var fontName = this.name;
 
-                    // add observer for this varient
-                    var ffObserver = new FontFaceObserver(fontName, fontVarient);
+            fontsLoaded[fontClass] = {};
 
-                    // add callbacks for this observer
-                    ffObserver.check(null, ffoTimeout).then(function () {
-                        // update fontsLoaded object to show it loaded
-                        fontsLoaded[fontClass][fontVarientNumber] = true;
-                        ffoCheck();
-                    }, function () {
-                        // timeout
-                        ffoCheck();
-                    });
+            // for each varient of this font
+            $.each(this.varient, function(index) {
+                var fontVarient = this;
+                var fontVarientNumber = index;
 
+                // add observer for this varient
+                var ffObserver = new FontFaceObserver(fontName, fontVarient);
+
+                // add an object to the fontsLoaded object
+                fontsLoaded[fontClass][fontVarientNumber] = false;
+
+                // add callbacks for this observer
+                ffObserver.check(null, ffoTimeout).then(function () {
+                    // update fontsLoaded object to show it loaded
+                    fontsLoaded[fontClass][fontVarientNumber] = true;
+                    ffoCheck();
+                }, function () {
+                    // timeout
+                    ffoCheck();
                 });
+
             });
-        }
+        });
+    }
 
-        function ffoCheck(){
-            var allLoaded = true;
+    function ffoCheck(){
+        var allLoaded = true;
 
-            // loop through fontsLoaded looking to see if they've loaded
-            $.each(fontsLoaded, function(){
-                var isLoaded = true;
-                $.each(this, function(){
-                    if(this == false) {
-                        isLoaded = false;
-                    }
-                })
-                if(isLoaded == false) {
-                    allLoaded = false;
+        // loop through fontsLoaded looking to see if they've loaded
+        $.each(fontsLoaded, function(){
+            var isLoaded = true;
+            $.each(this, function(){
+                if(this == false) {
+                    isLoaded = false;
                 }
-            });
-
-            // if all fonts are loaded swap font in
-            if(allLoaded) {
-                ffoFinished();
-
-                // set cookie so fonts are displayed automatically with the server's help
-                var expires = new Date();
-                expires.setDate(expires.getDate() + 1);
-                document.cookie = 'ffo=true; expires=' + expires + '; path=/';
+            })
+            if(isLoaded == false) {
+                allLoaded = false;
             }
-        }
+        });
 
-        function ffoFinished() {
-            ffoSwap();
-            tabzillaLoad();
-        }
+        // if all fonts are loaded swap font in
+        if(allLoaded) {
+            ffoFinished();
 
-        function ffoSwap() {
-            // swaps in available fonts
-            $.each(fontsLoaded, function(index) {
-                var fontAttribute = index;
-                var fontLoaded = true;
-                $.each(this, function() {
-                    if(this == false) {
-                        fontLoaded = false;
-                    }
-                })
-                if(fontLoaded == true) {
-                    $('html').attr("data-"+fontAttribute, true);
+            // set cookie so fonts are displayed automatically with the server's help
+            var expires = new Date();
+            expires.setDate(expires.getDate() + 1);
+            document.cookie = 'ffo=true; expires=' + expires + '; path=/';
+        }
+    }
+
+    function ffoFinished() {
+        ffoSwap();
+        tabzillaLoad();
+    }
+
+    function ffoSwap() {
+        // swaps in available fonts
+        $.each(fontsLoaded, function(index) {
+            var fontAttribute = index;
+            var fontLoaded = true;
+            $.each(this, function() {
+                if(this == false) {
+                    fontLoaded = false;
                 }
+            })
+            if(fontLoaded == true) {
+                $('html').attr("data-"+fontAttribute, true);
+            }
+        });
+    }
+
+    function tabzillaLoad() {
+        var $tabzilla = $('#tabzilla');
+        if(!$tabzilla.length) return;
+
+        $('<link />').attr({
+            href: '//mozorg.cdn.mozilla.net/media/css/tabzilla-min.css',
+            type: 'text/css',
+            rel: 'stylesheet'
+        }).on('load', function() {
+
+            $tabzilla.addClass('loaded');
+
+            $.ajax({
+                url: '//mozorg.cdn.mozilla.net/en-US/tabzilla/tabzilla.js',
+                dataType: 'script',
+                cache: true
             });
-        }
-
-        function tabzillaLoad() {
-            $tabzilla = $('#tabzilla');
-
-            if(!$tabzilla.length) return;
-
-            $('<link />').attr({
-                href: '//mozorg.cdn.mozilla.net/media/css/tabzilla-min.css',
-                type: 'text/css',
-                rel: 'stylesheet'
-            }).on('load', function() {
-
-                $('#tabzilla').addClass('loaded');
-
-                $.ajax({
-                    url: '//mozorg.cdn.mozilla.net/en-US/tabzilla/tabzilla.js',
-                    dataType: 'script',
-                    cache: true
-                });
-            }).prependTo(doc.head);
-        };
-
-        // load the observer plug in
-        $.getScript('/media/js/fontfaceobserver-min.js')
-        .done(ffoLoad)
-        .fail(ffoFinished);
-
-    };
+        }).prependTo(doc.head);
+    }
 
 })(window, document, jQuery);
