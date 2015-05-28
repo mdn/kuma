292432986db702a42ffa72274547f9388976e58b
groovecoder <luke.crouch@gmail.com>
2015-05-28 15:41:43 -0500

    bug 1116335 - use vm uploads dir during vm tests

---
+++ provisioning/roles/kuma/files/vagrant/settings_test.py
@@ -0,0 +1,14 @@
+from settings import *
+
+DEBUG = False
+CELERY_ALWAYS_EAGER = True
+ES_LIVE_INDEX = False
+ES_URLS = ['localhost:9200']
+
+INSTALLED_APPS += (
+    'kuma.core.tests.taggit_extras',
+    'kuma.actioncounters.tests',
+)
+BANISH_ENABLED = False
+
+DEMO_UPLOADS_ROOT = '/home/vagrant/uploads/demos'
