caaf880f2dee693a738695eedb7d3e6f2884587d
hoosteeno <justin@tentacu.com>
2015-06-25 16:35:35 -0700

    [bug 1032455] Add more info question

--- media/js/helpfulness.js
+++ media/js/helpfulness.js
@@ -1,30 +1,59 @@
 (function(win, doc, $) {
     if (document.cookie.replace(/(?:(?:^|.*;\s*)helpful-stfu\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== 'true') {
         if (window.waffle && window.waffle.flag_is_active('helpfulness')) {
-            // if they stay a minute, ask if it was helpful
+             
             setTimeout(function() {
-                var question = mdn.Notifier.growl('Did this page help you?<br> <a href="#" class="notification-button" id="helpful-yes">Yes</a><a href="#" class="notification-button" id="helpful-no">No</a><a id="helpful-stfu" href="#">Never ask me again</a>', { closable: true, duration: 0}).question();
+                var notification = mdn.Notifier.growl(gettext('Did this page help you?<br> <a href="#" class="notification-button" id="helpful-yes">Yes</a><a href="#" class="notification-button" id="helpful-no">No</a><a id="helpful-stfu" href="#">Never ask me again</a>'), { closable: true, duration: 0}).question();
 
                 $('#helpful-yes').click(function() {
                    confirm("Thanks for your feedback!", 'success', 'Yes');
                 });
                 $('#helpful-no').click(function() {
-                   confirm("Thanks! We\'ll improve it.", 'error', 'No');
+                   confirm("", 'error', 'No');
                 });
                 $('#helpful-stfu').click(function() {
                    confirm("We won't bug you again.", 'info', 'STFU');
                    document.cookie = "helpful-stfu=true; expires=Fri, 31 Dec 9999 23:59:59 GMT;";
                 });
 
+                // create a dropdown in case the page is unhelpful 
+                var unhelpful_options = [
+                    {val: 'Translate', text: gettext('Translate it into my language')},
+                    {val: 'Make-Simpler', text: gettext('Make it less confusing')},
+                    {val: 'Needs-More-Info', text: gettext('It needs more information')},
+                    {val: 'Incorrect', text: gettext('It contains incorrect information')},
+                    {val: 'Other', text: gettext('Something else')}
+                ]
+                var $select = $('<select />').attr({
+                    id: 'helpful-detail',
+                    value: 'Choose one...'
+                }).append($('<option>').attr('selected', 'selected').text('Choose one...'));
+                $(unhelpful_options).each(function() {
+                    $select.append($('<option>').attr('value', this.val).text(this.text));
+                });
+
+                // handle feedback
                 function confirm(msg, type, label) {
-                    question.close(question.item);
-                    mdn.Notifier.growl(msg, { closable: true, duration: 2500 })[type]();
-                    mdn.analytics.trackEvent({
-                        category: 'Helpful',
-                        action: 'Clicked',
-                        label: label,
-                        {'nonInteraction': 1} //http://www.lunametrics.com/blog/2014/05/06/noninteraction-events-google-analytics/
-                    });
+                    // if "No" ask for more info
+                    if (type === 'error') {
+                        mdn.analytics.trackEvent({
+                            category: 'Helpful',
+                            action: 'Clicked',
+                            label: label
+                        });
+                        notification.error('Uh oh. What would make it better?<br>' + $select[0].outerHTML, 0);
+                        $('#helpful-detail').change(function() {
+                            confirm("Thanks! We\'ll fix it.", 'info', $(this).val());
+                        });
+                    }
+                    else {
+                        notification[type](gettext(msg), 2000);
+                        mdn.analytics.trackEvent({
+                            category: 'Helpful',
+                            action: 'Clicked',
+                            label: label
+                        });
+                    }
                 }
 
             }, 60000);
