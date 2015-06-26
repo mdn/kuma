006581142c641bd25308af7d6b9b1acc66f92381
hoosteeno <justin@tentacu.com>
2015-06-25 17:04:21 -0700

    [bug 1032455] Add l10n wrappers

--- media/js/helpfulness.js
+++ media/js/helpfulness.js
@@ -1,10 +1,21 @@
 (function(win, doc, $) {
     if (document.cookie.replace(/(?:(?:^|.*;\s*)helpful-stfu\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== 'true') {
         if (window.waffle && window.waffle.flag_is_active('helpfulness')) {
-             
-            setTimeout(function() {
-                var notification = mdn.Notifier.growl(gettext('Did this page help you?<br> <a href="#" class="notification-button" id="helpful-yes">Yes</a><a href="#" class="notification-button" id="helpful-no">No</a><a id="helpful-stfu" href="#">Never ask me again</a>'), { closable: true, duration: 0}).question();
 
+            setTimeout(function() {
+
+                // create a notification with a simple question
+                var ask = gettext('Did this page help you?') +
+                    '<br> <a href="#" class="notification-button" id="helpful-yes">' +
+                    gettext('Yes') +
+                    '</a><a href="#" class="notification-button" id="helpful-no">' +
+                    gettext('No') +
+                    '</a><a id="helpful-stfu" href="#">' +
+                    gettext('Never ask me again') +
+                    '</a>';
+                var notification = mdn.Notifier.growl(ask, {closable: true, duration: 0}).question();
+
+                // answers to the simple question include...
                 $('#helpful-yes').click(function() {
                    confirm("Thanks for your feedback!", 'success', 'Yes');
                 });
@@ -20,14 +31,13 @@
                 var unhelpful_options = [
                     {val: 'Translate', text: gettext('Translate it into my language')},
                     {val: 'Make-Simpler', text: gettext('Make it less confusing')},
-                    {val: 'Needs-More-Info', text: gettext('It needs more information')},
-                    {val: 'Incorrect', text: gettext('It contains incorrect information')},
+                    {val: 'Needs-More-Info', text: gettext('Add more information')},
+                    {val: 'Needs-Correction', text: gettext('Correct bad information')},
                     {val: 'Other', text: gettext('Something else')}
                 ]
                 var $select = $('<select />').attr({
-                    id: 'helpful-detail',
-                    value: 'Choose one...'
-                }).append($('<option>').attr('selected', 'selected').text('Choose one...'));
+                    id: 'helpful-detail'
+                }).append($('<option>').attr('selected', 'selected').text(gettext('Choose one...')));
                 $(unhelpful_options).each(function() {
                     $select.append($('<option>').attr('value', this.val).text(this.text));
                 });
@@ -41,9 +51,9 @@
                             action: 'Clicked',
                             label: label
                         });
-                        notification.error('Uh oh. What would make it better?<br>' + $select[0].outerHTML, 0);
+                        notification.error(gettext('Uh oh. What would make it better?') + '<br>' + $select[0].outerHTML, 0);
                         $('#helpful-detail').change(function() {
-                            confirm("Thanks! We\'ll fix it.", 'info', $(this).val());
+                            confirm(gettext("Thanks! We\'ll fix it."), 'info', $(this).val());
                         });
                     }
                     else {
@@ -55,7 +65,6 @@
                         });
                     }
                 }
-
             }, 60000);
         }
     }
