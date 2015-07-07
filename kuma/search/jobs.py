1d5d8c0f1d4e4e9a04ffc37d9872a9672179e32e
Jannis Leidel <jannis@leidel.info>
2015-07-07 10:57:12 +0200

    Bug 1179887 - Use a cacheback job for the search filter group data used by the context processor.

--- kuma/search/jobs.py
+++ kuma/search/jobs.py
@@ -2,9 +2,24 @@ from kuma.core.jobs import KumaJob
 
 
 class AvailableFiltersJob(KumaJob):
+    lifetime = 60 * 60 * 24
+    refresh_timeout = 15
 
     def fetch(self, *args, **kwargs):
         from .models import Filter
 
         return (Filter.objects.filter(enabled=True)
                               .prefetch_related('tags', 'group'))
+
+
+class CommandFiltersJob(KumaJob):
+    lifetime = 60 * 60 * 24
+    refresh_timeout = 15
+
+    def fetch(self, *args, **kwargs):
+        from .models import FilterGroup
+        from .serializers import GroupWithFiltersSerializer
+
+        groups = FilterGroup.objects.all()
+        serializer = GroupWithFiltersSerializer(groups, many=True)
+        return {'command_search_filters': serializer.data}
