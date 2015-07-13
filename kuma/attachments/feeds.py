mdn/kuma

commit f317947a9291fe8e2c542c22937fe06a9dbe5ae4

Fix bug 1179897 - Reduce number of db queries on profile page.
This does a bunch of things:

- Move revision helper to custom user model (ref bug 1180208).
- Use else clause for for loop for getting rid of slow count query.
- Prefetch revision documents to reduce related queries.
- Use a cached property for the previous revision for in-page load caching.

Signed-off-by: Jannis Leidel <jannis@leidel.info>

kuma/attachments/feeds.py
@@ -16,8 +16,7 @@ class AttachmentsFeed(DocumentsFeed):
         return item.title
 
     def item_description(self, item):
-        previous = item.get_previous()
-        if previous is None:
+        if item.previous is None:
             return '<p>Created by: %s</p>' % item.creator.username
         return "<p>Edited by %s: %s" % (item.creator.username, item.comment)
 

kuma/demos/feeds.py
@@ -13,6 +13,7 @@ import jingo
 
 from kuma.core.validators import valid_jsonp_callback_value
 from kuma.core.urlresolvers import reverse
+from kuma.users.helpers import gravatar_url
 
 from . import TAG_DESCRIPTIONS
 from .models import Submission
@@ -47,7 +48,8 @@ class SubmissionJSONFeedGenerator(SyndicationFeed):
                 'link', 'title', 'pubdate', 'author_name', 'author_link',
             ))
 
-            item_out['author_avatar'] = item['obj'].creator.profile.gravatar
+            if item['obj'].creator.email:
+                item_out['author_avatar'] = gravatar_url(item['obj'].creator)
 
             # Linkify the tags used in the feed item
             item_out['categories'] = dict(

kuma/wiki/feeds.py
@@ -308,7 +308,7 @@ class RevisionsFeed(DocumentsFeed):
 
     def item_description(self, item):
         # TODO: put this in a jinja template if django syndication will let us
-        previous = item.get_previous()
+        previous = item.previous
         if previous is None:
             action = u'Created'
         else:

kuma/dashboards/templates/dashboards/includes/revision_dashboard_body.html
@@ -16,8 +16,7 @@
         </thead>
         <tbody>
             {% for revision in revisions %}
-
-                {% set previous_id = revision.get_previous().id %}
+                {% set previous_id = revision.previous.id %}
                 {% set revision_url = revision.document.get_absolute_url() %}
                 <tr
                     tabindex="0"

kuma/demos/templates/demos/elements/profile_link.html
@@ -1,2 +1,2 @@
 {% set display_name = user_display(user) %}
-<a href="{{ user.get_absolute_url() }}" class="url fn" title="{% trans %}See {{ display_name }}'s profile{% endtrans %}">{% if show_gravatar %}<img src="{{ user.profile.gravatar() }}" class="photo avatar" width="{{ gravatar_size }}" height="{{ gravatar_size }}" border="0" /> {% endif %}<span>{{ display_name }}</span></a>
+<a href="{{ user.get_absolute_url() }}" class="url fn" title="{% trans %}See {{ display_name }}'s profile{% endtrans %}">{% if show_gravatar %}<img src="{{ gravatar_url(user, size=gravatar_size) }}" class="photo avatar" width="{{ gravatar_size }}" height="{{ gravatar_size }}" border="0" /> {% endif %}<span>{{ display_name }}</span></a>

kuma/users/models.py
@@ -19,9 +19,7 @@ from kuma.core.fields import LocaleField, JSONField
 from kuma.core.managers import NamespacedTaggableManager
 from kuma.core.models import ModelBase
 from kuma.core.urlresolvers import reverse
-from kuma.wiki.models import Revision
 
-from .helpers import gravatar_url
 from .jobs import UserGravatarURLJob
 from .tasks import send_welcome_email
 
@@ -65,6 +63,11 @@ class User(AbstractUser):
     """
     class Meta:
         db_table = 'auth_user'
+
+    def wiki_revisions(self, count=5):
+        return (self.created_revisions.prefetch_related('document')
+                                      .defer('content', 'summary')
+                                      .order_by('-created')[:count])
 
     @cached_property
     def profile(self):
@@ -204,19 +207,12 @@ class UserProfile(ModelBase):
         if self.is_banned:
             return self.user.bans.filter(is_active=True)[:1][0]
 
-    def gravatar(self):
-        return gravatar_url(self.user)
-
     def allows_editing_by(self, user):
         if user == self.user:
             return True
         if user.is_staff or user.is_superuser:
             return True
         return False
-
-    def wiki_activity(self):
-        return (Revision.objects.filter(creator=self.user)
-                                .order_by('-created')[:5])
 
 
 @receiver(models.signals.post_save, sender=settings.AUTH_USER_MODEL)
@@ -229,9 +225,14 @@ def create_user_profile(sender, instance, created, **kwargs):
 def invalidate_gravatar_url(sender, instance, created, **kwargs):
     job = UserGravatarURLJob()
     if instance.email:
-        job.invalidate(instance.email)
+        handler = job.invalidate
     elif instance.email is None:
-        job.delete(instance.email)
+        handler = job.delete
+    else:
+        return
+    # do the heavy-lifting for all avatar sizes
+    for size in settings.AVATAR_SIZES:
+        handler(instance.email, size=size)
 
 
 @receiver(user_signed_up)

kuma/users/templates/users/profile.html
@@ -54,7 +54,6 @@
             {{ admin_link(profile.user) }}
           {% endif %}
 
-
           <!-- Only shown for the user and admins -->
           {% if profile.allows_editing_by(request.user) %}
               <a id="edit-profile" href="{{ url('users.profile_edit', username=profile.user.username) }}" class="button neutral">{{ _("Edit") }}<i aria-hidden="true" class="icon-pencil"></i></a>
@@ -116,7 +115,7 @@
       </div>
       <div class="column-2 ">
         <figure>
-          <img src="{{ profile.gravatar() }}" alt="{{ profile.user.username }}" width="200" height="200" class="profile-photo avatar" />
+          <img src="{{ gravatar_url(profile.user, size=200) }}" alt="{{ profile.user.username }}" width="200" height="200" class="profile-photo avatar" />
         </figure>
 
         <ul class="profile-links">
@@ -137,7 +136,7 @@
       </div>
     </section>
 
-    {% if demos_page.object_list|length %}
+    {% if demos_paginator.count %}
         <section id="profile-demos" class="profile-section">
             {{ submission_listing(
                 request, demos_page.object_list, demos_page.has_other_pages(),
@@ -161,7 +160,6 @@
               <h2>{{ _("Recent Docs Activity") }}</h2> <a href="{{ url('dashboards.revisions') }}?user={{ profile.user.username }}" class="button">{{ _('View all activity') }}</a>
             </header>
 
-            {% if wiki_activity and wiki_activity|length %}
             <table class="activity">
               <thead>
                 <th scope="col" class="activity-page">{{ _("Page") }}</th>
@@ -169,46 +167,38 @@
                 <th scope="col" class="activity-summary">{{ _("Comment") }}</th>
               </thead>
               <tbody>
-                {% for rev in wiki_activity %}
-                {% set previous_rev = rev.get_previous() %}
+                {% for revision in profile.user.wiki_revisions() %}
                 <tr>
                   <th scope="row">
-                      <h3><a href="{{ rev.document.get_absolute_url() }}">{{ rev.document.title }}</a></h3>
+                      <h3><a href="{{ revision.document.get_absolute_url() }}">{{ revision.document.title }}</a></h3>
                       <ul class="activity-actions">
-                          <li><a href="{{ rev.document.get_absolute_url() }}$edit" class="edit">{{ _("Edit page") }}</a></li>
-                          <li>{% if previous_rev %}<a href="{{ rev.document.get_absolute_url() }}$compare?from={{ previous_rev.id }}&amp;to={{ rev.id }}" class="diff">{{ _("View complete diff") }}</a>{% endif %}</li>
-                          <li><a href="{{ rev.document.get_absolute_url() }}$history" class="history">{{ _("View page history") }}</a></li>
+                          <li><a href="{{ url('wiki.edit_document', revision.document.slug, locale=revision.document.locale) }}" class="edit">{{ _("Edit page") }}</a></li>
+                          <li>{% if revision.previous %}<a href="{{ url('wiki.compare_revisions', revision.document.slug, locale=revision.document.locale) }}?from={{ revision.previous.id }}&amp;to={{ revision.id }}" class="diff">{{ _("View complete diff") }}</a>{% endif %}</li>
+                          <li><a href="{{ url('wiki.document_revisions', revision.document.slug, locale=revision.document.locale) }}" class="history">{{ _("View page history") }}</a></li>
                       </ul>
                   </th>
-                  <td>{{ datetimeformat(rev.created, format='date') }}<br />
-                      {{ datetimeformat(rev.created, format='time') }}</td>
+                  <td>{{ datetimeformat(revision.created, format='date') }}<br />
+                      {{ datetimeformat(revision.created, format='time') }}</td>
                   {# TODO: auto-generate smart comment like "N words changed." #}
-                  <td>{{ format_comment(rev) }}</td>
+                  <td>{{ format_comment(revision, previous_revision=revision.previous) }}</td>
+                </tr>
+                {% else %}
+                <tr>
+                    <th scope="row">
+                    {% if profile.user == request.user %}
+                        {% trans docs_url=wiki_url('Project:MDN/Contributing/Creating_and_editing_pages') %}
+                        <p class="none">You haven't contributed to any MDN docs. <a href="{{ docs_url }}">Pitch in!</a></p>
+                        {% endtrans %}
+                    {% else %}
+                        <p class="none">{{ _('This user has no activity.') }}</p>
+                    {% endif %}
+                    </th>
                 </tr>
                 {% endfor %}
               </tbody>
               <tfoot>
-                      {# TODO: create user revision activity feed
-                <tr>
-                  <td colspan="3">
-                      <a href="/index.php?title=Special:Contributions&feed=rss&target={{ profile.user.username | urlencode }}" rel="alternate" class="feed">{{ _("Feed") }}</a>
-                  </td>
-                </tr>
-                      #}
               </tfoot>
             </table>
-
-            {% else %}
-
-            {% if profile.user == request.user %}
-                {% trans docs_url=wiki_url('Project:MDN/Contributing/Creating_and_editing_pages') %}
-                <p class="none">You haven't contributed to any MDN docs. <a href="{{ docs_url }}">Pitch in!</a></p>
-                {% endtrans %}
-            {% else %}
-                <p class="none">{{ _('This user has no activity.') }}</p>
-            {% endif %}
-
-          {% endif %}
           </section>
 
   </section><!-- /#content-main -->

kuma/users/templates/users/profile_edit.html
@@ -35,7 +35,7 @@
 
       <div class="column-2 smaller">
         <figure class="acc-avatar">
-          <img src="{{ profile.gravatar() }}" alt="{{ profile.user.username }}" width="200" height="200" class="profile-photo avatar" />
+          <img src="{{ gravatar_url(profile.user, size=200) }}" alt="{{ profile.user.username }}" width="200" height="200" class="profile-photo avatar" />
           <figcaption>
               {{ _('Change your avatar at') }} <a href="http://gravatar.com" rel="external">gravatar.com</a>
           </figcaption>

commit f3be77a3b5d4530c54f95758e1f50ae09bd19803

Fix bug 1183082 - Stop ISE in attachments feed introduced in f317947a9291fe8e2c542c22937fe06a9dbe5ae4.

kuma/attachments/feeds.py
@@ -16,7 +16,7 @@ class AttachmentsFeed(DocumentsFeed):
         return item.title
 
     def item_description(self, item):
-        if item.previous is None:
+        if item.get_previous() is None:
             return '<p>Created by: %s</p>' % item.creator.username
         return "<p>Edited by %s: %s" % (item.creator.username, item.comment)
 