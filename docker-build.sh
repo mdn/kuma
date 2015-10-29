b6285151f0ecd3298feff1eb94fbcc4fb67fcb64
Benson Wong <mostlygeek+git@gmail.com>
2015-10-29 22:31:51 +0000

    start adding .travis-ci stuff

--- docker-build.sh
+++ docker-build.sh
@@ -11,9 +11,12 @@ git submodule update --init --recursive
 rm -r static/*
 
 # make sure the build container is updated
+# the build container only includes the necessary 
+# system dependencies to build out the static assets
 docker build -t kuma:builder -f Dockerfile-builder .
 
-# Build the static assets
+# Build the static assets by volume mounting the 
+# current source code
 docker run -it --rm=true -v "$PWD:/app" kuma:builder \
     sh -c './manage.py collectstatic --noinput && ./manage.py compilejsi18n'
 
