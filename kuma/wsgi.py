73998de8190355dd96d9215e5580391c5665d3b7
Jannis Leidel <jannis@leidel.info>
2015-12-21 22:23:13 +0100

    Bug 1057293 - Get rid of the vendor folder.
    - moved requirements files into separate files
    - updated docs
    - start to use peep

--- kuma/wsgi.py
+++ kuma/wsgi.py
@@ -1,13 +1,3 @@
-import os
-import site
-
-# Add the Kuma root dir to the python path so we can import manage.
-this_dir = os.path.dirname(__file__)
-site.addsitedir(os.path.abspath(os.path.join(this_dir, '..')))
-
-# manage adds /lib, and /vendor to the Python path.
-import manage  # noqa
-
 # Enable WhiteNoise
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
