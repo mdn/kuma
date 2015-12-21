9a280c3f17d9264350ae40ed6d0ae5b360111c2f
Jannis Leidel <jannis@leidel.info>
2015-12-21 22:23:45 +0100

    Bug 1057293 - Get rid of the vendor folder.
    Deleted them all.

--- vendor/packages/dbgettext/templatetags/dbgettext_tags.py
+++
@@ -1,8 +0,0 @@
-from dbgettext.parser import parsed_gettext as _parsed_gettext
-from django import template
-
-register = template.Library()
-
-@register.filter
-def parsed_gettext(obj, attribute):
-    return _parsed_gettext(obj, attribute)
