587e21123208b2f1afcec4e64b0edd40ff57de7b
Will Kahn-Greene <willkg@mozilla.com>
2016-01-08 16:55:47 -0500

    Fix bug 1237072 - Fix issue with whitenoise and settings
    whitenoise needs DJANGO_SETTINGS_MODULE to be set in the environment
    otherwise it raises an ImproperlyConfigured exception.
    
    This adds some code to kuma/wsgi.py which only affects dev
    environments where it'll set the variable to "settings_local" if it's
    not in the environment. That's in line with what we're doing already
    elsewhere.

--- kuma/wsgi.py
+++ kuma/wsgi.py
@@ -1,3 +1,8 @@
+# Fix the settings so that Whitenoise is happy
+import os
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_local'
+
 # Enable WhiteNoise
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
