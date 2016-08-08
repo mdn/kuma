7149a06a7615d82612f4fc1dc8e3fbc83b24c5fe
Jeff Bradberry <jeff.bradberry@gmail.com>
2016-08-08 16:04:39 -0400

    bug 1260264 - Brought in the spam_dashboard_recent_events utility function.

---
+++ kuma/dashboards/utils.py
@@ -0,0 +1,70 @@
+import datetime
+
+from django.utils import timezone
+
+from kuma.wiki.models import DocumentDeletionLog, RevisionAkismetSubmission
+
+
+def spam_dashboard_recent_events(start_date=None):
+    """Gather data for recent spam events."""
+    now = timezone.now()
+    data = {
+        'now': now,
+        'recent_spam': [],
+    }
+    if not start_date:
+        start_date = now - datetime.timedelta(days=181)
+
+    # Gather recent published spam
+    recent_spam = RevisionAkismetSubmission.objects.filter(
+        type='spam', revision__created__gt=start_date
+    ).select_related(
+        'revision__document'
+    ).order_by('-id')
+
+    for rs in recent_spam:
+        revision = rs.revision
+        document = revision.document
+
+        # We only care about the spam rev and the one immediately
+        # following, if there is one.
+        revisions = list(
+            document.revisions.filter(
+                id__gte=revision.id
+            ).only('id', 'created').order_by('id')[:2]
+        )
+
+        # How long was it active?
+        if len(revisions) == 1:
+            if document.deleted:
+                try:
+                    entry = DocumentDeletionLog.objects.filter(
+                        locale=document.locale, slug=document.slug
+                    ).latest('id')
+                    time_active_raw = entry.timestamp - revision.created
+                    time_active = int(time_active_raw.total_seconds())
+                except DocumentDeletionLog.DoesNotExist:
+                    time_active = 'Deleted'
+            else:
+                time_active = 'Current'
+        else:
+            next_rev = revisions[1]
+            time_active_raw = next_rev.created - revision.created
+            time_active = int(time_active_raw.total_seconds())
+
+        change_type = 'changetype_{}{}'.format(
+            'edit' if revision.previous else 'new',
+            'trans' if document.parent else ''
+        )
+
+        # Gather table data
+        data['recent_spam'].append({
+            'date': revision.created.date(),
+            'time_active': time_active,
+            'revision_id': revision.id,
+            'revision_path': revision.get_absolute_url(),
+            'change_type': change_type,
+            'document_path': revision.document.get_absolute_url(),
+        })
+
+    return data
