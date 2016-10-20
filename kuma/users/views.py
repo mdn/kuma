8a0c3a9b6130122b22b1a68723a25cad59ca5992
Stephanie Hobson <sjhobson@gmail.com>
2016-10-19 19:22:17 -0700

    bug 1308630: Add account deletion page

--- kuma/users/views.py
+++ kuma/users/views.py
@@ -436,6 +436,16 @@ def user_edit(request, username):
     return render(request, 'users/user_edit.html', context)
 
 
+def user_delete(request, username):
+
+    edit_user = get_object_or_404(User, username=username)
+
+    if not edit_user.allows_editing_by(request.user):
+        return HttpResponseForbidden()
+
+    return render(request, 'users/user_delete.html')
+
+
 class SignupView(BaseSignupView):
     """
     The default signup view from the allauth account app.
