54b02c26f3def5d6b7048617227c8217049e18ea
Stephanie Hobson <sjhobson@gmail.com>
2016-12-19 16:58:53 -0800

    Bug 1314708: Tests for homepage callouts and blog links

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -13,6 +13,29 @@ def test_masthead_displayed(base_url, selenium):
     assert page.is_masthead_displayed
 
 
+@pytest.mark.smoke
+@pytest.mark.nondestructive
+def test_hacks_blog(base_url, selenium):
+    page = HomePage(selenium, base_url).open()
+    assert page.hacks_items_length == 5
+    assert_valid_url(page.hacks_url, follow_redirects=True)
+
+
+@pytest.mark.smoke
+@pytest.mark.nodata
+@pytest.mark.nondestructive
+def test_callouts(base_url, selenium):
+    page = HomePage(selenium, base_url).open()
+    # three of them?
+    assert page.callout_items_length == 3
+    # in a row
+    callout_container = page.callout_container
+    assert callout_container.is_expected_stacking()
+    # valid links?
+    callout_links = page.callout_link_list
+    for link in callout_links:
+        this_link = link.get_attribute('href')
+        assert_valid_url(this_link, follow_redirects=True)
 # header tests
 @pytest.mark.smoke
 @pytest.mark.nodata
