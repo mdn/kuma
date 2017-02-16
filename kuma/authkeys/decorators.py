20c9250df684e20a2e3ed32d12e34ccc09e721bd
rjohnson <rjohnson@mozilla.com>
2017-02-16 13:00:42 -0800

    block basic http auth in maintenance mode
    * modify "accepts_auth_key" decorator to skip
      authorization if in maintenance mode
    * refactor some authkeys tests to use
      pytest-django's goodness instead of
      django.test.TestCase (allows use of
      pytest's parametrize) and replace
      "eq_" and "ok_" functions with asserts
    * add tests for authkeys decorator in
      maintenance mode

--- kuma/authkeys/decorators.py
+++ kuma/authkeys/decorators.py
@@ -2,6 +2,8 @@ import base64
 import binascii
 from functools import wraps
 
+from django.conf import settings
+
 from .models import Key
 
 
@@ -15,19 +17,20 @@ def accepts_auth_key(func):
     @wraps(func)
     def process(request, *args, **kwargs):
         request.authkey = None
-        http_auth = request.META.get('HTTP_AUTHORIZATION', '')
-        if http_auth:
-            try:
-                basic, b64_auth = http_auth.split(' ', 1)
-                if 'Basic' == basic:
-                    auth = base64.decodestring(b64_auth)
-                    key_id, secret = auth.split(':', 1)
-                    key = Key.objects.get(key=key_id)
-                    if key.check_secret(secret):
-                        request.authkey = key
-                        request.user = key.user
-            except (binascii.Error, ValueError, Key.DoesNotExist):
-                pass
+        if not settings.MAINTENANCE_MODE:
+            http_auth = request.META.get('HTTP_AUTHORIZATION', '')
+            if http_auth:
+                try:
+                    basic, b64_auth = http_auth.split(' ', 1)
+                    if 'Basic' == basic:
+                        auth = base64.decodestring(b64_auth)
+                        key_id, secret = auth.split(':', 1)
+                        key = Key.objects.get(key=key_id)
+                        if key.check_secret(secret):
+                            request.authkey = key
+                            request.user = key.user
+                except (binascii.Error, ValueError, Key.DoesNotExist):
+                    pass
         return func(request, *args, **kwargs)
 
     return process
--- kuma/authkeys/tests/test_models.py
+++ kuma/authkeys/tests/test_models.py
@@ -1,28 +1,16 @@
 import pytest
 
-from django.test import TestCase
 
-from kuma.core.tests import ok_
-from kuma.users.tests import user
-
-from ..models import Key
-
-
-class KeyViewsTest(TestCase):
-
-    @pytest.mark.current
-    def test_secret_generation(self):
-        """Generated secret should be saved as a hash and pass a check"""
-        u = user(username="tester23",
-                 email="tester23@example.com",
-                 save=True)
-        key = Key(user=u)
-        secret = key.generate_secret()
-        key.save()
-        ok_(key.key)
-        ok_(key.hashed_secret)
-        ok_(len(key.hashed_secret) > 0)
-        ok_(len(secret) > 0)
-        ok_(secret != key.hashed_secret)
-        ok_(not key.check_secret("I AM A FAKE"))
-        ok_(key.check_secret(secret))
+@pytest.mark.current
+@pytest.mark.django_db
+def test_secret_generation(user_auth_key):
+    """Generated secret should be saved as a hash and pass a check"""
+    key = user_auth_key.key
+    secret = user_auth_key.secret
+    assert key.key
+    assert key.hashed_secret
+    assert len(key.hashed_secret) > 0
+    assert len(secret) > 0
+    assert secret != key.hashed_secret
+    assert not key.check_secret("I AM A FAKE")
+    assert key.check_secret(secret)
