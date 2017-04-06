e8ab3d3ad0e65d98f8b56e50306a3d9b8bc9038e
Stephanie Hobson <sjhobson@gmail.com>
2017-04-06 10:14:59 -0700

    Automatically lint .scss files at compilation and in PRs

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const stylelint = require('gulp-stylelint');
 const watch = require('gulp-watch');
 
 // compiles top-level .scss files
@@ -16,10 +17,33 @@ gulp.task('styles', () => {
         .pipe(gulp.dest('./static/styles'));
 });
 
+// lints .scss files with stylelint
+gulp.task('css:lint', () => {
+  return gulp.src('./kuma/static/styles/**/*.scss')
+    .pipe(stylelint({
+      reporters: [{
+        formatter: 'string',
+        console: true
+      }]
+    }));
+});
+
 // initiates 'styles' task above for *all* .scss file changes
 // (so if a lib file is changed, re-render all top-level .scss files that may include said lib file)
 gulp.task('styles:watch', () => {
+    /* compile all files */
     gulp.watch('./kuma/static/styles/**/*.scss', ['styles']);
+    /* pass only changed file to the linter */
+    gulp.watch('./kuma/static/styles/**/*.scss').on('change', file => {
+      return gulp.src(file.path)
+        .pipe(stylelint({
+            failAfterError: false,
+            reporters: [{
+                formatter: 'string',
+                console: true
+            }]
+        }));
+   });
 });
 
 // watches all non-Sass files in /kuma/static and copies them over to /static
