ceeba3df03ea0dfe8c866e1c3029439800cf3835
John Whitlock <jwhitlock@mozilla.com>
2017-05-30 16:48:02 -0500

    bug 1271509: Reduce user test fixtures
    Any fixture that touches the database has to be destroyed and recreated
    with each test, so there is no benefit to capturing HTML or one-time
    model setup as a fixture.

--- kuma/scrape/tests/conftest.py
+++ kuma/scrape/tests/conftest.py
@@ -13,10 +13,3 @@ def simple_user(db, django_user_model):
         username='JackDeveloper',
         email='jack@example.com',
         date_joined=datetime(2016, 11, 4, 9, 1))
-
-
-@pytest.fixture
-def simple_user_html(simple_user, client):
-    """The profile HTML for a simple user."""
-    response = client.get('/en-US/profiles/' + simple_user.username)
-    return response.content
