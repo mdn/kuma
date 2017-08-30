989ef9fb67ef899e84a0734e8038ae088ab314c8
John Whitlock <jwhitlock@mozilla.com>
2017-08-30 14:26:06 -0500

    bug 1394484: Run headless tests during integration

--- Jenkinsfiles/integration-tests.groovy
+++ Jenkinsfiles/integration-tests.groovy
@@ -6,16 +6,31 @@ stage('Build') {
   }
 }
 
-stage('Tests') {
-  def cmd = "py.test tests/functional --driver Remote --capability browserName firefox --host hub --base-url='${config.job.base_url}'"
+stage('Functional') {
+  def cmd = "py.test tests/functional" +
+            " --driver Remote" +
+            " --capability browserName firefox" +
+            " --host hub" +
+            " --base-url='${config.job.base_url}'"
   if (config.job && config.job.tests) {
     cmd += " -m \"${config.job.tests}\""
   }
 
-  dockerRun("selenium/hub:${config.job.selenium}", ["docker_args": "--name selenium-hub-${BUILD_TAG}"]) {
-    dockerRun("selenium/node-firefox:${config.job.selenium}", ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub", "copies": config.job.selenium_nodes]) {
+  dockerRun("selenium/hub:${config.job.selenium}",
+            ["docker_args": "--name selenium-hub-${BUILD_TAG}"]) {
+    dockerRun("selenium/node-firefox:${config.job.selenium}",
+              ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub",
+               "copies": config.job.selenium_nodes]) {
       dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
-                ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub", "cmd": cmd])
+                ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub",
+                 "cmd": cmd])
     }
   }
+}
+
+stage('Headless') {
+  dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
+            ["docker_args": "--volume ${pwd}/test_results:/test_results"
+             "cmd": "py.test tests/headless" +
+                    " --base-url='${config.job.base_url}'"])
 }
