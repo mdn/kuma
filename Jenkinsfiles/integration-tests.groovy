4985dc9580d88ac298157c6b968b4034f0597f68
John Whitlock <jwhitlock@mozilla.com>
2017-09-27 13:50:49 -0500

    bug 1394484: Add junit reporting

--- Jenkinsfiles/integration-tests.groovy
+++ Jenkinsfiles/integration-tests.groovy
@@ -7,11 +7,13 @@ stage('Build') {
 }
 
 stage('Functional') {
+  def pwd = pwd()
   def cmd = "py.test tests/functional" +
             " --driver Remote" +
             " --capability browserName firefox" +
             " --host hub" +
-            " --base-url='${config.job.base_url}'"
+            " --base-url='${config.job.base_url}'" +
+            " --junit-xml=/test_results/integration.xml"
   if (config.job && config.job.tests) {
     cmd += " -m \"${config.job.tests}\""
   }
@@ -25,15 +27,20 @@ stage('Functional') {
               ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub",
                "copies": config.job.selenium_nodes]) {
       dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
-                ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub",
+                ["docker_args": "--link selenium-hub-${BUILD_TAG}:hub" +
+                                " --volume ${pwd}/test_results:/test_results" +
+                                " --user 1000",
                  "cmd": cmd])
     }
   }
 }
 
 stage('Headless') {
+  def pwd = pwd()
   dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
-            ["docker_args": "--volume ${pwd}/test_results:/test_results"
+            ["docker_args": "--volume ${pwd}/test_results:/test_results" +
+                            " --user 1000",
              "cmd": "py.test tests/headless" +
-                    " --base-url='${config.job.base_url}'"])
+                    " --base-url='${config.job.base_url}'" +
+                    " --junit-xml=/test_results/headless.xml"])
 }
