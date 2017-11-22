e7ddee77cbad02fec3f6d2c387e98777ef31671f
schalkneethling <sneethling@mozilla.com>
2017-11-22 14:46:31 +0200

    Fix Bug 1401291, ensure mozilla.cookiehelper and tag-it passes linting

--- kuma/static/js/libs/mozilla.cookiehelper.js
+++ kuma/static/js/libs/mozilla.cookiehelper.js
@@ -41,15 +41,15 @@ Mozilla.Cookies = {
         var sExpires = '';
         if (vEnd) {
             switch (vEnd.constructor) {
-            case Number:
-                sExpires = vEnd === Infinity ? '; expires=Fri, 31 Dec 9999 23:59:59 GMT' : '; max-age=' + vEnd;
-                break;
-            case String:
-                sExpires = '; expires=' + vEnd;
-                break;
-            case Date:
-                sExpires = '; expires=' + vEnd.toUTCString();
-                break;
+                case Number:
+                    sExpires = vEnd === Infinity ? '; expires=Fri, 31 Dec 9999 23:59:59 GMT' : '; max-age=' + vEnd;
+                    break;
+                case String:
+                    sExpires = '; expires=' + vEnd;
+                    break;
+                case Date:
+                    sExpires = '; expires=' + vEnd.toUTCString();
+                    break;
             }
         }
         document.cookie = encodeURIComponent(sKey) + '=' + encodeURIComponent(sValue) + sExpires + (sDomain ? '; domain=' + sDomain : '') + (sPath ? '; path=' + sPath : '') + (bSecure ? '; secure' : '');
