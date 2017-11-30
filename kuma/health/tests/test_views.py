f6557e7706312649974df2870cad1cfc7b1e3eca
John Whitlock <jwhitlock@mozilla.com>
2017-11-30 16:54:23 -0600

    bug 1422009: Refactor tests for bad HTTP methods

--- kuma/health/tests/test_views.py
+++ kuma/health/tests/test_views.py
@@ -4,22 +4,27 @@ from django.db import DatabaseError
 from django.core.urlresolvers import reverse
 
 
-@pytest.mark.parametrize('http_method',
-                         ['get', 'head', 'put', 'post', 'delete', 'options'])
+@pytest.mark.parametrize('http_method', ['put', 'post', 'delete', 'options'])
+@pytest.mark.parametrize('endpoint', ['health.liveness', 'health.readiness'])
+def test_disallowed_methods(client, http_method, endpoint):
+    """Alternate HTTP methods are not allowed."""
+    url = reverse(endpoint)
+    response = getattr(client, http_method)(url)
+    assert response.status_code == 405
+
+
+@pytest.mark.parametrize('http_method', ['get', 'head'])
 def test_liveness(client, http_method):
     url = reverse('health.liveness')
     response = getattr(client, http_method)(url)
-    assert (response.status_code ==
-            204 if http_method in ('get', 'head') else 405)
+    assert response.status_code == 204
 
 
-@pytest.mark.parametrize('http_method',
-                         ['get', 'head', 'put', 'post', 'delete', 'options'])
+@pytest.mark.parametrize('http_method', ['get', 'head'])
 def test_readiness(db, client, http_method):
     url = reverse('health.readiness')
     response = getattr(client, http_method)(url)
-    assert (response.status_code ==
-            204 if http_method in ('get', 'head') else 405)
+    assert response.status_code == 204
 
 
 def test_readiness_with_db_error(db, client):
