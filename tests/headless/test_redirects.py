265abe4a65a0c2083d46611e585acee2f1a0fd07
John Whitlock <jwhitlock@mozilla.com>
2018-01-29 13:45:47 -0600

    bug 1432370: Update headless redirect tests
    * Use the requested URL as the ID for headless redirect tests, instead
      of url1, url2, etc.
    * Add comments for the redirect tests converted from SCL3 Apache
    * Add a section for new redirects added after the AWS migration

--- tests/headless/test_redirects.py
+++ tests/headless/test_redirects.py
@@ -4,7 +4,7 @@ import pytest
 
 from utils.urls import assert_valid_url
 from .map_301 import (REDIRECT_URLS, GITHUB_IO_URLS, MOZILLADEMOS_URLS,
-                      LEGACY_URLS)
+                      LEGACY_URLS, SCL3_REDIRECT_URLS)
 
 # while these test methods are similar, they're each testing a
 # subset of redirects, and it was easier to work with them separately.
@@ -12,7 +12,8 @@ from .map_301 import (REDIRECT_URLS, GITHUB_IO_URLS, MOZILLADEMOS_URLS,
 
 @pytest.mark.headless
 @pytest.mark.nondestructive
-@pytest.mark.parametrize('url', REDIRECT_URLS)
+@pytest.mark.parametrize('url', REDIRECT_URLS,
+                         ids=[item['url'] for item in REDIRECT_URLS])
 def test_redirects(url, base_url):
     url['base_url'] = base_url
     assert_valid_url(**url)
@@ -20,7 +21,8 @@ def test_redirects(url, base_url):
 
 @pytest.mark.headless
 @pytest.mark.nondestructive
-@pytest.mark.parametrize('url', GITHUB_IO_URLS)
+@pytest.mark.parametrize('url', GITHUB_IO_URLS,
+                         ids=[item['url'] for item in GITHUB_IO_URLS])
 def test_github_redirects(url, base_url):
     url['base_url'] = base_url
     assert_valid_url(**url)
@@ -28,7 +30,8 @@ def test_github_redirects(url, base_url):
 
 @pytest.mark.headless
 @pytest.mark.nondestructive
-@pytest.mark.parametrize('url', MOZILLADEMOS_URLS)
+@pytest.mark.parametrize('url', MOZILLADEMOS_URLS,
+                         ids=[item['url'] for item in MOZILLADEMOS_URLS])
 def test_mozillademos_redirects(url, base_url):
     url['base_url'] = base_url
     assert_valid_url(**url)
@@ -36,7 +39,17 @@ def test_mozillademos_redirects(url, base_url):
 
 @pytest.mark.headless
 @pytest.mark.nondestructive
-@pytest.mark.parametrize('url', LEGACY_URLS)
+@pytest.mark.parametrize('url', LEGACY_URLS,
+                         ids=[item['url'] for item in LEGACY_URLS])
 def test_legacy_urls(url, base_url):
+    url['base_url'] = base_url
+    assert_valid_url(**url)
+
+
+@pytest.mark.headless
+@pytest.mark.nondestructive
+@pytest.mark.parametrize('url', SCL3_REDIRECT_URLS,
+                         ids=[item['url'] for item in SCL3_REDIRECT_URLS])
+def test_slc3_redirects(url, base_url):
     url['base_url'] = base_url
     assert_valid_url(**url)
