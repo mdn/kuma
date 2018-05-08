4203a942664738cdc4e17d451e1b9834a2f52538
Antonio Ladeia <contato@antonioladeia.com>
2018-05-08 20:48:24 -0300

    fix the place of charset

--- kuma/wiki/tests/test_views_code.py
+++ kuma/wiki/tests/test_views_code.py
@@ -48,10 +48,10 @@ def test_code_sample(code_sample_doc, constance_config, client, settings):
 
     normalized = normalize_html(response.content)
     expected = (
+        '<meta charset="utf-8">'
         '<link href="%sbuild/styles/samples.css"'
         ' rel="stylesheet" type="text/css">'
         '<style type="text/css">.some-css { color: red; }</style>'
-        '<meta charset="utf-8">'
         '<title>Root Document - sample1 - code sample</title>'
         'Some HTML'
         '<script>window.alert("HI THERE")</script>'
