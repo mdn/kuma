6eb165266ae3cbecf96c21636801f7fe3f6534d1
John Whitlock <jwhitlock@mozilla.com>
2018-05-14 09:38:03 -0500

    bug 1461350: Convert middleware tests to pytest
    Convert the remaining middleware tests (RemoveSlashMiddleware,
    SetRemoteAddrFromForwardedFor) to pytest.

--- kuma/core/tests/test_middleware.py
+++ kuma/core/tests/test_middleware.py
@@ -2,7 +2,6 @@ import pytest
 from django.test import RequestFactory
 from mock import MagicMock, patch
 
-from . import eq_, KumaTestCase
 from ..middleware import (
     ForceAnonymousSessionMiddleware,
     LegacyDomainRedirectsMiddleware,
@@ -13,38 +12,32 @@ from ..middleware import (
 )
 
 
-class TrailingSlashMiddlewareTestCase(KumaTestCase):
-    def test_no_trailing_slash(self):
-        response = self.client.get(u'/en-US/ohnoez')
-        eq_(response.status_code, 404)
-
-    def test_404_trailing_slash(self):
-        response = self.client.get(u'/en-US/ohnoez/')
-        eq_(response.status_code, 404)
-
-    def test_remove_trailing_slash(self):
-        response = self.client.get(u'/en-US/docs/files/?xxx=\xc3')
-        eq_(response.status_code, 301)
-        assert response['Location'].endswith('/en-US/docs/files?xxx=%C3%83')
+@pytest.mark.parametrize('path', ('/en-US/ohnoez', '/en-US/ohnoez/'))
+def test_remove_slash_middleware_keep_404(client, db, path):
+    '''The RemoveSlashMiddleware retains 404s.'''
+    response = client.get(path)
+    assert response.status_code == 404
 
 
-class SetRemoteAddrFromForwardedForTestCase(KumaTestCase):
+def test_remove_slash_middleware_retains_querystring(client, db):
+    '''The RemoveSlashMiddleware handles encoded querystrings.'''
+    response = client.get(u'/en-US/docs/files/?xxx=\xc3')
+    assert response.status_code == 301
+    assert response['Location'].endswith('/en-US/docs/files?xxx=%C3%83')
 
-    def test_rate_x_forwarded_for(self):
-        rf = RequestFactory()
-        middleware = SetRemoteAddrFromForwardedFor()
 
-        req1 = rf.get('/', HTTP_X_FORWARDED_FOR='1.1.1.1')
-        middleware.process_request(req1)
-        eq_(req1.META['REMOTE_ADDR'], '1.1.1.1')
-
-        req2 = rf.get('/', HTTP_X_FORWARDED_FOR='2.2.2.2')
-        middleware.process_request(req2)
-        eq_(req2.META['REMOTE_ADDR'], '2.2.2.2')
-
-        req3 = rf.get('/', HTTP_X_FORWARDED_FOR='3.3.3.3, 4.4.4.4')
-        middleware.process_request(req3)
-        eq_(req3.META['REMOTE_ADDR'], '3.3.3.3')
+@pytest.mark.parametrize(
+    'forwarded_for,remote_addr',
+    (('1.1.1.1', '1.1.1.1'),
+     ('2.2.2.2', '2.2.2.2'),
+     ('3.3.3.3, 4.4.4.4', '3.3.3.3')))
+def test_set_remote_addr_from_forwarded_for(rf, forwarded_for, remote_addr):
+    '''SetRemoteAddrFromForwardedFor parses the X-Forwarded-For Header.'''
+    rf = RequestFactory()
+    middleware = SetRemoteAddrFromForwardedFor()
+    request = rf.get('/', HTTP_X_FORWARDED_FOR=forwarded_for)
+    middleware.process_request(request)
+    assert request.META['REMOTE_ADDR'] == remote_addr
 
 
 def test_force_anonymous_session_middleware(rf, settings):
