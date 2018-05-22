ac3c6e5526d95ac382f9b480c25b74e2c50ffcf6
John Whitlock <jwhitlock@mozilla.com>
2018-05-22 13:55:47 -0500

    bug 1461350: Update URLs for attachment host
    Update the URLs for the attachment host to ensure that code samples and
    file attachments are processed with locale prefixes.
    
    This adds a test that confirms that a code sample can be viewed with
    ENABLED_RESTRICTIONS_BY_HOST. This test fails before this change. Other
    tests are split up so they test a single configuration.
    
    The urls cache is set when the RestrictedEndpointsMiddleware overrides
    the urlconf. Django's TestCase resets this cache, but pytest doesn't
    unless mark.urls is used. mark.urls is set to the default urlconf, the
    test sets it to the restricted urlconf, and pytest-django cleans it up.

--- kuma/urls_untrusted.py
+++ kuma/urls_untrusted.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.conf.urls import include, url
 
 from kuma.core import views as core_views
+from kuma.core.urlresolvers import i18n_patterns
 from kuma.landing.views import robots_txt
+from kuma.wiki.urls_untrusted import lang_urlpatterns as wiki_lang_urlpatterns
 
 handler403 = core_views.handler403
 handler404 = core_views.handler404
@@ -10,7 +12,6 @@ handler500 = core_views.handler500
 
 urlpatterns = [
     url('', include('kuma.attachments.urls')),
-    url(r'^docs', include('kuma.wiki.urls_untrusted')),
     url(r'^robots.txt', robots_txt, name='robots_txt'),
 ]
 
@@ -19,3 +20,5 @@ if getattr(settings, 'DEBUG_TOOLBAR_INSTALLED', False):
     urlpatterns.append(
         url(r'^__debug__/', include(debug_toolbar.urls)),
     )
+
+urlpatterns += i18n_patterns(url(r'^docs/', include(wiki_lang_urlpatterns)))
