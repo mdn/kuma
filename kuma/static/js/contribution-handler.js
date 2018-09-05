94c8ea7ca53e41e3c6d8394f5620ce6567b41d8f
josh jarvis <josh.jarvis@potatolondon.com>
2018-09-05 17:08:02 +0100

    bug 1490727: Implement checkout flow on frontend

---
+++ kuma/static/js/contribution-handler.js
@@ -0,0 +1,43 @@
+(function(win, doc, $, StripeCheckout) {
+    'use strict';
+    var form = $('#contribute-form'),
+        amountRadio = form.find('input[name=amount-selector]'),
+        defaultAmount = form.find('input[type="radio"]:checked');
+
+    var handler = StripeCheckout.configure({
+        key: '',
+        locale: 'en',
+        name: 'Sand Castles United',
+        description: 'One-time donation',
+        token: function(token) {
+            $('input#stripeToken').val(token.id);
+            $('form').submit();
+        }
+    });
+
+    var selectedAmount = defaultAmount.length ? defaultAmount[0].value * 100 : 0;
+
+    function onAmountSelect(ev) {
+        selectedAmount = ev.target.value * 100;
+    }
+
+    function onSubmit(ev) {
+        ev.preventDefault();
+        handler.open({
+            image: 'https://avatars1.githubusercontent.com/u/7565578?s=280&v=4',
+            name: 'MDN Web Docs',
+            description: 'Contribute to MDN Web Docs',
+            zipCode: true,
+            amount: selectedAmount,
+            closed: function() {
+                form.removeClass('disabled');
+            }
+        });
+    }
+
+    // Register event handlers
+    amountRadio.change(onAmountSelect);
+    form.submit(onSubmit);
+
+    // Destroy event handlers
+})(window, document, jQuery, null /*StripeCheckout*/);
