a1af64d3bbfffa1c281f1b8408fa9edb7c099684
Michal Macioszczyk <michal@potatolondon.com>
2018-09-12 12:38:30 +0100

    bug 1490727: Add views for success and error

--- kuma/landing/views.py
+++ kuma/landing/views.py
@@ -61,16 +61,14 @@ def contribute(request):
         form = ContributionForm(request.POST)
         if form.is_valid():
             charge = form.make_charge()
-            context = {
-                'stripe_response': charge
-            }
-            if settings.MDN_CONTRIBUTION_CONFIRMATION_EMAIL:
-                if charge and charge.id and charge.status == 'succeeded':
+            if charge and charge.id and charge.status == 'succeeded':
+                if settings.MDN_CONTRIBUTION_CONFIRMATION_EMAIL:
                     contribute_thank_you_email.delay(
                         form.cleaned_data['name'],
                         form.cleaned_data['email']
                     )
-            return render(request, 'landing/contribute_thank_you.html', context)
+                return redirect('contribute_confirmation_succeeded')
+            return redirect('contribute_confirmation_error')
 
         form = ContributionForm(request.POST)
     else:
@@ -80,6 +78,12 @@ def contribute(request):
         'form': form,
     }
     return render(request, 'landing/contribute.html', context)
+
+
+@never_cache
+def contribute_confirmation(request, status):
+    context = {'status': status}
+    return render(request, 'landing/contribute_thank_you.html', context)
 
 
 ROBOTS_ALLOWED_TXT = '''\
