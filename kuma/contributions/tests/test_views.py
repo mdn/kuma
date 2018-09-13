46f4823cf9d0ee00527c5c8654f0b929973b7000
josh jarvis <josh.jarvis@potatolondon.com>
2018-09-13 18:53:19 +0100

    remove unused test

--- kuma/contributions/tests/test_views.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from kuma.core.urlresolvers import reverse
-
-
-@pytest.mark.django_db
-def test_contribute_view(client):
-    response = client.get(reverse('contribute'))
-    assert response.status_code == 200
