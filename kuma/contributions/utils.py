656462728ef256c84d5ba85c94c5eb62030e9cfb
John Whitlock <jwhitlock@mozilla.com>
2018-09-23 15:09:56 +0100

    bug 1490727: Handle request without user

--- kuma/contributions/utils.py
+++ kuma/contributions/utils.py
@@ -7,4 +7,5 @@ from .constants import CONTRIBUTION_BETA_FLAG
 def enabled(request):
     """Return True if contributions are enabled."""
     return (settings.MDN_CONTRIBUTION and
+            hasattr(request, 'user') and
             flag_is_active(request, CONTRIBUTION_BETA_FLAG))
