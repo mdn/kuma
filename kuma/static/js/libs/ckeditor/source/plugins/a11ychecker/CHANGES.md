18e1c3335fdd0a1bc2c798b568afd8fd55139b8a
Florian Scholz <fscholz@mozilla.com>
2018-09-23 10:31:57 +0200

    Bug 1169350 – Add the Accessibility Checker plugin to CKEditor

---
+++ kuma/static/js/libs/ckeditor/source/plugins/a11ychecker/CHANGES.md
@@ -0,0 +1,24 @@
+CKEditor Accessibility Checker Changelog
+========================================
+
+[CKEditor Accessibility Checker](https://cksource.com/accessibility-checker/)
+
+Copyright (c) 2014-2016, CKSource - Frederico Knabben. All rights reserved.
+
+## Version 1.0
+
+A brand new CKEditor plugin that lets you inspect accessibility level of content created in CKEditor and immediately solve any issues that are found. For an overview of its features, see the [Accessibility Checker website](https://cksource.com/accessibility-checker/).
+
+It is built upon three key elements:
+
+* User Interface optimized for quick problem solving.
+* Flexibility allowing you to use the accessibility checking engine of your choice (default: [Quail](http://quailjs.org/)).
+* Quick Fix feature letting you fix common problems fully automatically!
+
+All of this comes bundled with a tight integration with CKEditor.
+
+The first release includes three language versions:
+
+* English
+* German (provided by Sebastian Peilicke of [Sopra Steria GmbH](http://www.soprasteria.de/de))
+* Dutch (provided by [Dutch Government](https://www.government.nl/))
---
+++ kuma/static/js/libs/ckeditor/source/plugins/a11ychecker/quickfix/en/AttributeRename.js
@@ -0,0 +1,79 @@
+/**
+ * @license Copyright (c) 2014-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+( function() {
+	'use strict';
+
+	CKEDITOR.plugins.a11ychecker.quickFixes.get( { langCode: 'en',
+		name: 'QuickFix',
+		callback: function( QuickFix ) {
+			/**
+			 * QuickFix renaming an attribute {@link #attributeName} to another name
+			 * {@link #attributeTargetName}.
+			 *
+			 * @member CKEDITOR.plugins.a11ychecker.quickFix
+			 * @class AttributeRename
+			 * @constructor
+			 * @param {CKEDITOR.plugins.a11ychecker.Issue} issue Issue QuickFix is created for.
+			 */
+			function AttributeRename( issue ) {
+				QuickFix.call( this, issue );
+			}
+
+			AttributeRename.prototype = new QuickFix();
+
+			AttributeRename.prototype.constructor = AttributeRename;
+
+			/**
+			 * Name of the attribute to be renamed.
+			 *
+			 * @member CKEDITOR.plugins.a11ychecker.quickFix.AttributeRename
+			 */
+			AttributeRename.prototype.attributeName = 'title';
+
+			/**
+			 * A desired name for the attribute.
+			 *
+			 * @member CKEDITOR.plugins.a11ychecker.quickFix.AttributeRename
+			 */
+			AttributeRename.prototype.attributeTargetName = 'alt';
+
+			AttributeRename.prototype.display = function( form ) {
+				var proposedValue = this.issue.element.getAttribute( this.attributeName) || '';
+
+				form.setInputs( {
+					value: {
+						type: 'text',
+						label: 'Value',
+						value: proposedValue
+					}
+				} );
+			};
+
+			/**
+			 * @param {Object} formAttributes Object containing serialized form inputs. See
+			 * {@link CKEDITOR.plugins.a11ychecker.ViewerForm#serialize}.
+			 * @param {Function} callback Function to be called when a fix was applied. Gets QuickFix object
+			 * as a first parameter.
+			 */
+			AttributeRename.prototype.fix = function( formAttributes, callback ) {
+				var issueElement = this.issue.element,
+					desiredValue = formAttributes.value;
+
+				// Set desired attribute value.
+				issueElement.setAttribute( this.attributeTargetName, desiredValue );
+				// Unset the old attribute.
+				issueElement.removeAttribute( this.attributeName );
+
+				if ( callback ) {
+					callback( this );
+				}
+			};
+
+			AttributeRename.prototype.lang = {};
+			CKEDITOR.plugins.a11ychecker.quickFixes.add( 'en/AttributeRename', AttributeRename );
+		}
+	} );
+}() );
