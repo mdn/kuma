8ade76fa90923ad212fed3dc0e6d55a86d58e0b7
Schalk Neethling <sneethling@mozilla.com>
2018-10-11 18:49:48 +0200

    bug 1479817, anchor link headings are obscured by toc (#4954)
    * bug 1479817, anchor link headings are obscured by toc
    * Fix remaining bugs

--- kuma/static/js/components/local-anchor.js
+++ kuma/static/js/components/local-anchor.js
@@ -7,17 +7,25 @@
     'use strict';
 
     function prependLocalAnchors() {
-        // collect all headings in wiki articles with an `id` attribute
-        var headings = Array.from(document.querySelectorAll('#wikiArticle h2[id]')).concat(
-            Array.from(document.querySelectorAll('#wikiArticle h3[id]'))
+        var mainDocument = document.getElementById('document-main');
+        // collect all headings with an `id` attribute
+        var headings = Array.from(document.querySelectorAll('h2[id]')).concat(
+            Array.from(document.querySelectorAll('h3[id]'))
         );
-        var pageUrl = document.location.href;
+        var pageUrl = new URL(document.location.href);
+
+        // if the pageUrl containes a hash, clear it
+        if (pageUrl.hash) {
+            pageUrl.hash = '';
+        }
 
         for (var i = 0, l = headings.length; i < l; i++) {
             var currentHeading = headings[i];
             var localAnchorTag = document.createElement('a');
             var span = document.createElement('span');
-            var link = window.mdnIcons ? window.mdnIcons.getIcon('link') : gettext('Link');
+            var link = window.mdnIcons
+                ? window.mdnIcons.getIcon('link')
+                : gettext('Link');
 
             localAnchorTag.classList.add('local-anchor');
             span.textContent = gettext('Section');
@@ -26,10 +34,40 @@
 
             // only append to headings that are not offscreen
             if (!currentHeading.classList.contains('offscreen')) {
-                localAnchorTag.href = pageUrl + '#' + currentHeading.id;
-                currentHeading.insertAdjacentElement('beforeend', localAnchorTag);
+                pageUrl.hash = currentHeading.id;
+                localAnchorTag.href = pageUrl;
+                localAnchorTag.dataset.headingId = currentHeading.id;
+                currentHeading.insertAdjacentElement(
+                    'beforeend',
+                    localAnchorTag
+                );
             }
         }
+
+        mainDocument.addEventListener('click', function(event) {
+            var target = event.target;
+
+            // only handle clicks on anchor elements
+            if (target.tagName === 'A') {
+                /* getting the attribute value as apposed to the target property
+                   to ensure we get the actual text value of the attribute */
+                var hrefAttrValue = target.getAttribute('href');
+                var isInDocumentLink = hrefAttrValue.startsWith('#');
+
+                var headingId = isInDocumentLink
+                    ? hrefAttrValue.substr(1)
+                    : target.dataset.headingId;
+                /* only handle clicks on in document links or, that originated from
+                   a `local-anchor` */
+                if (
+                    target.classList.contains('local-anchor') ||
+                    isInDocumentLink
+                ) {
+                    event.preventDefault();
+                    mdn.utils.scrollToHeading(headingId);
+                }
+            }
+        });
     }
 
     // `Array.from` is not supported in IE, progressively enhance
--- kuma/static/js/utils/utils.js
+++ kuma/static/js/utils/utils.js
@@ -1,4 +1,23 @@
 window.mdn.utils = {
+    isTocSticky: false,
+    tocHeight: 0,
+    /**
+     * From the jQuery source:
+     * Get document-relative position by adding viewport scroll
+     * to viewport-relative gBCR.
+     * @param {Object} elem - The element for which to get the top offset
+     * @returns {Number} The top offset of the element
+     */
+    getOffsetTop: function(elem) {
+        'use strict';
+        var boundingClientRect = elem.getBoundingClientRect();
+        /* Get the window object associated with the
+           top-level document object for this node */
+        var elemDocumentWindow = elem.ownerDocument.defaultView;
+        var top = boundingClientRect.top;
+        var yOffset = parseInt(elemDocumentWindow.pageYOffset, 10);
+        return top + yOffset;
+    },
     /**
      * Generate and returns a random string thanks to:
      * https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
@@ -6,6 +25,7 @@ window.mdn.utils = {
      * @return {String} a randomly generated string with a chracter count of `strLength`
      */
     randomString: function(strLength) {
+        'use strict';
         var length = strLength || 5;
         var possible =
             'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -18,5 +38,39 @@ window.mdn.utils = {
         }
 
         return text;
+    },
+    /**
+     * Scroll the page to the location of the element with
+     * the given `id`. Takes into account the space taken
+     * up by the `#toc` to avoid overlap.
+     * @param {String} id = The elements `id`
+     */
+    scrollToHeading: function(id) {
+        'use strict';
+        var toc = document.getElementById('toc');
+
+        // if page has a TOC,
+        if (toc !== undefined) {
+            // get and store the `offsetHeight`
+            this.tocHeight = toc.offsetHeight;
+            // and its sticky status
+            this.isTocSticky = getComputedStyle(toc).position === 'sticky';
+        }
+
+        var heading = document.getElementById(id);
+        var headingTop = mdn.utils.getOffsetTop(heading);
+        var scrollY = 0;
+
+        // if the toc is sticky
+        if (toc && this.isTocSticky) {
+            scrollY = headingTop - this.tocHeight;
+        } else {
+            scrollY = headingTop;
+        }
+
+        // update hash
+        window.location.hash = id;
+        // scroll to heading
+        window.scroll(0, parseInt(scrollY, 10));
     }
 };
