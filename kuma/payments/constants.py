df201f397834acd7e9a139be580f0e4f7236e56d
Michal Macioszczyk <michal@potatolondon.com>
2018-10-24 16:31:09 +0100

    add waffle flag for recurring payments

--- kuma/payments/constants.py
+++ kuma/payments/constants.py
@@ -1,1 +1,2 @@
 CONTRIBUTION_BETA_FLAG = 'contrib_beta'
+RECURRING_PAYMENT_BETA_FLAG = 'recurring_payment_beta'
--- kuma/payments/utils.py
+++ kuma/payments/utils.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from waffle import flag_is_active
 
-from .constants import CONTRIBUTION_BETA_FLAG
+from .constants import CONTRIBUTION_BETA_FLAG, RECURRING_PAYMENT_BETA_FLAG
 
 
 def enabled(request):
@@ -14,3 +14,10 @@ def popup_enabled(request):
     return (enabled(request) and
             hasattr(request, 'user') and
             flag_is_active(request, CONTRIBUTION_BETA_FLAG))
+
+
+def recurring_payment_enabled(request):
+    """Returns True if recurring payment is enabled for the user."""
+    return (enabled(request) and
+            hasattr(request, 'user') and
+            flag_is_active(request, RECURRING_PAYMENT_BETA_FLAG))
