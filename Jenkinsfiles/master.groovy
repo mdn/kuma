dd87f07639ea9ca2c094c908eb530729dd51db2b
rjohnson <rjohnson@mozilla.com>
2018-10-25 10:04:02 -0700

    bug 1483072: stop pushing images from MozMEAO Jenkins

--- Jenkinsfiles/master.groovy
+++ Jenkinsfiles/master.groovy
@@ -9,13 +9,16 @@ stage('Test') {
     utils.compose_test()
 }
 
-stage('Build & push images') {
-    utils.sh_with_notify(
-        'make build-kuma push-kuma',
-        "Build & push of commit-tagged Kuma image"
-    )
-    utils.sh_with_notify(
-        'make push-base VERSION=latest',
-        'Push of latest-tagged Kuma base image'
-    )
+// TODO: After cutover to IT-owned services, remove this condition.
+if (!utils.is_mozmeao_pipeline()) {
+    stage('Build & push images') {
+        utils.sh_with_notify(
+            'make build-kuma push-kuma',
+            "Build & push of commit-tagged Kuma image"
+        )
+        utils.sh_with_notify(
+            'make push-base VERSION=latest',
+            'Push of latest-tagged Kuma base image'
+        )
+    }
 }
