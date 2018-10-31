b3fc7492a1cb8e338bc3d6ce4866decd61412525
John Whitlock <jwhitlock@mozilla.com>
2018-10-31 13:14:28 -0500

    Update strings 2018.13
    * Add Dashboard titles for /dashboards
    * Update payments UI strings, FAQ entries

--- kuma/settings/common.py
+++ kuma/settings/common.py
@@ -641,7 +641,7 @@ TEMPLATES = [
 ]
 
 PUENTE = {
-    'VERSION': '2018.12',
+    'VERSION': '2018.13',
     'BASE_DIR': BASE_DIR,
     'TEXT_DOMAIN': 'django',
     # Tells the extract script what files to look for l10n in and what function
