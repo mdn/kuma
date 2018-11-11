d582f022d7fd926bffaa1cf360f3c097e8117c5e
John Whitlock <jwhitlock@mozilla.com>
2018-11-11 11:36:32 -0600

    Update strings 2018.14
    * Strings for recurring payments program

--- kuma/settings/common.py
+++ kuma/settings/common.py
@@ -642,7 +642,7 @@ TEMPLATES = [
 ]
 
 PUENTE = {
-    'VERSION': '2018.13',
+    'VERSION': '2018.14',
     'BASE_DIR': BASE_DIR,
     'TEXT_DOMAIN': 'django',
     # Tells the extract script what files to look for l10n in and what function
