31d05c1b5cbeacc9097ee61260c02d62f53480c8
rjohnson <rjohnson@mozilla.com>
2018-11-13 10:48:46 -0800

    use standalone selenium images

--- Jenkinsfiles/integration-tests.groovy
+++ Jenkinsfiles/integration-tests.groovy
@@ -6,17 +6,21 @@ stage('Build') {
   }
 }
 
-def functional_test(browser, hub_name, base_dir) {
-  // Define a parallel build that runs Selenium Node for the given browser
+def functional_test(browser, base_dir) {
+  // Define a parallel build that runs stand-alone Selenium for the given browser
   return {
     node {
+      def port = (browser == 'chrome') ? '4444' : '4445'
+      def test_name = "kuma-functional-tests-${browser}-${BUILD_TAG}"
+      def selenium_name = "kuma-selenium-standalone-${browser}-${BUILD_TAG}"
       // Setup the pytest command
       // Timeout after 6 minutes, due to stalled nodes
       def cmd = "timeout --preserve-status 6m" +
                 " py.test tests/functional" +
                 " --driver Remote" +
                 " --capability browserName ${browser}" +
-                " --host hub" +
+                " --host ${selenium_name}" +
+                " --port ${port}" +
                 " --base-url='${config.job.base_url}'" +
                 " --junit-prefix=${browser}" +
                 " --junit-xml=/test_results/functional-${browser}.xml" +
@@ -28,32 +32,34 @@ def functional_test(browser, hub_name, base_dir) {
       if (config.job && config.job.maintenance_mode) {
         cmd += " --maintenance-mode"
       }
-      def node_name = "kuma-selenium-node-${browser}-${BUILD_TAG}"
-      def test_name = "kuma-functional-tests-${browser}-${BUILD_TAG}"
 
       try {
-        // Create named nodes
-        dockerRun("selenium/node-${browser}:${config.job.selenium}",
-                  ["docker_args": "-d" +
-                                  " --name ${node_name}" +
-                                  " --link ${hub_name}:hub"])
+        // Create the Selenium stand-alone container for the given browser
+        // The "--shm-size=2g" is recommended to avoid browser crashes (see
+        // https://github.com/SeleniumHQ/docker-selenium#running-the-images)
+        // which we've seen in our chrome integration tests without it.
+        dockerRun("selenium/standalone-${browser}:${config.job.selenium}",
+                  ["docker_args": "-d " +
+                                  "--shm-size=2g " +
+                                  "-e SE_OPTS='-port ${port}' "  +
+                                  "--name ${selenium_name}"])
 
         try {
             // Timeout after 7 minutes, if in-container timeout fails
             timeout(time: 7, unit: 'MINUTES') {
                 // Run test node
                 dockerRun("kuma-integration-tests:${GIT_COMMIT_SHORT}",
-                          ["docker_args": "--link ${hub_name}:hub" +
-                                          " --name ${test_name}" +
-                                          " --volume ${base_dir}/test_results:/test_results" +
-                                          " --user ${UID}",
+                          ["docker_args": "--link ${selenium_name} " +
+                                          "--name ${test_name} " +
+                                          "--volume ${base_dir}/test_results:/test_results " +
+                                          "--user ${UID}",
                            "cmd": cmd])
             }
         } finally {
             dockerStop(test_name)
         }
       } finally {
-        dockerStop("${node_name}")
+          dockerStop(selenium_name)
       }
     }
   }
@@ -80,37 +86,19 @@ stage('Test') {
     // Setup parallel tests
     def allTests = [:]
     def base_dir = pwd()
-    def hub_name = "kuma-selenium-hub-${BUILD_TAG}"
-    allTests['chrome'] = functional_test('chrome', hub_name, base_dir)
-    allTests['firefox'] = functional_test('firefox', hub_name, base_dir)
-    allTests['headless'] = headless_test(base_dir)
+    allTests['chrome'] = functional_test('chrome', base_dir)
+    allTests['firefox'] = functional_test('firefox', base_dir)
+    // allTests['headless'] = headless_test(base_dir)
 
     def nick =  "ci-bot"
-    try {
-        // Setup the selenium hub
-        dockerRun("selenium/hub:${config.job.selenium}",
-                  ["docker_args": "-d --name ${hub_name}"])
 
-        try {
-            // Run the tests in parallel
-            parallel allTests
-            // Notify on success
-            utils.notify_irc([
-                irc_nick: nick,
-                stage: 'Test',
-                status: 'success'
-            ])
-        } catch(err) {
-            utils.notify_irc([
-                irc_nick: nick,
-                stage: 'Test',
-                status: 'failure'
-            ])
-            throw err
-        } finally {
-            dockerStop(hub_name)
-        }
-    } finally {
-        dockerStop(hub_name)
+    try {
+        // Run the tests in parallel
+        parallel allTests
+        // Notify on success
+        utils.notify_irc([irc_nick: nick, stage: 'Test', status: 'success'])
+    } catch(err) {
+        utils.notify_irc([irc_nick: nick, stage: 'Test', status: 'failure'])
+        throw err
     }
 }
