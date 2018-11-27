99a978cd1d3895716f097a6e9c6a0ae4938b18bd
joshJarr <josh.jarvis@potatolondon.com>
2018-11-27 17:52:02 +0000

    Styling for the cancel subscription page.

--- kuma/payments/views.py
+++ kuma/payments/views.py
@@ -144,19 +144,29 @@ def payment_terms(request):
 @login_required
 @never_cache
 def recurring_payment_management(request):
-    context = {}
+    context = {
+        'support_mail_link': 'mailto:' + settings.CONTRIBUTION_SUPPORT_EMAIL + '?Subject=Recurring%20payment%20support',
+        'support_mail': settings.CONTRIBUTION_SUPPORT_EMAIL,
+        'cancel_request': False,
+        'cancel_success': False,
+    }
+
     if request.user.stripe_customer_id and 'stripe_cancel_subscription' in request.POST:
-        cancel_stripe_customer_subscription(
+        cancel_success = cancel_stripe_customer_subscription(
             request.user.stripe_customer_id,
             request.user.email,
             request.user.username
         )
+        context['cancel_request'] = True
+        context['cancel_success'] = cancel_success
 
     if request.user.stripe_customer_id:
-        context = get_stripe_customer_data(
-            request.user.stripe_customer_id,
-            request.user.email,
-            request.user.username
+        context.update(
+            get_stripe_customer_data(
+                request.user.stripe_customer_id,
+                request.user.email,
+                request.user.username
+            ),
         )
 
     return render(request, 'payments/management.html', context)
