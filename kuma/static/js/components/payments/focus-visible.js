2ce6fe4d903fa13624af5166c3fda8e6b2d5c366
Schalk Neethling <sneethling@mozilla.com>
2019-01-17 15:42:48 +0200

    Bug 1518844, ensure focus is clearly indicated on amount radio buttons

--- kuma/static/js/components/payments/focus-visible.js
+++ kuma/static/js/components/payments/focus-visible.js
@@ -2,6 +2,9 @@
     'use strict';
     var contributeForm = document.getElementById('contribute-form');
 
+    /**
+     * Removes the `focus-visible` class form the current element
+     */
     function removeFocusVisible() {
         this.classList.remove('focus-visible');
         this.removeEventListener('focusout', removeFocusVisible);
@@ -11,6 +14,23 @@
     contributeForm.addEventListener('keyup', function(event) {
         var currentTarget = event.target;
         var targetType = currentTarget.type;
+
+        /* The key pressed was the tab key, or the left or right
+           arrow keys(used to cycle through radio buttons), and the
+           target field type is one of `radio` */
+        if (
+            (event.key === 'Tab' ||
+                event.key === 'ArrowLeft' ||
+                event.key === 'ArrowRight') &&
+            targetType === 'radio'
+        ) {
+            // get the parent label element
+            var radioButtonLabel = currentTarget.parentElement;
+            // add the `focus-visible` class
+            radioButtonLabel.classList.add('focus-visible');
+            // listen for when the field loses focus,
+            radioButtonLabel.addEventListener('focusout', removeFocusVisible);
+        }
 
         /* The key pressed was the tab key, and the target field
            type is one of `text`, `email`, or `number */
