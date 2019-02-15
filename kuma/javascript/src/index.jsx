cb92b06b7294450cec233e6420ae752c697c5eb2
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2019-02-15 20:08:26 +0300

    bug 1525719: react spike: get the ducks back

--- kuma/javascript/src/index.jsx
+++ kuma/javascript/src/index.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './logo.jsx';
 
-let container = document.getElementById('main-header-logo');
+const container = document.getElementById('main-header-logo');
+
 if (container) {
     ReactDOM.render(<Logo url={container.dataset.url}/>, container);
 }
