4bb901a9858ea89dce06fc09d73aa84f9bf99a31
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2019-02-16 00:27:54 +0300

    bug 1525719: React: moved Logo component to own directory

--- kuma/javascript/src/index.jsx
+++ kuma/javascript/src/index.jsx
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Logo from './logo.jsx';
+import Logo from './Logo';
 
 const container = document.getElementById('main-header-logo');
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodePath = process.env.NODE_PATH || './node_modules';
 
 module.exports = {
-    mode: "production",
+    mode: 'production',
     entry: path.resolve(__dirname, './kuma/javascript/src/index.jsx'),
     output: {
         filename: 'react.js',
@@ -10,21 +10,22 @@ module.exports = {
     },
     module: {
         rules: [{
-            "test": /\.(js|jsx)$/,
-            "exclude": /node_modules/,
-            "use": {
-                "loader": "babel-loader",
-                "options": {
-                    "presets": [
-                        nodePath + "/@babel/preset-env",
-                        nodePath + "/@babel/preset-react",
-                        nodePath + "/@babel/preset-flow",
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        `${nodePath}/@babel/preset-env`,
+                        `${nodePath}/@babel/preset-react`,
+                        `${nodePath}/@babel/preset-flow`,
                     ]
                 },
             }
         }]
     },
     resolve: {
+        extensions: ['*', '.js', '.jsx'],
         modules: [nodePath]
     },
     resolveLoader: {
