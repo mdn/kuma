a7a2ea68e23e279a8420b2b213923535bd782c50
David Flanagan <dflanagan@mozilla.com>
2019-02-20 11:31:21 -0800

    nodePath tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
-const nodePath = process.env.NODE_PATH || './node_modules';
+
+// When running in docker, we install libraries into /tools/node_modules
+// so that we don't overwrite the user's local kuma/node_modules directory
+// with linux versions of modules (like the binary newrelic module).
+// To make this work, the docker container sets the NODE_PATH environment
+// variable so that code can be found in /tools/node_modules.
+// But we also have to convince webpack and babel to use the /tools
+// directory when we're running in docker, so this nodePath constant
+// is the absolute path to the node_modules director we want to use.
+// We set it on resolve.modules below, and prepend it to the name of
+// all the babel plugins so we're sure we're gettting precisely the
+// file we want both locally (NODE_PATH not set) and in docker.
+//
+// TODO: there ought to be a better way to do this.
+//
+const nodePath = process.env.NODE_PATH || path.join(__dirname, 'node_modules');
 
 module.exports = {
-//    mode: "development",
-    mode: "production",
+    mode: "production",  // Or switch to "development"
     entry: path.resolve(__dirname, './kuma/javascript/src/index.jsx'),
     output: {
         filename: 'react.js',
@@ -17,13 +31,14 @@ module.exports = {
                 "loader": "babel-loader",
                 "options": {
                     "presets": [
-                        "@babel/preset-env",
-                        "@babel/preset-react",
-                        "@babel/preset-flow",
-                        "@emotion/babel-preset-css-prop"
+                        path.join(nodePath, "@babel/preset-env"),
+                        path.join(nodePath, "@babel/preset-react"),
+                        path.join(nodePath, "@babel/preset-flow"),
+                        path.join(nodePath, "@emotion/babel-preset-css-prop")
                     ],
                     "plugins": [
-                        "@babel/plugin-proposal-class-properties"
+                        path.join(nodePath,
+                                  "@babel/plugin-proposal-class-properties")
                     ]
                 }
             }
