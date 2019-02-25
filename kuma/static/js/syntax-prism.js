80139866d94f52760cdb88ceef052ef1d2fff817
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-02-25 15:00:00 +0100

    Bug 1520004: Don’t highlight unknown languages

--- kuma/static/js/syntax-prism.js
+++ kuma/static/js/syntax-prism.js
@@ -7,6 +7,7 @@
     languages.js = languages.javascript;
     languages.cpp = languages.clike;
 
+    // Style all as HTML initially
     var defaultBrush = 'html';
 
     // Treat and highlight PRE elements!
@@ -14,20 +15,25 @@
         var $pre = $(this);
         var klass = $.trim($pre.attr('class'));
 
-        // Split on ';' to accommodate for old line numbering
         var brush = defaultBrush;
         var lineSearch;
 
         // If the PRE has a child <code> tag, it's likely a copy/pasted, already-prism'd code samples.
         // Bail to avoid an error
-        if($pre.find('code').length) {
+        if ($pre.find('code').length) {
             return;
         }
 
         // Parse classname to look for brush
         var brushSearch = klass.match(/brush: ?(.*)/);
-        if(brushSearch && brushSearch[1]) {
-            brush = $.trim(brushSearch[1].replace(';', ' ').split(' ')[0].toLowerCase());
+        if (brushSearch && brushSearch[1]) {
+            // Split on ';' to accommodate for old line numbering
+            brush = $.trim(
+                brushSearch[1]
+                    .replace(';', ' ')
+                    .split(' ')[0]
+                    .toLowerCase()
+            );
         }
 
         if (!$pre.hasClass('no-line-numbers')) {
@@ -35,20 +41,23 @@
             $pre.addClass('line-numbers');
         }
 
-        // Style all as HTML initially
-        $pre.addClass('language-' + defaultBrush);
-
-        // Format PRE content for Prism highlighting
-        $pre.html('<code class="language-' + brush + '">' + $.trim($pre.html()) + '</code>');
+        // Format <pre> content for Prism highlighting
+        $pre.html(
+            '<code class="' +
+                // Don't highlight languages unknown to Prism
+                (brush in languages ? 'language-' + brush : '') +
+                '">' +
+                $.trim($pre.html()) +
+                '</code>'
+        );
 
         // Do we need to highlight any lines?
         // Legacy format: highlight:[8,9,10,11,17,18,19,20]
         lineSearch = klass.match(/highlight:? ?\[(.*)\]/);
-        if(lineSearch && lineSearch[1]) {
+        if (lineSearch && lineSearch[1]) {
             $pre.attr('data-line', lineSearch[1]);
         }
     });
 
     Prism.highlightAll();
-
 })(jQuery);
