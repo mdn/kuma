82fad7bef759d8695135c8d93586d10a711a6a82
David Flanagan <dflanagan@mozilla.com>
2019-03-01 16:05:39 -0800

    Bug 1525719: React spike: try simple client-side navigation
    This PR builds on previous /ducks/ PRs and adds simple pushState/popstate
    client side navigation for MDN wiki pages. The intent isn't to get
    everything working perfectly but to get an idea for what kind of
    speedup we can expect from this technique

--- kuma/javascript/src/index.jsx
+++ kuma/javascript/src/index.jsx
@@ -2,8 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './header/header.jsx';
+import ClientSideNav from './client-side-nav.js';
 
 let container = document.getElementById('react-header');
 if (container) {
     ReactDOM.render(<Header />, container);
 }
+
+ClientSideNav.init();
