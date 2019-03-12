c73ecad852a0cfb415d2784ce344faceae2eb56a
Schalk Neethling <volume4.schalk+github@gmail.com>
2019-03-12 09:24:54 +0200

    Bug 1534298, ensure contributeForm exist before adding event listener (#5293)

--- kuma/static/js/components/payments/focus-visible.js
+++ kuma/static/js/components/payments/focus-visible.js
@@ -10,40 +10,45 @@
         this.removeEventListener('focusout', removeFocusVisible);
     }
 
-    // When a keyup event is triggered on the contribute form,
-    contributeForm.addEventListener('keyup', function(event) {
-        var currentTarget = event.target;
-        var targetType = currentTarget.type;
+    if (contributeForm) {
+        // When a keyup event is triggered on the contribute form,
+        contributeForm.addEventListener('keyup', function(event) {
+            var currentTarget = event.target;
+            var targetType = currentTarget.type;
 
-        /* The key pressed was the tab key, or the left or right
-           arrow keys(used to cycle through radio buttons), and the
-           target field type is one of `radio` */
-        if (
-            (event.key === 'Tab' ||
-                event.key === 'ArrowLeft' ||
-                event.key === 'ArrowRight') &&
-            targetType === 'radio'
-        ) {
-            // get the parent label element
-            var radioButtonLabel = currentTarget.parentElement;
-            // add the `focus-visible` class
-            radioButtonLabel.classList.add('focus-visible');
-            // listen for when the field loses focus,
-            radioButtonLabel.addEventListener('focusout', removeFocusVisible);
-        }
+            /* The key pressed was the tab key, or the left or right
+               arrow keys(used to cycle through radio buttons), and the
+               target field type is one of `radio` */
+            if (
+                (event.key === 'Tab' ||
+                    event.key === 'ArrowLeft' ||
+                    event.key === 'ArrowRight') &&
+                targetType === 'radio'
+            ) {
+                // get the parent label element
+                var radioButtonLabel = currentTarget.parentElement;
+                // add the `focus-visible` class
+                radioButtonLabel.classList.add('focus-visible');
+                // listen for when the field loses focus,
+                radioButtonLabel.addEventListener(
+                    'focusout',
+                    removeFocusVisible
+                );
+            }
 
-        /* The key pressed was the tab key, and the target field
-           type is one of `text`, `email`, or `number */
-        if (
-            event.key === 'Tab' &&
-            (targetType === 'text' ||
-                targetType === 'email' ||
-                targetType === 'number')
-        ) {
-            // add the `focus-visible` class.
-            currentTarget.classList.add('focus-visible');
-            // listen for when the field loses focus,
-            currentTarget.addEventListener('focusout', removeFocusVisible);
-        }
-    });
+            /* The key pressed was the tab key, and the target field
+               type is one of `text`, `email`, or `number */
+            if (
+                event.key === 'Tab' &&
+                (targetType === 'text' ||
+                    targetType === 'email' ||
+                    targetType === 'number')
+            ) {
+                // add the `focus-visible` class.
+                currentTarget.classList.add('focus-visible');
+                // listen for when the field loses focus,
+                currentTarget.addEventListener('focusout', removeFocusVisible);
+            }
+        });
+    }
 })();
