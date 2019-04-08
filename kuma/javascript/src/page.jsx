1cf9858f10ff295ce6e93e3d4ff3a79939401cca
David Flanagan <djf@mozilla.com>
2019-04-08 09:34:28 -0700

    Bug 1525719: make the React Page component responsive. (#5337)
    This patch modifies the page.jsx component to use CSS Grid layout
    and display itself differently on narrow screens than it does on
    larger screens. The main difference is that on narrow screens the
    sidebar is moved to the bottom of the page, and the TOC bar is
    hidden. There are also some font and margin size changes for
    narrow screens as well.
    
    The patch also includes a change to the react_document.html template
    to make the MDN newsletter banner display with the correct margins.

--- kuma/javascript/src/page.jsx
+++ kuma/javascript/src/page.jsx
@@ -7,7 +7,7 @@ import ClockIcon from './icons/clock.svg';
 import ContributorsIcon from './icons/contributors.svg';
 import DocumentProvider from './document-provider.jsx';
 import gettext from './gettext.js';
-import { Row, Strut } from './layout.jsx';
+import { Row } from './layout.jsx';
 import Header from './header/header.jsx';
 
 const strings = {
@@ -16,21 +16,56 @@ const strings = {
     lastUpdatedBy: gettext('Last updated by:')
 };
 
+// A media query that identifies screens narrower than a tablet
+const NARROW = '@media (max-width: 749px)';
+
 const styles = {
+    pageLayout: css({
+        display: 'grid',
+        boxSizing: 'border-box',
+        gridTemplateColumns: '1fr 3fr',
+        gridTemplateAreas: '"title title"  "toc toc"  "side main"',
+        [NARROW]: {
+            // If we're narrower than a tablet, put the sidebar at the
+            // bottom and drop the toc line.
+            gridTemplateColumns: '1fr',
+            gridTemplateAreas: '"title" "main" "side"'
+        }
+    }),
     titlebar: css({
-        backgroundColor: '#f5f9fa',
-        padding: '12px 0 12px 32px'
+        gridArea: 'title',
+        boxSizing: 'border-box',
+        width: '100%',
+        overflowX: 'scroll',
+        backgroundColor: '#f5f9fa'
     }),
     title: css({
         fontFamily:
             'x-locale-heading-primary, zillaslab, "Palatino", "Palatino Linotype", x-locale-heading-secondary, serif',
+        margin: 0,
         fontSize: '2.8rem',
-        margin: 0
+        padding: '12px 12px 12px 24px',
+        [NARROW]: {
+            // Reduce the H1 size on narrow screens
+            fontSize: '1.77rem',
+            padding: 12
+        }
     }),
     toc: css({
+        gridArea: 'toc',
+        boxSizing: 'border-box',
+        width: '100%',
+        overflowX: 'scroll',
         color: '#fff',
         backgroundColor: '#222',
-        padding: '8px 0 8px 32px',
+        padding: '8px 8px 8px 24px',
+        [NARROW]: {
+            // We don't display the TOC bar on small screens.
+            // Leaving it out of the grid is not enough, though:
+            // we also have to explicitly hide it.
+            display: 'none'
+        },
+
         '& li': {
             display: 'inline-block'
         },
@@ -59,20 +94,36 @@ const styles = {
             display: 'none'
         }
     }),
-    content: css({
-        alignItems: 'start',
-        padding: '30px 24px'
-    }),
     article: css({
-        flex: '0 0 74%',
-        maxWidth: '74%',
+        gridArea: 'main',
+        boxSizing: 'border-box',
+        width: '100%',
+        overflowX: 'scroll',
+        // Less padding on the left because the sidebar also has
+        // padding on the right
+        padding: '30px 24px 30px 12px',
+        [NARROW]: {
+            // Except on small screens the sidebar is below, so we
+            // need the same (but overall smaller) padding on both sides.
+            padding: '15px 12px'
+        },
         '& p': {
             maxWidth: '42rem'
         }
     }),
     sidebar: css({
-        flex: '0 0 23%',
-        maxWidth: '23%'
+        gridArea: 'side',
+        boxSizing: 'border-box',
+        width: '100%',
+        overflowX: 'scroll',
+        // Less padding on the right because the article area
+        // has padding on the left, too.
+        padding: '30px 12px 30px 24px',
+        [NARROW]: {
+            // Except that on small screens the sidebar is at the bottom and
+            // so we need the same padding (but less of it) on both sides.
+            padding: '15px 12px'
+        }
     }),
     breadcrumbs: css({}),
     quicklinks: css({}),
@@ -242,15 +293,14 @@ function ArticleMetadata() {
 
 export default function Page() {
     return (
-        <div>
+        <>
             <Header />
-            <Titlebar />
-            <TOC />
-            <Row css={styles.content}>
-                <Sidebar />
-                <Strut width="3%" />
+            <div css={styles.pageLayout}>
+                <Titlebar />
+                <TOC />
                 <Article />
-            </Row>
-        </div>
+                <Sidebar />
+            </div>
+        </>
     );
 }
