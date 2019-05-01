171a790d59cf5ea25c07b17f8967b266b63c5edd
Peter Bengtsson <mail@peterbe.com>
2019-05-01 16:53:34 -0400

    fixes bug 1548082 - hide ES_URLS creds in admin display

--- kuma/core/context_processors.py
+++ kuma/core/context_processors.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from six.moves.urllib.parse import urlparse
 
 from django.conf import settings
 from django.utils import translation
@@ -8,7 +9,28 @@ from .i18n import get_language_mapping
 
 def global_settings(request):
     """Adds settings to the context."""
-    return {'settings': settings}
+
+    def clean_safe_url(url):
+        if '://' not in url:
+            # E.g. 'elasticsearch:9200'
+            url = 'http://' + url
+        parsed = urlparse(url)
+        if '@' in parsed.netloc:
+            parsed = parsed._replace(
+                netloc='username:secret@' + parsed.netloc.split('@')[-1]
+            )
+        return parsed.geturl()
+
+    return {
+        'settings': settings,
+
+        # Because the 'settings.ES_URLS' might contain the username:password
+        # it's never appropriate to display in templates. So clean them up.
+        # But return it as a lambda so it only executes if really needed.
+        'safe_es_urls': lambda: [
+            clean_safe_url(x) for x in settings.ES_URLS
+        ]
+    }
 
 
 def i18n(request):
