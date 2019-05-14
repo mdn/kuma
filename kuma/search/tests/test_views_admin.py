b296f56215fdc79c108b4655d7ea1b9227be9c47
Peter Bengtsson <mail@peterbe.com>
2019-05-14 09:41:40 -0400

    feedbacked

--- kuma/search/tests/test_views_admin.py
+++ kuma/search/tests/test_views_admin.py
@@ -4,23 +4,17 @@ from kuma.core.urlresolvers import reverse
 
 
 @pytest.mark.django_db
-def test_view_indexes(client, settings, wiki_user):
+def test_view_indexes(admin_client, settings):
     """Test that looking at the list of Indexes doesn't reveal the
     username:password from any 'settings.ES_URLS'.
     """
-    wiki_user.is_staff = True
-    wiki_user.is_superuser = True
-    wiki_user.set_password('secret')
-    wiki_user.save()
-    client.login(username=wiki_user.username, password='secret')
-
     settings.ES_URLS = [
         'localhost:9200',
         'https://uuuuser:passsw@remote.example.com:9200/prefix'
     ]
 
     url = reverse('admin:search_index_changelist')
-    response = client.get(url)
+    response = admin_client.get(url)
     assert response.status_code == 200
     assert settings.ES_URLS[0] in response.content
     assert settings.ES_URLS[1] not in response.content
