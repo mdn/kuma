4038eebb7a03e2512f03aa56dbdac86f8f6b3185
rjohnson <rjohnson@mozilla.com>
2019-05-17 13:36:00 -0700

    bug 1525719: fix headless test when run against prod

--- tests/headless/__init__.py
+++ tests/headless/__init__.py
@@ -8,11 +8,11 @@ pytest.register_assert_rewrite('utils.urls')
 # Untrusted attachments and samples domains that are indexed
 INDEXED_ATTACHMENT_DOMAINS = set((
     'mdn.mozillademos.org',          # Main attachments domain
+    'demos.mdn.mozit.cloud',         # Alternate attachments domain (testing)
     'demos-origin.mdn.mozit.cloud',  # Attachments origin
 ))
 
 # Kuma web domains that are indexed
 INDEXED_WEB_DOMAINS = set((
     'developer.mozilla.org',    # Main website, CDN origin
-    'cdn.mdn.mozilla.net',      # Assets CDN
 ))
