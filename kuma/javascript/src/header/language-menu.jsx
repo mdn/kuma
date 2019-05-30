8f2d5498caf125e06d40917859eff83d578063e8
David Flanagan <dflanagan@mozilla.com>
2019-05-30 11:16:37 -0700

    Convert language menu to display language name instead of icon
    This PR deletes the language.svg icon and changes the LanguageMenu
    component to display the name of the current language instead of
    that icon.
    
    The PR also fixes a bug in the Dropdown menu component that was
    requiring the user to click directly on the text of a menu item.
    With the new (and simplified) CSS, a click anywhere within the
    highlighted rectangle will work to trigger the menu item

--- kuma/javascript/src/header/language-menu.jsx
+++ kuma/javascript/src/header/language-menu.jsx
@@ -1,32 +1,37 @@
 //@flow
 import * as React from 'react';
 import { useContext } from 'react';
-import { css } from '@emotion/core';
 
 import DocumentProvider from '../document-provider.jsx';
 import Dropdown from './dropdown.jsx';
-import LanguageIcon from '../icons/language.svg';
-
-const styles = {
-    icon: css({
-        height: 20
-    })
-};
+import { gettext } from '../l10n.js';
 
 export default function LanguageMenu(): React.Node {
-    const documentData = useContext(DocumentProvider.context);
+    const document = useContext(DocumentProvider.context);
+
+    // If there aren't any translations available, don't display anyhing
+    if (!document || document.translations.length === 0) {
+        return null;
+    }
+
+    // For the menu label, we want to use the name of the document language.
+    // We need a special case for English because English documents can
+    // appear in pages for other locales, and in that case we need a
+    // translation for the word "English". In all other cases, the
+    // locale of the page and the locale of the document should match
+    // and we can just use the document language string without translation.
+    let label =
+        document.locale === 'en-US' ? gettext('English') : document.language;
 
     return (
-        documentData && (
-            <Dropdown label={<LanguageIcon css={styles.icon} />} right={true}>
-                {documentData.translations.map(t => (
-                    <li key={t.locale} lang={t.locale}>
-                        <a href={t.url} title={t.localizedLanguage}>
-                            <bdi>{t.language}</bdi>
-                        </a>
-                    </li>
-                ))}
-            </Dropdown>
-        )
+        <Dropdown label={label} right={true}>
+            {document.translations.map(t => (
+                <li key={t.locale} lang={t.locale}>
+                    <a href={t.url} title={t.localizedLanguage}>
+                        <bdi>{t.language}</bdi>
+                    </a>
+                </li>
+            ))}
+        </Dropdown>
     );
 }
