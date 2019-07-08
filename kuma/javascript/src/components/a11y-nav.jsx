18611441f216f94c7f607d2b52019a3803539524
Schalk Neethling <sneethling@mozilla.com>
2019-07-08 18:38:45 +0200

    Bug 1563466, add skip links to document header

---
+++ kuma/javascript/src/components/a11y-nav.jsx
@@ -0,0 +1,26 @@
+// @flow
+import * as React from 'react';
+
+import { gettext } from '../l10n.js';
+
+export default function A11yNav() {
+    return (
+        <ul id="nav-access">
+            <li>
+                <a id="skip-main" href="#content">
+                    {gettext('Skip to main content')}
+                </a>
+            </li>
+            <li>
+                <a id="skip-language" href="#language">
+                    {gettext('Select language')}
+                </a>
+            </li>
+            <li>
+                <a id="skip-search" href="#q">
+                    {gettext('Skip to search')}
+                </a>
+            </li>
+        </ul>
+    );
+}
--- kuma/javascript/src/document.jsx
+++ kuma/javascript/src/document.jsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { css } from '@emotion/core';
 
+import A11yNav from './components/a11y-nav.jsx';
 import Article from './article.jsx';
 import Banners from './banners.jsx';
 import { gettext } from './l10n.js';
@@ -237,6 +238,7 @@ function Content({ document }: DocumentProps) {
 function DocumentPage({ document }: DocumentProps) {
     return (
         <>
+            <A11yNav />
             <Header document={document} />
             <Titlebar title={document.title} document={document} />
             <Breadcrumbs document={document} />
