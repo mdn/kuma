ee2e5e957d03c4ae33cedc9339ded935b57170ca
Peter Bengtsson <mail@peterbe.com>
2019-07-31 16:20:02 -0700

    bug 1562270 - Control files to prettier from config file instead

--- kuma/javascript/src/search-results-page.jsx
+++ kuma/javascript/src/search-results-page.jsx
@@ -182,9 +182,7 @@ export class SearchRoute extends Route<SearchRouteParams, SearchResults> {
                         return response.json();
                     } else {
                         throw new Error(
-                            `${response.status} ${
-                                response.statusText
-                            } fetching ${url}`
+                            `${response.status} ${response.statusText} fetching ${url}`
                         );
                     }
                 })
