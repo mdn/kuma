93049c166a2d4b7d18749ee05369982dac9eadbf
John Giannelos <jgiannelos@mozilla.com>
2019-08-01 18:00:54 +0200

    Backend work (P1-P2) for signal feature on beta

--- kuma/api/v1/serializers.py
+++ kuma/api/v1/serializers.py
@@ -4,16 +4,22 @@ from rest_framework import serializers
 from kuma.wiki.models import BCSignal, Document
 
 
-class BCSignalSerializer(serializers.Serializer):
+class BCSignalSerializer(serializers.ModelSerializer):
     slug = serializers.CharField()
     locale = serializers.CharField()
 
+    class Meta:
+        model = BCSignal
+        fields = (
+            'slug', 'locale', 'browsers',
+            'feature', 'explanation', 'supporting_material'
+        )
+
     def create(self, validated_data):
-        document = Document.objects.filter(
-            slug=validated_data['slug'],
-            locale=validated_data['locale']
-        ).first()
+        slug = validated_data.pop('slug')
+        locale = validated_data.pop('locale')
+        document = Document.objects.filter(slug=slug, locale=locale).first()
 
         if document:
-            return BCSignal.objects.create(document=document)
+            return BCSignal.objects.create(document=document, **validated_data)
         raise exceptions.ValidationError('Document not found')
