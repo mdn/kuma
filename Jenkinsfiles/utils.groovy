0356f013835ea0b17db3f15adddc61e3b44e9b89
limed <limed@mozilla.com>
2019-08-28 17:25:45 -0700

    Setup slack notification for jenkins

--- Jenkinsfiles/utils.groovy
+++ Jenkinsfiles/utils.groovy
@@ -89,6 +89,17 @@ def notify_irc(Map args) {
     sh command
 }
 
+def notify_slack(Map args, credential_id='slack-hook') {
+    def command = "${env.WORKSPACE}/scripts/slack-notify.sh"
+    withCredentials([string(credentialsId: credential_id, variable: 'HOOK')]) {
+        for (arg in args) {
+            command += " --${arg.key} '${arg.value}'"
+        }
+        command += " --hook '${HOOK}'"
+        sh command
+    }
+}
+
 def sh_with_notify(cmd, display, notify_on_success=false) {
     def nick = "mdn-${env.BRANCH_NAME}"
     try {
