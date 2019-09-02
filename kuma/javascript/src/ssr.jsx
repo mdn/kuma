2781324669fea979cbd81152081ec457f78d2b6e
Schalk Neethling <sneethling@mozilla.com>
2019-09-02 16:22:20 +0200

    Fixes #5685, remove Emotion and move CSS to Sass - cleanup (#5710)

--- kuma/javascript/src/ssr.jsx
+++ kuma/javascript/src/ssr.jsx
@@ -56,33 +56,5 @@ export default function ssr(componentName, data) {
             break;
     }
 
-    // TODO: the server-side rendered HTML will include a lot of
-    // separate little stylesheets becasue that is the way Emotion
-    // outputs them when server-side rendering. Arguably, this might be
-    // the fastest way to render content above-the-fold because styles
-    // further down in the document don't need to be parsed until they
-    // are actually needed. But it does seem like it would be inefficient
-    // to have dozens of separate stylesheets to parse. We need data
-    // on this, I suppose.
-    //
-    // I attempted to consolidate the stylesheets with the following
-    // code, but it turned out to break the hydration process because
-    // emotion expect to have all of the separate stylesheets. So if you
-    // uncomment this code, you'll get a single big stylesheet, but the
-    // page will re-render after loading, which is worse than the problem
-    // we were trying to solve:
-    //
-    // Post-process the HTML string we to consolidate
-    // all of the emotion stylesheets into a single one.
-    // let stylesheets = [];
-    // html = html.replace(
-    //     /<style data-emotion-css="[^"]+">(.*?)<\/style>/g,
-    //     function(match, styles) {
-    //         stylesheets.push(styles);
-    //         return '';
-    //     }
-    // );
-    // return `<style>\n${stylesheets.join('\n')}\n</style>\n${html}`;
-    //
     return html;
 }
