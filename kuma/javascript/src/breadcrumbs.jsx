090064a9148037835c18769260391dda5686df7b
Peter Bengtsson <mail@peterbe.com>
2019-09-23 16:46:22 -0400

    correct schema on breadcrumb component
    Fixes #5856

--- kuma/javascript/src/breadcrumbs.jsx
+++ kuma/javascript/src/breadcrumbs.jsx
@@ -10,15 +10,44 @@ type DocumentProps = {
 export default function Breadcrumbs({ document }: DocumentProps) {
     return (
         <nav className="breadcrumbs" role="navigation">
-            <ol>
-                {document.parents.map(p => (
-                    <li key={p.url}>
-                        <a href={p.url} className="breadcrumb-chevron">
-                            {p.title}
+            <ol
+                typeof="BreadcrumbList"
+                vocab="https://schema.org/"
+                aria-label="breadcrumbs"
+            >
+                {document.parents.map((p, i) => (
+                    <li
+                        key={p.url}
+                        property="itemListElement"
+                        typeof="ListItem"
+                    >
+                        <a
+                            href={p.url}
+                            className="breadcrumb-chevron"
+                            property="item"
+                            typeof="WebPage"
+                        >
+                            <span property="name">{p.title}</span>
                         </a>
+                        <meta property="position" content={i + 1} />
                     </li>
                 ))}
-                <li>{document.title}</li>
+                <li property="itemListElement" typeof="ListItem">
+                    <a
+                        href={document.absoluteURL}
+                        className="crumb-current-page"
+                        property="item"
+                        typeof="WebPage"
+                    >
+                        <span property="name" aria-current="page">
+                            {document.title}
+                        </span>
+                    </a>
+                    <meta
+                        property="position"
+                        content={document.parents.length + 1}
+                    />
+                </li>
             </ol>
         </nav>
     );
