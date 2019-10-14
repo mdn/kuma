c3436d034248d78cadcbca788d992c37a9e7cb33
Schalk Neethling <sneethling@mozilla.com>
2019-10-14 15:37:55 +0200

    Removes Gulp and updates docs

--- kuma/core/pipeline/sass.py
+++ kuma/core/pipeline/sass.py
@@ -7,7 +7,7 @@ class DebugSassCompiler(SASSCompiler):
     """
     DEBUG=True replacement for standard SASSCompiler. Same behavior as
     SASSCompiler (e.g. file extension match), but no compilation takes place
-    (handled by gulp).
+    (handled by node-sass).
     """
 
     def compile_file(self, *args, **kwargs):
