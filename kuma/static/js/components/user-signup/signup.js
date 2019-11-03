17ad0027d839eb69c1163e2964728fce6d3aa0fa
Schalk Neethling <sneethling@mozilla.com>
2019-11-03 15:56:18 +0200

    Fixes #5830, show user avatar on create profile

--- kuma/static/js/components/user-signup/signup.js
+++ kuma/static/js/components/user-signup/signup.js
@@ -1,8 +1,17 @@
 (function() {
     'use strict';
-    
+
     var otherEmailInput = document.getElementById('id_other_email');
     var signupForm = document.getElementById('social-signup-form');
+
+    /**
+     * A simple test to determine if `onanimationend` is supported
+     * @returns {Boolean}
+     */
+    function isOnAnimationEndSupported() {
+        var elem = document.createElement('img');
+        return 'onanimationend' in elem;
+    }
 
     /**
      * Hides the specified static content container, shows the edit
@@ -17,14 +26,38 @@
         focusElement.focus();
     }
 
+    /**
+     * Adds checkmark to MDN auth avatar, loads and animates the users Github avatar
+     * @param {HTMLFormElement} form - The form to submit after `onanimationend`
+     */
+    function animateAvatar(form) {
+        var avatarURL = event.target.dataset['avatar'];
+        var mdnProfileImgContainer = document.querySelector('.mdn-profile');
+        var mdnProfileImg = mdnProfileImgContainer.querySelector('img');
+
+        mdnProfileImg.src = avatarURL;
+        mdnProfileImg.addEventListener('load', function() {
+            mdnProfileImgContainer.classList.add('checked');
+            mdnProfileImg.classList.add('animate');
+
+            mdnProfileImg.onanimationend = function() {
+                form.submit();
+            };
+        });
+    }
+
     signupForm.addEventListener('click', function(event) {
         var editContainer;
         var focusElement;
         var staticContainer;
 
         if (event.target.id === 'change-username') {
-            staticContainer = document.getElementById('username-static-container');
-            editContainer = document.getElementById('change-username-container');
+            staticContainer = document.getElementById(
+                'username-static-container'
+            );
+            editContainer = document.getElementById(
+                'change-username-container'
+            );
             focusElement = editContainer.querySelector('input');
             showEditFields(staticContainer, editContainer, focusElement);
         } else if (event.target.id === 'change-email') {
@@ -32,6 +65,14 @@
             editContainer = document.getElementById('change-email-container');
             focusElement = editContainer.querySelector('label');
             showEditFields(staticContainer, editContainer, focusElement);
+        } else if (event.target.id === 'create-mdn-account') {
+            if (isOnAnimationEndSupported()) {
+                event.preventDefault();
+                animateAvatar(signupForm);
+            } else {
+                var mdnProfileImgContainer = document.querySelector('.mdn-profile');
+                mdnProfileImgContainer.classList.add('checked');
+            }
         }
     });
 
@@ -41,7 +82,9 @@
     if (otherEmailInput) {
         otherEmailInput.addEventListener('blur', function(event) {
             if (event.target.validity.valid) {
-                event.target.parentElement.querySelector('input[type="radio"]').checked = true;
+                event.target.parentElement.querySelector(
+                    'input[type="radio"]'
+                ).checked = true;
             }
         });
     }
