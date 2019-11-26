5a9fb57aa304ed0724e7fd1cff89b4540ba0f0de
Peter Bengtsson <mail@peterbe.com>
2019-11-26 06:00:30 -0500

    Remove wiki-compat-signal.js (#6156)
    Fixes #6155
    Fixes #6154

--- kuma/static/js/wiki-compat-trigger.js
+++ kuma/static/js/wiki-compat-trigger.js
@@ -32,20 +32,6 @@
                 cache: true
             }).then(function() {
                 $('.bc-table').mozCompatTable();
-
-                if (window.waffle.flag_is_active('bc-signals')) {
-                    // Load signaling feature
-                    const compatSignalJS = mdn.assets.js['wiki-compat-signal'][0];
-
-                    $.ajax({
-                        url: compatSignalJS,
-                        dataType: 'script',
-                        cache: true
-                    }).fail(function(jqXHR, textStatus, errorThrown) {
-                        console.error('Failed to load BC signal JS: ' + textStatus + ': ', errorThrown);
-                    });
-
-                }
             });
 
         }).appendTo(doc.head);
