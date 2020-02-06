57cb9c728b02425e664f04516cc9c1fa60d86768
Dan Callahan <dan.callahan@gmail.com>
2020-02-06 05:14:03 +0000

    Update whitenoise (3.3.1 -> 5.0.1) (#6424)
    - Updating whitenoise (3.3.1 -> 5.0.1)
      Raw Changelog: https://github.com/evansd/whitenoise/blob/v5.0.1/docs/changelog.rst
      Rendered: http://whitenoise.evans.io/en/stable/changelog.html
    
    For us, the only meaningful behavioral change is that whitenoise now
    generates ETag headers for static assets, using the same algorithm as
    nginx. In practice, this shouldn't change anything as our production
    builds serve static content from CloudFront with hashes in the filenames
    and immutable cache-control headers.
    
    I've also reverted `kuma/wsgi.py` to match what Django generates out of
    the box. Once upon a time we had to customize it to enable Whitenoise,
    but those changes were backed out over two years ago:
    
    https://github.com/mdn/kuma/commit/2526865ecb6272baff58da03a36b0e7b3b2db95d#diff-0b07db8a3993b2a29af197f02dacb31c

--- kuma/wsgi.py
+++ kuma/wsgi.py
@@ -1,8 +1,16 @@
-# Fix the settings so that Whitenoise is happy
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kuma.settings.local')
+"""
+WSGI config for kuma project.
 
-# Enable WhiteNoise
-from django.core.wsgi import get_wsgi_application  # noqa
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kuma.settings.local')
 
 application = get_wsgi_application()
