55586e66f46c8d9e02846dce68dc4393326edc2d
Gregor Weber <gweber@mozilla.com>
2020-02-11 17:19:10 +0100

    Add error IDs and plan inactive error to stripe sys check
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>

--- kuma/users/checks.py
+++ kuma/users/checks.py
@@ -3,16 +3,31 @@ from django.conf import settings
 from django.core.checks import Error, register
 
 
+STRIPE_ERROR_ID = "kuma.users.E001"
+STRIPE_PLAN_INACTIVE_ERROR = "kuma.users.E002"
+
+
 @register()
 def stripe_check(app_configs, **kwargs):
+    errors = []
+
     if settings.STRIPE_SECRET_KEY == "testing" or (
         not settings.STRIPE_SECRET_KEY and not settings.STRIPE_PLAN_ID
     ):
-        return []
+        return errors
 
     try:
-        stripe.Plan.retrieve(settings.STRIPE_PLAN_ID)
-    except stripe.error.StripeError as e:
-        return [Error(f"invalid stripe config: {str(e)}")]
+        plan = stripe.Plan.retrieve(settings.STRIPE_PLAN_ID)
+        if not plan.active:
+            errors.append(
+                Error(
+                    f"{settings.STRIPE_PLAN_ID} points to an inactive Strip plan",
+                    id=STRIPE_PLAN_INACTIVE_ERROR,
+                )
+            )
+    except stripe.error.StripeError as error:
+        errors.append(
+            Error(f"unable to retrieve stripe plan: {error}", id=STRIPE_ERROR_ID)
+        )
 
-    return []
+    return errors
