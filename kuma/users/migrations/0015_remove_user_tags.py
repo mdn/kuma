5c510d34e89ba7b6d2b45a3c9132a07d4551d3ad
Schalk Neethling <sneethling@mozilla.com>
2020-02-12 13:54:08 +0200

    Blacken the generated migration file

--- kuma/users/migrations/0015_remove_user_tags.py
+++ kuma/users/migrations/0015_remove_user_tags.py
@@ -8,12 +8,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0014_auto_20200110_0519'),
+        ("users", "0014_auto_20200110_0519"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='user',
-            name='tags',
-        ),
+        migrations.RemoveField(model_name="user", name="tags",),
     ]
