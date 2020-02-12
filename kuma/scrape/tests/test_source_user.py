ec7425615471d93c3a8f6c670103dabd853f0275
Schalk Neethling <sneethling@mozilla.com>
2020-02-12 21:10:34 +0200

    Remove jQueryUI, interest and expertise fields - Profile cleanup (#6446)
    * Remove jQueryUI, interest and expertise fields - Profile cleanup
    
    * Fix failing Python tests
    
    * blackened
    
    * flake8 and fixes busted common.py
    
    * save user form but do not assign return value to variable
    
    * Remove tags from User object
    
    * Blacken the generated migration file
    
    * Final fixes to common.py

--- kuma/scrape/tests/test_source_user.py
+++ kuma/scrape/tests/test_source_user.py
@@ -28,8 +28,6 @@ def complex_user(db, django_user_model):
         linkedin_url="http://www.linkedin.com/in/jilldev1999",
         mozillians_url="http://mozillians.org/u/jilldev/",
     )
-    user.tags.set_ns("profile:interest:", "JavaScript", "HTML", "CSS")
-    user.tags.set_ns("profile:expertise:", "HTML")
     return user
 
 
@@ -100,8 +98,6 @@ def test_extract_complex(complex_user, client):
         "title": "Web Developer",
         "organization": "Acme, Inc.",
         "location": "Springfield, USA",
-        "interest": ["CSS", "HTML", "JavaScript"],
-        "expertise": ["HTML"],
         "date_joined": datetime(1999, 1, 1, 10, 40, 23),
     }
     assert data == expected_data
@@ -117,8 +113,6 @@ def test_extract_complex_social(complex_user, client):
         "title": "Web Developer",
         "organization": "Acme, Inc.",
         "location": "Springfield, USA",
-        "interest": ["CSS", "HTML", "JavaScript"],
-        "expertise": ["HTML"],
         "twitter_url": "http://twitter.com/jilldev1999",
         "github_url": "https://github.com/jilldev1999",
         "stackoverflow_url": "http://stackoverflow.com/users/1/jilldev1999",
