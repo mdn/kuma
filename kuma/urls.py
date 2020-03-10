42f2ba69526055778e80ebe78f2737f05a112245
Mindy Tchieu <mtchieu@gmail.com>
2020-03-10 17:12:35 -0400

    move payments index url to payments/urls

--- kuma/urls.py
+++ kuma/urls.py
@@ -101,10 +101,7 @@ urlpatterns += i18n_patterns(
         name="redirect-to-payments",
     ),
 )
-urlpatterns += i18n_patterns(re_path(r"^payments/", include(payments_lang_urlpatterns)))
-urlpatterns += i18n_patterns(
-    re_path(r"payments$", payment_views.index, name="payments")
-)
+urlpatterns += i18n_patterns(re_path(r"^payments/?", include(payments_lang_urlpatterns)))
 urlpatterns += i18n_patterns(
     re_path("", decorator_include(never_cache, users_lang_urlpatterns))
 )
