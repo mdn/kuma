ddc26e58e2936ef0755ea15120e36c8bc3853913
Peter Bengtsson <mail@peterbe.com>
2020-03-20 11:52:16 -0400

    cleaning up

--- kuma/core/checks.py
+++ kuma/core/checks.py
@@ -3,12 +3,12 @@ import os
 from json.decoder import JSONDecodeError
 
 from django.conf import settings
-from django.core.checks import Error, register as register_check, Warning
+from django.core.checks import Error, Warning
 
 
-WARNING_MISSING_I18N_DATA = 'kuma.core.W001'
-ERROR_MISSING_I18N_FILE = 'kuma.core.E001'
-ERROR_CORRUPT_I18N_FILE = 'kuma.core.E002'
+WARNING_MISSING_I18N_DATA = "kuma.core.W001"
+ERROR_MISSING_I18N_FILE = "kuma.core.E001"
+ERROR_CORRUPT_I18N_FILE = "kuma.core.E002"
 
 
 def react_i18n_check(app_configs, **kwargs):
@@ -23,42 +23,38 @@ def react_i18n_check(app_configs, **kwargs):
     general_hint = "Run 'docker-compose exec web make build-static'"
 
     for locale in settings.ACCEPTED_LOCALES:
-        path = os.path.join(
-            settings.BASE_DIR, 'static', 'jsi18n', locale, 'react.json')
+        path = os.path.join(settings.BASE_DIR, "static", "jsi18n", locale, "react.json")
         try:
             with open(path) as f:
                 try:
                     data = json.load(f)
                 except JSONDecodeError as exception:
-                    errors.append(Error(
-                        'Locale file {} is corrupt ({})'.format(
-                            path, exception
-                        ),
-                        hint=general_hint,
-                        id=ERROR_CORRUPT_I18N_FILE,
-                    ))
+                    errors.append(
+                        Error(
+                            "Locale file {} is corrupt ({})".format(path, exception),
+                            hint=general_hint,
+                            id=ERROR_CORRUPT_I18N_FILE,
+                        )
+                    )
                     continue
 
                 missing = [
-                    key for key in ('catalog', 'plural', 'formats')
-                    if key not in data
+                    key for key in ("catalog", "plural", "formats") if key not in data
                 ]
                 if missing:
-                    errors.append(Warning(
-                        'Locale file {} is missing keys {!r}'.format(
-                            path, missing
-                        ),
-                        hint=general_hint,
-                        id=WARNING_MISSING_I18N_DATA,
-                    ))
+                    errors.append(
+                        Warning(
+                            "Locale file {} is missing keys {!r}".format(path, missing),
+                            hint=general_hint,
+                            id=WARNING_MISSING_I18N_DATA,
+                        )
+                    )
         except FileNotFoundError:
-            errors.append(Error(
-                'Locale file {} does not exist'.format(path),
-                hint=general_hint,
-                id=ERROR_MISSING_I18N_FILE,
-            ))
+            errors.append(
+                Error(
+                    "Locale file {} does not exist".format(path),
+                    hint=general_hint,
+                    id=ERROR_MISSING_I18N_FILE,
+                )
+            )
     return errors
-
-
-def register():
-    register_check(react_i18n_check)
