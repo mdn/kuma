6fac8aad717aa3f7e80ff13fbe791cddeab899d6
Peter Bengtsson <mail@peterbe.com>
2020-03-26 13:53:07 -0400

    progress

--- kuma/javascript/src/payments/subscription-form.jsx
+++ kuma/javascript/src/payments/subscription-form.jsx
@@ -1,29 +1,58 @@
 // @flow
 import * as React from 'react';
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 
 import { getLocale, gettext, Interpolated } from '../l10n.js';
+import UserProvider from '../user-provider.jsx';
 
 export default function SubscriptionForm() {
+    const userData = useContext(UserProvider.context);
     const locale = getLocale();
+
     const subscriptionFormRef = useRef(null);
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [paymentAuthorized, setPaymentAuthorized] = useState(false);
+
+    const [redirectingTo, setRedirectingTo] = useState(null);
+
+    useEffect(() => {
+        let dismounted = false;
+        if (redirectingTo) {
+            window.setTimeout(() => {
+                window.location.href = redirectingTo;
+                if (!dismounted) {
+                    setRedirectingTo(null);
+                }
+            }, 300000);
+        }
+        return () => {
+            dismounted = true;
+        };
+    }, [redirectingTo]);
 
     const toggleButton = event => {
         setPaymentAuthorized(event.target.checked);
     };
 
+    /**
+     * If you arrived on this page, being anonymous, you'd have to first sign in.
+     * Suppose that you do that, we will make sure to send you back to this page
+     * with the '#continuestripe' hash.
+     * Basically, if this is your location hash, it will, for you, check the
+     * checkbox and press the "Continue" button.
+     */
     useEffect(() => {
-        if (window.location.hash === '#continuestripe') {
+        if (
+            window.location.hash === '#continuestripe' &&
+            userData.isAuthenticated
+        ) {
             const subscriptionForm = subscriptionFormRef.current;
             if (subscriptionForm) {
                 setPaymentAuthorized(true);
-                subscriptionForm.submit();
-                // console.log('FORM:', subscriptionForm);
+                initStripeForm();
             }
         }
-    }, []);
+    }, [userData.isAuthenticated]);
 
     /**
      * Opens Stripe modal allowing a user to complete their subscription.
@@ -32,33 +61,45 @@ export default function SubscriptionForm() {
     const submit = event => {
         event.preventDefault();
 
-        const subscriptionForm = subscriptionFormRef.current;
-        if (subscriptionForm) {
-            const formData = new FormData(subscriptionForm);
-            setIsSubmitting(true);
-
-            const stripeHandler = window.StripeCheckout.configure({
-                key: window.mdn.stripePublicKey,
-                locale: 'auto',
-                name: 'MDN Web Docs',
-                zipCode: true,
-                currency: 'usd',
-                amount: 500,
-                email: '',
-                token: function(response) {
-                    formData.set('stripe_token', response.id);
-                    subscriptionForm.submit();
-                },
-                closed: function() {
-                    if (!formData.get('stripe_token')) {
-                        setIsSubmitting(false);
-                    }
-                }
-            });
-
-            stripeHandler.open();
+        if (!userData.isAuthenticated) {
+            const next = encodeURIComponent(
+                window.location.pathname + '#continuestripe'
+            );
+            setRedirectingTo(
+                `/${locale}/users/account/signup-landing?next=${next}`
+            );
+            return;
         }
+
+        initStripeForm();
     };
+
+    function initStripeForm() {
+        const subscriptionForm = subscriptionFormRef.current;
+        const formData = new FormData(subscriptionForm);
+        setIsSubmitting(true);
+
+        const stripeHandler = window.StripeCheckout.configure({
+            key: window.mdn.stripePublicKey,
+            locale: 'auto',
+            name: 'MDN Web Docs',
+            zipCode: true,
+            currency: 'usd',
+            amount: 500,
+            email: '',
+            token: function(response) {
+                formData.set('stripe_token', response.id);
+                subscriptionForm.submit();
+            },
+            closed: function() {
+                if (!formData.get('stripe_token')) {
+                    setIsSubmitting(false);
+                }
+            }
+        });
+
+        stripeHandler.open();
+    }
 
     return (
         <div className="subscriptions-form">
@@ -70,50 +111,56 @@ export default function SubscriptionForm() {
                     />
                 </h2>
             </header>
-            <form
-                ref={subscriptionFormRef}
-                name="subscription-form"
-                method="post"
-                onSubmit={submit}
-                disabled={isSubmitting}
-            >
-                <label className="payment-opt-in">
-                    <input
-                        type="checkbox"
-                        required="required"
-                        checked={paymentAuthorized}
-                        onChange={toggleButton}
-                    />
-                    <small>
-                        <Interpolated
-                            id={gettext(
-                                'By clicking this button, I authorize Mozilla to charge this payment method each month, according to the <paymentTermsLink />, until I cancel my subscription.'
-                            )}
-                            paymentTermsLink={
-                                <a
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    href={`/${locale}/payments/terms/`}
-                                >
-                                    {gettext('Payment Terms')}
-                                </a>
-                            }
-                        />
-                    </small>
-                </label>
-                <input type="hidden" name="stripe_token" />
-                <input type="hidden" name="stripe_email" />
-                <button
-                    type="submit"
-                    className="button cta primary"
-                    disabled={!paymentAuthorized}
+            {redirectingTo ? (
+                <p className="redirecting">
+                    <i>Redirecting to sign in...</i>
+                </p>
+            ) : (
+                <form
+                    ref={subscriptionFormRef}
+                    name="subscription-form"
+                    method="post"
+                    onSubmit={submit}
+                    disabled={isSubmitting}
                 >
-                    {gettext('Continue')}
-                </button>
-                <small className="subtext">
-                    {gettext('Payments are not tax deductible')}
-                </small>
-            </form>
+                    <label className="payment-opt-in">
+                        <input
+                            type="checkbox"
+                            required="required"
+                            checked={paymentAuthorized}
+                            onChange={toggleButton}
+                        />
+                        <small>
+                            <Interpolated
+                                id={gettext(
+                                    'By clicking this button, I authorize Mozilla to charge this payment method each month, according to the <paymentTermsLink />, until I cancel my subscription.'
+                                )}
+                                paymentTermsLink={
+                                    <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href={`/${locale}/payments/terms/`}
+                                    >
+                                        {gettext('Payment Terms')}
+                                    </a>
+                                }
+                            />
+                        </small>
+                    </label>
+                    <input type="hidden" name="stripe_token" />
+                    <input type="hidden" name="stripe_email" />
+                    <button
+                        type="submit"
+                        className="button cta primary"
+                        disabled={!paymentAuthorized}
+                    >
+                        {gettext('Continue')}
+                    </button>
+                    <small className="subtext">
+                        {gettext('Payments are not tax deductible')}
+                    </small>
+                </form>
+            )}
         </div>
     );
 }
