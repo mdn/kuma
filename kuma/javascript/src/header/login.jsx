3bcb9f3d958d63051efe427030e51de4cd3867e3
Peter Bengtsson <mail@peterbe.com>
2020-03-26 14:07:23 -0400

    not trigger 'Started sign-in' on click
    Fixes #6747

--- kuma/javascript/src/header/login.jsx
+++ kuma/javascript/src/header/login.jsx
@@ -5,10 +5,8 @@ import { useContext } from 'react';
 import Dropdown from './dropdown.jsx';
 import { getLocale, gettext } from '../l10n.js';
 import UserProvider from '../user-provider.jsx';
-import GAProvider from '../ga-provider.jsx';
 
 export default function Login(): React.Node {
-    const ga = useContext(GAProvider.context);
     const locale = getLocale();
     const userData = useContext(UserProvider.context);
 
@@ -22,22 +20,6 @@ export default function Login(): React.Node {
     // server side rendering, but this code will never run during
     // server side rendering because we won't have user data then.
     const LOCATION = window.location.pathname;
-
-    /**
-     * Send a signal to GA when a user clicks on the Sing In
-     * lnk in the header.
-     * @param {Object} event - The event object that was triggered
-     */
-    function sendSignInEvent(event) {
-        const service = event.target.dataset.service;
-
-        ga('send', {
-            hitType: 'event',
-            eventCategory: 'Authentication',
-            eventAction: 'Started sign-in',
-            eventLabel: service
-        });
-    }
 
     if (userData.isAuthenticated && userData.username) {
         // If we have user data and the user is logged in, show their
@@ -85,10 +67,8 @@ export default function Login(): React.Node {
         return (
             <a
                 href={`/${locale}/users/account/signup-landing?next=${LOCATION}`}
-                data-service="GitHub"
                 rel="nofollow"
                 className="signin-link"
-                onClick={sendSignInEvent}
             >
                 {gettext('Sign in')}
             </a>
