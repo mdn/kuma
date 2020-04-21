59a97707056341fb0228be9f0cb42648bfdb4c4f
Schalk Neethling <sneethling@mozilla.com>
2020-04-21 19:18:13 +0200

    Responsive toc

--- kuma/javascript/src/toc.jsx
+++ kuma/javascript/src/toc.jsx
@@ -5,12 +5,15 @@ import { useContext, useEffect, useRef } from 'react';
 import GAProvider from './ga-provider.jsx';
 import { gettext } from './l10n.js';
 
+import Caret from './icons/caret-down.svg';
+
 type Props = {
     html: string,
 };
 
 export default function TOC({ html }: Props) {
     const ga = useContext(GAProvider.context);
+    const documentTOCRef = useRef(null);
     const tocRef = useRef(null);
 
     /**
@@ -32,8 +35,19 @@ export default function TOC({ html }: Props) {
         }
     }
 
+    /**
+     * Show or hide the table of contents on
+     * mobile devices
+     */
+    function toggleTOC() {
+        const toc = tocRef.current;
+        if (toc) {
+            toc.classList.toggle('show-toc');
+        }
+    }
+
     useEffect(() => {
-        const documentToc = tocRef.current;
+        const documentToc = documentTOCRef.current;
 
         if (documentToc) {
             documentToc.addEventListener('click', sendTOCClicks);
@@ -48,11 +62,20 @@ export default function TOC({ html }: Props) {
 
     return (
         <aside className="document-toc-container">
-            <section className="document-toc" ref={tocRef}>
+            <section className="document-toc" ref={documentTOCRef}>
                 <header>
                     <h2>{gettext('On this Page')}</h2>
+                    <button
+                        type="button"
+                        className="toc-trigger-mobile"
+                        onClick={toggleTOC}
+                    >
+                        {gettext('Jump to section')}
+                        <Caret />
+                    </button>
                 </header>
                 <ul
+                    ref={tocRef}
                     dangerouslySetInnerHTML={{
                         __html: html,
                     }}
