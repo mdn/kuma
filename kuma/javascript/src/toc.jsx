8a501a3a47439d0d65eeb114202c35115f91b8f5
Schalk Neethling <sneethling@mozilla.com>
2020-04-23 14:15:45 +0200

    Responsive toc (#6928)
    * Responsive toc
    
    * Small tweak to toc width on tablet
    
    * Changes based on review feedback
    
    * Use calc for toc width
    
    * Changes based on review feedback
    
    * Only disable specific eslint rules

--- kuma/javascript/src/toc.jsx
+++ kuma/javascript/src/toc.jsx
@@ -1,9 +1,11 @@
 // @flow
 import * as React from 'react';
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useState } from 'react';
 
 import GAProvider from './ga-provider.jsx';
 import { gettext } from './l10n.js';
+
+import Caret from './icons/caret-down.svg';
 
 type Props = {
     html: string,
@@ -11,7 +13,7 @@ type Props = {
 
 export default function TOC({ html }: Props) {
     const ga = useContext(GAProvider.context);
-    const tocRef = useRef(null);
+    const [showTOC, setShowTOC] = useState(false);
 
     /**
      * Send a signal to GA when the user clicks on one of links
@@ -32,27 +34,31 @@ export default function TOC({ html }: Props) {
         }
     }
 
-    useEffect(() => {
-        const documentToc = tocRef.current;
-
-        if (documentToc) {
-            documentToc.addEventListener('click', sendTOCClicks);
-        }
-
-        return () => {
-            if (documentToc) {
-                documentToc.removeEventListener('click', sendTOCClicks);
-            }
-        };
-    });
+    /**
+     * Show or hide the table of contents on
+     * mobile devices
+     */
+    function toggleTOC() {
+        setShowTOC(!showTOC);
+    }
 
     return (
         <aside className="document-toc-container">
-            <section className="document-toc" ref={tocRef}>
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+            <section className="document-toc" onClick={sendTOCClicks}>
                 <header>
                     <h2>{gettext('On this Page')}</h2>
+                    <button
+                        type="button"
+                        className="toc-trigger-mobile"
+                        onClick={toggleTOC}
+                    >
+                        {gettext('Jump to section')}
+                        <Caret />
+                    </button>
                 </header>
                 <ul
+                    className={showTOC ? 'show-toc' : undefined}
                     dangerouslySetInnerHTML={{
                         __html: html,
                     }}
