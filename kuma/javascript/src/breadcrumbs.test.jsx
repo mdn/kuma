2893bcc6453936c48fc97c0a5d1171473368da72
Mindy Tchieu <mtchieu@gmail.com>
2020-04-28 17:36:04 -0400

    update test

---
+++ kuma/javascript/src/breadcrumbs.test.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import {
+    toBeInTheDocument,
+    toHaveClass,
+} from '@testing-library/jest-dom/matchers';
+import GAProvider from './ga-provider.jsx';
+import Breadcrumbs from './breadcrumbs.jsx';
+
+expect.extend({ toBeInTheDocument, toHaveClass });
+
+describe('Breadcrumbs', () => {
+    const mockDocumentData = {
+        // url needs a # to get around a JSDOM error:
+        // Error: Not implemented: navigation (except hash changes)
+        absoluteURL: '#main-url',
+        title: 'main crumb',
+        parents: [
+            {
+                title: 'crumb 1',
+                url: '/url-1',
+            },
+            {
+                title: 'crumb 2',
+                url: '/url-2',
+            },
+            {
+                title: 'crumb 3',
+                url: '/url-3',
+            },
+        ],
+    };
+
+    it('renders correct number of crumbs (parents + current page)', () => {
+        const expected = mockDocumentData.parents.length + 1;
+        const { queryAllByText } = render(
+            <Breadcrumbs document={mockDocumentData} />
+        );
+        const items = queryAllByText(/crumb/);
+        expect(items).toHaveLength(expected);
+    });
+
+    it('gives the last parent a different class name, so we can style it differently for small screens', () => {
+        const expected = 'breadcrumb-chevron';
+        const firstParent = mockDocumentData.parents[0];
+        const lastParent =
+            mockDocumentData.parents[mockDocumentData.parents.length - 1];
+        const { queryByText } = render(
+            <Breadcrumbs document={mockDocumentData} />
+        );
+        const firstElement = queryByText(firstParent.title).closest('a');
+        const lastElement = queryByText(lastParent.title).closest('a');
+
+        expect(firstElement).toHaveClass(expected);
+        expect(lastElement).not.toHaveClass(expected);
+    });
+
+    it('logs GA event for every crumb click', () => {
+        const mockGA = jest.fn();
+        const { queryByText } = render(
+            <GAProvider.context.Provider value={mockGA}>
+                <Breadcrumbs document={mockDocumentData} />
+            </GAProvider.context.Provider>
+        );
+        const link = queryByText(mockDocumentData.title).closest('a');
+
+        // click on current crumb
+        fireEvent.click(link);
+
+        // check that our GA event was called
+        expect(mockGA).toHaveBeenCalledWith('send', {
+            eventAction: 'Crumbs',
+            eventCategory: 'Wiki',
+            eventLabel: `${location.href}${mockDocumentData.absoluteURL}`,
+            hitType: 'event',
+        });
+    });
+});
