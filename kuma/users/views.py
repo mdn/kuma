e71f842c8532954e9d6f1b11d348f438f9480fef
Gregor <gregorwbr@gmail.com>
2020-04-28 23:11:58 +0200

    Read subscription invoice total from stripe object (#6965)
    * Read subscription invoice total from stripe object
    
    * Mock invoice total amount for testing invoice email

--- kuma/users/views.py
+++ kuma/users/views.py
@@ -856,10 +856,8 @@ def stripe_hooks(request):
     # to deal with here.
 
     if event.type == "invoice.payment_succeeded":
-        payment_intent = event.data.object
-        _send_payment_received_email(
-            payment_intent, request.LANGUAGE_CODE,
-        )
+        invoice = event.data.object
+        _send_payment_received_email(invoice, request.LANGUAGE_CODE)
         track_event(
             CATEGORY_MONTHLY_PAYMENTS,
             ACTION_SUBSCRIPTION_CREATED,
@@ -880,15 +878,15 @@ def stripe_hooks(request):
     return HttpResponse()
 
 
-def _send_payment_received_email(payment_intent, locale):
-    user = get_user_model().objects.get(stripe_customer_id=payment_intent.customer)
+def _send_payment_received_email(invoice, locale):
+    user = get_user_model().objects.get(stripe_customer_id=invoice.customer)
     subscription_info = retrieve_and_synchronize_subscription_info(user)
     locale = locale or settings.WIKI_DEFAULT_LANGUAGE
     context = {
-        "payment_date": datetime.fromtimestamp(payment_intent.created),
+        "payment_date": datetime.fromtimestamp(invoice.created),
         "next_payment_date": subscription_info["next_payment_at"],
-        "invoice_number": payment_intent.number,
-        "cost": settings.CONTRIBUTION_AMOUNT_USD,
+        "invoice_number": invoice.number,
+        "cost": invoice.total / 100,
         "credit_card_brand": subscription_info["brand"],
         "manage_subscription_url": absolutify(reverse("recurring_payment_management")),
         "faq_url": absolutify(reverse("payments_index")),
@@ -906,8 +904,8 @@ def _send_payment_received_email(payment_intent, locale):
             settings.DEFAULT_FROM_EMAIL,
             [user.email],
             attachment={
-                "name": os.path.basename(urlparse(payment_intent.invoice_pdf).path),
-                "bytes": _download_from_url(payment_intent.invoice_pdf),
+                "name": os.path.basename(urlparse(invoice.invoice_pdf).path),
+                "bytes": _download_from_url(invoice.invoice_pdf),
                 "mime": "application/pdf",
             },
         )
