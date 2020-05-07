2b6d32236f36846d1056139844f94809d3425be1
Gregor <gregorwbr@gmail.com>
2020-05-07 17:21:34 +0200

    Add command for exporting subscribers to sendinblue (#6967)
    * Add command for exporting subscribers to sendinblue
    
    * Update kuma/users/management/commands/export_to_sendinblue.py
    
    Co-Authored-By: Peter Bengtsson <peterbe@mozilla.com>
    
    * Use Django ORM for querying paying users
    
    * remove unused fn
    
    * use requests json and retry requests
    
    * don't shadow stdlib's list
    
    * Update kuma/users/management/commands/export_to_sendinblue.py
    
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>
    
    * fix list reference
    
    * add sendinblue config keys, system check and module
    
    * set default for sendinblue config list ids
    
    * fix lint
    
    * stop casting sendinblue IDs
    
    * add options back to the sendinblue command
    
    * add docs for setting up sendinblue
    
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>

---
+++ kuma/users/sendinblue.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+
+from kuma.core.utils import requests_retry_session
+
+
+API_URL = "https://api.sendinblue.com/v3/"
+
+
+def request(method, path, **kwargs):
+    return requests_retry_session().request(
+        method,
+        API_URL + path,
+        headers={"api-key": settings.SENDINBLUE_API_KEY, "accept": "application/json"},
+        **kwargs,
+    )
