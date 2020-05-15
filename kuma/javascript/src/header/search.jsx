e64bb17215919e494a30caea09abd15ae4420c3f
Mindy Tchieu <mtchieu@mozilla.com>
2020-05-14 22:03:45 -0400

    Update mobile search interaction (#7052)

--- kuma/javascript/src/header/search.jsx
+++ kuma/javascript/src/header/search.jsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from 'react';
 
 import { getLocale, gettext } from '../l10n.js';
 import SearchIcon from '../icons/search.svg';
+import CloseIcon from '../icons/close.svg';
 
 type Props = {
     initialQuery: string,
@@ -11,38 +12,67 @@ type Props = {
 
 export default function Search({ initialQuery }: Props) {
     const locale = getLocale();
+    const [showForm, setShowForm] = React.useState(false);
+    const [query, setQuery] = React.useState(initialQuery);
 
     // After our first render, set the input field's initial value
+    // and show search form
     const inputfield = useRef(null);
     useEffect(() => {
-        if (inputfield.current && initialQuery) {
-            inputfield.current.value = initialQuery;
+        if (inputfield.current && query) {
+            inputfield.current.value = query;
+            setShowForm(true);
         }
-    }, [initialQuery]);
+    }, [query, showForm]);
+
+    const handleClick = (event) => {
+        event.preventDefault();
+
+        if (showForm === true) {
+            setShowForm(false);
+            setQuery(null);
+            if (inputfield.current) {
+                inputfield.current.value = '';
+            }
+        } else {
+            setShowForm(true);
+            inputfield.current && inputfield.current.focus();
+        }
+    };
 
     return (
-        <form
-            className="header-search"
-            id="nav-main-search"
-            action={`/${locale}/search`}
-            method="get"
-            role="search"
-        >
-            <SearchIcon className="search-icon" />
+        <div className={`header-search ${showForm ? 'show-form' : ''}`}>
+            <form
+                id="nav-main-search"
+                action={`/${locale}/search`}
+                method="get"
+                role="search"
+            >
+                <SearchIcon className="search-icon" />
 
-            <label htmlFor="main-q" className="visually-hidden">
-                {gettext('Search MDN')}
-            </label>
-            <input
-                className="search-input-field"
-                ref={inputfield}
-                type="search"
-                id="main-q"
-                name="q"
-                placeholder={gettext('Search MDN')}
-                pattern="(.|\s)*\S(.|\s)*"
-                required
-            />
-        </form>
+                <label htmlFor="main-q" className="visually-hidden">
+                    {gettext('Search MDN')}
+                </label>
+                <input
+                    className="search-input-field"
+                    ref={inputfield}
+                    type="search"
+                    id="main-q"
+                    name="q"
+                    placeholder={gettext('Search MDN')}
+                    pattern="(.|\s)*\S(.|\s)*"
+                    required
+                />
+            </form>
+            <button className="toggle-form" onClick={handleClick}>
+                <CloseIcon className="close-icon" />
+                <SearchIcon className="search-icon" />
+                <span>
+                    {showForm
+                        ? gettext('Close search')
+                        : gettext('Open search')}
+                </span>
+            </button>
+        </div>
     );
 }
