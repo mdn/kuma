2bcf29940ea1c819a7e4a4ee50531f8541468f8b
Gregor Weber <gweber@mozilla.com>
2020-06-23 15:50:28 +0200

    send username to sendinblue on user change

--- kuma/users/newsletter/tasks.py
+++ kuma/users/newsletter/tasks.py
@@ -22,6 +22,7 @@ def create_or_update_contact(user_pk):
             "updateEnabled": True,
             "email": user.email,
             "attributes": {
+                "USERNAME": user.username,
                 "IS_PAYING": UserSubscription.objects.filter(
                     user=user, canceled__isnull=True
                 ).exists()
