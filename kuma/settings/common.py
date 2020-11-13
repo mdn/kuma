dd5f7b16a4131dfb15beb0c3b9503a8d4a82dc1c
rjohnson <rjohnson@mozilla.com>
2020-11-12 16:01:52 -0800

    update sub-modules

--- kuma/settings/common.py
+++ kuma/settings/common.py
@@ -593,7 +593,7 @@ TEMPLATES = [
 ]
 
 PUENTE = {
-    "VERSION": "2020.28",
+    "VERSION": "2020.29",
     "BASE_DIR": BASE_DIR,
     "TEXT_DOMAIN": "django",
     # Tells the extract script what files to look for l10n in and what function
