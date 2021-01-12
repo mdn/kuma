513948bc49a006da5d8cf02c0eef77842f6ed1a4
Peter Bengtsson <mail@peterbe.com>
2021-01-12 10:48:52 -0500

    remove render_stale_documents

--- kuma/wiki/apps.py
+++ kuma/wiki/apps.py
@@ -1,8 +1,6 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-from kuma.celery import app
-
 
 class WikiConfig(AppConfig):
     """
@@ -17,8 +15,3 @@ class WikiConfig(AppConfig):
         """Configure kuma.wiki after models are loaded."""
         # Register signal handlers
         from . import signal_handlers  # noqa
-
-        # Render stale documents: every 60 minutes
-        from kuma.wiki.tasks import render_stale_documents
-
-        app.add_periodic_task(60 * 60, render_stale_documents.s())
