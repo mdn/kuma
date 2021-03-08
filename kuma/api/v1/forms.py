ab195450d7a1ea710392c890e7e6cd759d49d7d2
Ryan Johnson <escattone@gmail.com>
2021-03-08 15:03:21 -0800

    Update kuma/api/v1/forms.py

--- kuma/api/v1/forms.py
+++ kuma/api/v1/forms.py
@@ -7,7 +7,7 @@ class AccountSettingsForm(forms.Form):
         required=False,
         # The `settings.LANGUAGES` looks like this:
         #   [('en-US', 'English (US)'), ...]
-        # But the valid choices actually comes from Yari who is the only
+        # But the valid choices actually come from Yari which is the only
         # one that knows which ones are really valid.
         # Here in kuma we can't enforce it as string. But at least we
         # have a complete list of all possible languages
