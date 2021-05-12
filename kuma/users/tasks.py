8d5d7a74138f290d48a8c345f6a4b57651d7428d
Peter Bengtsson <mail@peterbe.com>
2021-05-12 16:46:03 -0400

    Refactorings for welcome HTML email (#7869)
    Fixes #7868

--- kuma/users/tasks.py
+++ kuma/users/tasks.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils import translation
 from django.utils.translation import gettext_lazy as _
+from pyquery import PyQuery as pq
 
 from kuma.core.decorators import skip_in_maintenance_mode
 from kuma.core.email_utils import render_email
@@ -48,8 +49,18 @@ def send_welcome_email(user_pk, locale):
         context = {"username": user.username}
         log.debug("Using the locale %s to send the welcome email", locale)
         with translation.override(locale):
-            content_plain = render_email("users/email/welcome/plain.ltxt", context)
             content_html = render_email("users/email/welcome/html.ltxt", context)
+            doc = pq(content_html)
+            urls = []
+            for i, link in enumerate(doc("body a[href]").items()):
+                link.text(f"{link.text()}[{i + 1}]")
+                urls.append((i + 1, link.attr("href")))
+
+            content_plain = doc("body").text().replace("\n", "\n\n")
+            if urls:
+                content_plain += "\n\n"
+                for i, url in urls:
+                    content_plain += f"[{i}] {url}\n"
 
             email = EmailMultiAlternativesRetrying(
                 _("Getting started with your new MDN account"),
