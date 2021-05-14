2c91e285262b9c5d9d545f387634c10c5e323f4c
rjohnson <rjohnson@mozilla.com>
2021-05-14 13:32:38 -0700

    deletions 1

--- webpack-build-utils/presets/webpack.hardsource.js
+++
@@ -1,6 +0,0 @@
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-
-// for more on this plugin see https://github.com/mzgoddard/hard-source-webpack-plugin
-module.exports = () => ({
-    plugins: [new HardSourceWebpackPlugin()],
-});
