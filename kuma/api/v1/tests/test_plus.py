0c20ed713e2585776a4e6740d1e94e75344f156c
Peter Bengtsson <mail@peterbe.com>
2021-05-19 17:35:09 -0400

    fix tests

--- kuma/api/v1/tests/test_plus.py
+++ kuma/api/v1/tests/test_plus.py
@@ -8,10 +8,13 @@ from kuma.plus.models import LandingPageSurvey
 
 @pytest.mark.django_db
 def test_ping_landing_page_survey_happy_path(client):
+    # This sets the needed session cookie
+    variant_url = reverse("api.v1.plus.landing_page_variant")
+    response = client.get(variant_url)
+    assert response.status_code == 200
+
     url = reverse("api.v1.plus.landing_page_survey")
-    response = client.get(
-        url, {"variant": 1}, HTTP_CLOUDFRONT_VIEWER_COUNTRY_NAME="Antartica"
-    )
+    response = client.get(url, HTTP_CLOUDFRONT_VIEWER_COUNTRY_NAME="Antartica")
     assert response.status_code == 200
     (result,) = LandingPageSurvey.objects.all()
     assert result.variant == 1
@@ -22,7 +25,7 @@ def test_ping_landing_page_survey_happy_path(client):
     # Now fill it with an email
     response = client.post(
         url,
-        {"variant": 1, "uuid": str(result.uuid), "email": " peterbe@example.com  "},
+        {"uuid": str(result.uuid), "email": " peterbe@example.com  "},
     )
     assert response.status_code == 200
     (result,) = LandingPageSurvey.objects.all()
@@ -34,7 +37,6 @@ def test_ping_landing_page_survey_happy_path(client):
     response = client.post(
         url,
         {
-            "variant": 1,
             "uuid": str(result.uuid),
             "response": json.dumps({"price": "perfect"}),
         },
@@ -54,32 +56,34 @@ def test_ping_landing_page_survey_bad_request(client):
     response = client.get(url)
     assert response.status_code == 400
 
-    # Not a valid number
-    response = client.get(url, {"variant": "xxx"})
-    assert response.status_code == 400
+    # This sets the needed session cookie
+    variant_url = reverse("api.v1.plus.landing_page_variant")
+    response = client.get(variant_url)
+    assert response.status_code == 200
 
     # Not a valid UUID
-    response = client.get(url, {"variant": 1, "uuid": "xxx"})
+    response = client.get(url, {"uuid": "xxx"})
     assert response.status_code == 400
 
     # Not a recognized UUID
-    response = client.get(
-        url, {"variant": 1, "uuid": "88f7a689-454a-4647-99bf-d62fa66da24a"}
-    )
+    response = client.get(url, {"uuid": "88f7a689-454a-4647-99bf-d62fa66da24a"})
     assert response.status_code == 404
 
 
 @pytest.mark.django_db
 def test_ping_landing_page_survey_reuse_uuid(client):
+    # This sets the needed session cookie
+    variant_url = reverse("api.v1.plus.landing_page_variant")
+    response = client.get(variant_url)
+    assert response.status_code == 200
+
     url = reverse("api.v1.plus.landing_page_survey")
-    response1 = client.get(
-        url, {"variant": 1}, HTTP_CLOUDFRONT_VIEWER_COUNTRY_NAME="Sweden"
-    )
+    response1 = client.get(url, HTTP_CLOUDFRONT_VIEWER_COUNTRY_NAME="Sweden")
     assert response1.status_code == 200
     assert LandingPageSurvey.objects.all().count() == 1
     response2 = client.get(
         url,
-        {"variant": 1, "uuid": response1.json()["uuid"]},
+        {"uuid": response1.json()["uuid"]},
         HTTP_CLOUDFRONT_VIEWER_COUNTRY_NAME="USA",
     )
     assert response2.json()["uuid"] == response1.json()["uuid"]
@@ -88,8 +92,13 @@ def test_ping_landing_page_survey_reuse_uuid(client):
 
 @pytest.mark.django_db
 def test_ping_landing_page_survey_authenticated(user_client, wiki_user):
+    # This sets the needed session cookie
+    variant_url = reverse("api.v1.plus.landing_page_variant")
+    response = user_client.get(variant_url)
+    assert response.status_code == 200
+
     url = reverse("api.v1.plus.landing_page_survey")
-    response = user_client.get(url, {"variant": 1})
+    response = user_client.get(url)
     assert response.status_code == 200
     (result,) = LandingPageSurvey.objects.all()
     assert result.user == wiki_user
