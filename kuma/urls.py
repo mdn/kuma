1e5b950f255e21dee7dfa946d3e99689cdb4377b
Peter Bengtsson <mail@peterbe.com>
2021-07-21 11:28:32 -0400

    remove /:locale/events redirect

--- kuma/urls.py
+++ kuma/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.decorators.cache import cache_control, never_cache
+from django.urls import include, re_path
+from django.views.decorators.cache import never_cache
 from django.views.generic import RedirectView
 
 from kuma.attachments import views as attachment_views
@@ -14,21 +14,6 @@ MONTH = DAY * 30
 admin.autodiscover()
 
 urlpatterns = [re_path("", include("kuma.health.urls"))]
-urlpatterns += [
-    path(
-        "<locale>/events/",
-        # Here the "shared_cache_control" decorator is an optimization. It
-        # informs the CDN to cache the redirect for a month, so once this URL
-        # has been requested by a client, all other client requests will be
-        # redirected by the CDN instead of this Django service.
-        cache_control(maxage=MONTH)(
-            RedirectView.as_view(
-                url="https://community.mozilla.org/events/", permanent=False
-            )
-        ),
-        name="events",
-    ),
-]
 
 if settings.MAINTENANCE_MODE:
     urlpatterns.append(
