6c627d5a430582d30fd8c14e62706530960812f2
Peter Bengtsson <mail@peterbe.com>
2021-07-23 15:16:18 -0400

    rewrite

--- kuma/api/v1/plus.py
+++ kuma/api/v1/plus.py
@@ -4,13 +4,10 @@ from uuid import UUID
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.middleware.csrf import get_token
 from django.shortcuts import get_object_or_404
-from django.views.decorators.http import require_GET, require_http_methods
+from django.views.decorators.http import require_http_methods
 from ratelimit.decorators import ratelimit
 
 from kuma.plus.models import LandingPageSurvey
-
-
-VARIANTS_SESSION_KEY = "plus-landing-page-variant-v1"
 
 
 @ratelimit(key="user_or_ip", rate="100/m", block=True)
@@ -22,11 +19,6 @@ def landing_page_survey(request):
         if not uuid:
             return HttpResponseBadRequest("missing 'uuid'")
         survey = get_object_or_404(LandingPageSurvey, uuid=uuid)
-        email = request.POST.get("email")
-        if email:
-            survey.email = email.strip()
-            survey.save()
-
         response_json = request.POST.get("response")
         if response_json:
             try:
@@ -37,9 +29,6 @@ def landing_page_survey(request):
             survey.save()
         context["ok"] = True
     else:
-        variant = request.session.get(VARIANTS_SESSION_KEY)
-        if not variant:
-            return HttpResponseBadRequest("missing 'variant'")
         uuid = request.GET.get("uuid")
         if uuid:
             try:
@@ -53,25 +42,9 @@ def landing_page_survey(request):
                 request.META.get("HTTP_CLOUDFRONT_VIEWER_COUNTRY_NAME") or ""
             )
             survey = LandingPageSurvey.objects.create(
-                variant=variant,
                 geo_information=geo_information,
-                user=request.user if request.user.is_authenticated else None,
             )
         context["uuid"] = survey.uuid
         context["csrfmiddlewaretoken"] = get_token(request)
 
-    return JsonResponse(context)
-
-
-@ratelimit(key="user_or_ip", rate="100/m", block=True)
-@require_GET
-def landing_page_variant(request):
-    variant = request.session.get(VARIANTS_SESSION_KEY)
-    if not variant:
-        # As of July 23, 2021 we only have 1 variant in this version of the
-        # experiment. There's no split A/B. But the number still needs to be
-        # *something* so that it can tie back to a survey result.
-        variant = 3
-        request.session[VARIANTS_SESSION_KEY] = variant
-    context = {"variant": variant}
     return JsonResponse(context)
--- kuma/api/v1/tests/test_plus.py
+++ kuma/api/v1/tests/test_plus.py
@@ -8,31 +8,12 @@ from kuma.plus.models import LandingPageSurvey
 
 @pytest.mark.django_db
 def test_ping_landing_page_survey_happy_path(client, settings):
-    # This sets the needed session cookie
-    variant_url = reverse("api.v1.plus.landing_page_variant")
-    response = client.get(variant_url)
-    assert response.status_code == 200
-    variant = response.json()["variant"]
-
     url = reverse("api.v1.plus.landing_page_survey")
     response = client.get(url, HTTP_CLOUDFRONT_VIEWER_COUNTRY_NAME="Antartica")
     assert response.status_code == 200
     (result,) = LandingPageSurvey.objects.all()
-    assert result.variant == variant
-    assert not result.email
     assert not result.response
     assert result.geo_information == "Antartica"
-
-    # Now fill it with an email
-    response = client.post(
-        url,
-        {"uuid": str(result.uuid), "email": " peterbe@example.com  "},
-    )
-    assert response.status_code == 200
-    (result,) = LandingPageSurvey.objects.all()
-    assert result.variant == variant
-    assert result.email == "peterbe@example.com"
-    assert not result.response
 
     # Now fill it with a response
     response = client.post(
@@ -44,23 +25,12 @@ def test_ping_landing_page_survey_happy_path(client, settings):
     )
     assert response.status_code == 200
     (result,) = LandingPageSurvey.objects.all()
-    assert result.variant == variant
-    assert result.email == "peterbe@example.com"
     assert result.response == {"price": "perfect"}
 
 
 @pytest.mark.django_db
 def test_ping_landing_page_survey_bad_request(client):
     url = reverse("api.v1.plus.landing_page_survey")
-
-    # No ?variant=...
-    response = client.get(url)
-    assert response.status_code == 400
-
-    # This sets the needed session cookie
-    variant_url = reverse("api.v1.plus.landing_page_variant")
-    response = client.get(variant_url)
-    assert response.status_code == 200
 
     # Not a valid UUID
     response = client.get(url, {"uuid": "xxx"})
@@ -83,11 +53,6 @@ def test_ping_landing_page_survey_bad_request(client):
 
 @pytest.mark.django_db
 def test_ping_landing_page_survey_reuse_uuid(client):
-    # This sets the needed session cookie
-    variant_url = reverse("api.v1.plus.landing_page_variant")
-    response = client.get(variant_url)
-    assert response.status_code == 200
-
     url = reverse("api.v1.plus.landing_page_survey")
     response1 = client.get(url, HTTP_CLOUDFRONT_VIEWER_COUNTRY_NAME="Sweden")
     assert response1.status_code == 200
@@ -99,27 +64,3 @@ def test_ping_landing_page_survey_reuse_uuid(client):
     )
     assert response2.json()["uuid"] == response1.json()["uuid"]
     assert LandingPageSurvey.objects.all().count() == 1
-
-
-@pytest.mark.django_db
-def test_ping_landing_page_survey_authenticated(user_client, wiki_user):
-    # This sets the needed session cookie
-    variant_url = reverse("api.v1.plus.landing_page_variant")
-    response = user_client.get(variant_url)
-    assert response.status_code == 200
-
-    url = reverse("api.v1.plus.landing_page_survey")
-    response = user_client.get(url)
-    assert response.status_code == 200
-    (result,) = LandingPageSurvey.objects.all()
-    assert result.user == wiki_user
-
-
-@pytest.mark.django_db
-def test_landing_page_variant_happy_path(client):
-    url = reverse("api.v1.plus.landing_page_variant")
-    response = client.get(url)
-    assert response.status_code == 200
-    first_time = response.json()
-    # As of July 23 2021, this is now hardcoded. See comment in the view function.
-    assert first_time["variant"] == 3
