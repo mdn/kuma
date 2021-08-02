cc83a93f2169c33983eadb8b6e80b23979d3cc02
Peter Bengtsson <mail@peterbe.com>
2021-08-02 07:26:56 -0400

    refactor style based on feedback

--- kuma/api/v1/plus/bookmarks.py
+++ kuma/api/v1/plus/bookmarks.py
@@ -20,8 +20,8 @@ class NotOKDocumentURLError(Exception):
     """When the document URL doesn't resolve as a 200 OK"""
 
 
-def require_subscriber(func):
-    @functools.wraps(func)
+def require_subscriber(view_function):
+    @functools.wraps(view_function)
     def inner(request, *args, **kwargs):
         user = request.user
         if not user.is_authenticated:
@@ -34,7 +34,7 @@ def require_subscriber(func):
         #     user=user, canceled__isnull=True
         # ).exists():
         #     return HttpResponseForbidden("not a subscriber")
-        return func(request, *args, **kwargs)
+        return view_function(request, *args, **kwargs)
 
     return inner
 
@@ -44,9 +44,13 @@ def require_subscriber(func):
 @require_subscriber
 def bookmarks(request):
     # E.g. POST -d url=https://... /api/v1/bookmarks/
-    # or   GET /api/v1/bookmarks/?url=https://...
-    if request.method == "POST" or request.GET.get("url"):
+
+    if request.method == "POST":
         return _toggle_bookmark(request)
+
+    # E.g GET /api/v1/bookmarks/?url=https://...
+    elif request.GET.get("url"):
+        return _get_bookmark(request)
 
     # E.g. GET /api/v1/bookmarks/
     return _get_bookmarks(request)
@@ -97,101 +101,105 @@ def _get_bookmarks(request):
     return JsonResponse(context)
 
 
-def _toggle_bookmark(request):
-    url = (
-        (request.GET.get("url") or "")
-        .strip()
-        .replace("https://developer.mozilla.org", "")
-    )
-    if not url:
-        return HttpResponseBadRequest("missing 'url'")
-    if not valid_url(url):
-        return HttpResponseBadRequest("invalid 'url'")
-
-    absolute_url = f"{settings.BOOKMARKS_BASE_URL}{url}/index.json"
-
-    if request.method == "POST":
-        # Let's toggle!
-        try:
-            documenturl = DocumentURL.objects.get(uri=DocumentURL.normalize_uri(url))
-            assert not documenturl.invalid
-        except DocumentURL.DoesNotExist:
-            response = download_url(absolute_url)
-            # Because it's so big, only store certain fields that are used.
-            full_metadata = response.json()["doc"]
-            metadata = {}
-            # Should we so day realize that we want to and need to store more
-            # about the remote Yari documents, we'd simply invoke some background
-            # processing job that forces a refresh.
-            for key in ("title", "mdn_url", "parents"):
-                if key in full_metadata:
-                    metadata[key] = full_metadata[key]
-
-            documenturl = DocumentURL.objects.create(
-                uri=DocumentURL.normalize_uri(url),
-                absolute_url=absolute_url,
-                metadata=metadata,
-            )
-
-        users_bookmarks = Bookmark.objects.filter(
-            user_id=request.user.id, documenturl=documenturl
-        )
-
-        for bookmark in users_bookmarks:
-            # If had a bookmark before, only need to toggle the 'deleted'
-            if bookmark.deleted:
-                bookmark.deleted = None
-
-                # If a user deletes a bookmark and then decides to bookmark it again
-                # it's because they used the "undo" functionality in the front-end.
-                # If this is the case, the most recently modified bookmark is going
-                # to be the one they deleted.
-                was_undo = False
-                for most_recently_modified in Bookmark.objects.filter(
-                    user_id=request.user.id,
-                ).order_by("-modified")[:1]:
-                    was_undo = most_recently_modified.id == bookmark.id
-                if not was_undo:
-                    # When you undo, it doesn't change the 'created' date.
-                    # But if you've re-bookmarked it after some time
-                    bookmark.created = timezone.now()
-            else:
-                bookmark.deleted = timezone.now()
-            bookmark.save()
-            break
-        else:
-            # Otherwise, create a brand new entry
-            Bookmark.objects.create(
-                user_id=request.user.id,
-                documenturl=documenturl,
-            )
-
-        return JsonResponse({"OK": True}, status=201)
-    else:
-
-        def serialize_bookmark(bookmark):
-            return {"id": bookmark.id, "created": bookmark.created}
-
-        # raise Exception(url)
-        users_bookmarks = Bookmark.objects.filter(
-            user_id=request.user.id, deleted__isnull=True
-        )
-        bookmarked = None
-        for bookmark in users_bookmarks.filter(
-            documenturl__uri=DocumentURL.normalize_uri(url)
-        ):
-            bookmarked = serialize_bookmark(bookmark)
-
-        context = {
-            "bookmarked": bookmarked,
-            "csrfmiddlewaretoken": get_token(request),
-        }
-        return JsonResponse(context)
-
-
 def valid_url(url):
     return (
         url.startswith("/")
         and ("/docs/" in url or "/plus/" in url)
         and "://" not in url
     )
+
+
+def get_url(view_function):
+    @functools.wraps(view_function)
+    def inner(request, *args, **kwargs):
+        url = (
+            (request.GET.get("url") or "")
+            .strip()
+            .replace("https://developer.mozilla.org", "")
+        )
+        if not url:
+            return HttpResponseBadRequest("missing 'url'")
+        if not valid_url(url):
+            return HttpResponseBadRequest("invalid 'url'")
+
+        return view_function(request, *(args + (url,)), **kwargs)
+
+    return inner
+
+
+@get_url
+def _get_bookmark(request, url):
+    users_bookmarks = Bookmark.objects.filter(
+        user_id=request.user.id, deleted__isnull=True
+    )
+    bookmarked = None
+    for bookmark in users_bookmarks.filter(
+        documenturl__uri=DocumentURL.normalize_uri(url)
+    ):
+        bookmarked = {"id": bookmark.id, "created": bookmark.created}
+
+    context = {
+        "bookmarked": bookmarked,
+        "csrfmiddlewaretoken": get_token(request),
+    }
+    return JsonResponse(context)
+
+
+@get_url
+def _toggle_bookmark(request, url):
+    absolute_url = f"{settings.BOOKMARKS_BASE_URL}{url}/index.json"
+    try:
+        documenturl = DocumentURL.objects.get(uri=DocumentURL.normalize_uri(url))
+        assert not documenturl.invalid
+    except DocumentURL.DoesNotExist:
+        response = download_url(absolute_url)
+        # Because it's so big, only store certain fields that are used.
+        full_metadata = response.json()["doc"]
+        metadata = {}
+        # Should we so day realize that we want to and need to store more
+        # about the remote Yari documents, we'd simply invoke some background
+        # processing job that forces a refresh.
+        for key in ("title", "mdn_url", "parents"):
+            if key in full_metadata:
+                metadata[key] = full_metadata[key]
+
+        documenturl = DocumentURL.objects.create(
+            uri=DocumentURL.normalize_uri(url),
+            absolute_url=absolute_url,
+            metadata=metadata,
+        )
+
+    users_bookmarks = Bookmark.objects.filter(
+        user_id=request.user.id, documenturl=documenturl
+    )
+
+    for bookmark in users_bookmarks:
+        # If had a bookmark before, only need to toggle the 'deleted'
+        if bookmark.deleted:
+            bookmark.deleted = None
+
+            # If a user deletes a bookmark and then decides to bookmark it again
+            # it's because they used the "undo" functionality in the front-end.
+            # If this is the case, the most recently modified bookmark is going
+            # to be the one they deleted.
+            was_undo = False
+            for most_recently_modified in Bookmark.objects.filter(
+                user_id=request.user.id,
+            ).order_by("-modified")[:1]:
+                was_undo = most_recently_modified.id == bookmark.id
+            if not was_undo:
+                # When you undo, it doesn't change the 'created' date.
+                # But if you've re-bookmarked it after some time
+                bookmark.created = timezone.now()
+        else:
+            bookmark.deleted = timezone.now()
+        bookmark.save()
+        break
+    else:
+        # Otherwise, create a brand new entry
+        Bookmark.objects.create(
+            user_id=request.user.id,
+            documenturl=documenturl,
+        )
+
+    return JsonResponse({"OK": True}, status=201)
