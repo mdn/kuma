00acf60fc7c1a2f34b99661b6bc036ab7b586b0b
Chris Beaven <smileychris@gmail.com>
2022-02-16 12:38:36 +1300

    Ensure ninja does not cache responses

--- kuma/api/v1/api.py
+++ kuma/api/v1/api.py
@@ -1,10 +1,21 @@
+from django.http import HttpResponse
+from django.utils.cache import add_never_cache_headers
 from ninja import NinjaAPI
 from ratelimit.exceptions import Ratelimited
 
 from .auth import admin_auth, profile_auth
 
-api = NinjaAPI(auth=profile_auth, csrf=True, version="v1")
-admin_api = NinjaAPI(
+
+class NoCacheNinjaAPI(NinjaAPI):
+    def create_response(self, *args, **kwargs) -> HttpResponse:
+        response = super().create_response(*args, **kwargs)
+        add_never_cache_headers(response)
+        return response
+
+
+api = NoCacheNinjaAPI(auth=profile_auth, csrf=True, version="v1")
+
+admin_api = NoCacheNinjaAPI(
     auth=admin_auth, csrf=False, version="v1", urls_namespace="admin_api"
 )
 
